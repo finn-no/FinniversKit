96e90374abe2cc56d7cef511d21afee00af7a25b
Dybdal, Aksel <aksel.dybdal@finn.no>
2017-09-21 11:03:58 +0200

    Adds the Troika framework

---
+++ TroikaIOS/TroikaIOS.h
@@ -0,0 +1,19 @@
+//
+//  TroikaIOS.h
+//  TroikaIOS
+//
+//  Created by Dybdal, Aksel on 21.09.2017.
+//  Copyright © 2017 FINN AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TroikaIOS.
+FOUNDATION_EXPORT double TroikaIOSVersionNumber;
+
+//! Project version string for TroikaIOS.
+FOUNDATION_EXPORT const unsigned char TroikaIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TroikaIOS/PublicHeader.h>
+
+
