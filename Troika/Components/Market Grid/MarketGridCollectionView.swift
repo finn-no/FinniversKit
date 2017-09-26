d03bea4a551050eb1c8e23b87dfe490a015c1822
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-09-26 15:40:53 +0200

    Adds a template suggestion for the market grid collection view

---
+++ Troika/Components/Market Grid/MarketGridCollectionView.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class MarketGridCollectionView: UIView {
+    
+    // Mark: - Internal properties
+    
+    // Mark: - External properties
+    
+    // Mark: - Setup
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        // Perform setup
+    }
+    
+    // Mark: - Test
+    
+    // Mark: - Layout
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Add custom subviews
+        // Layout your custom views
+    }
+    
+    // Mark: - Dependency injection
+    
+}
+
