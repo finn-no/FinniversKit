210ba20234a7b24ae6ea153ba43beab4a5d48302
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-09-28 15:28:44 +0200

    Adds playground file to display the market grid collection view

---
+++ Project.playground/Pages/Market Grid Collection View.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+import PlaygroundSupport
+import Troika
+
+struct MarketGridDataModel: MarketGridPresentable {
+    let iconImage: UIImage?
+    let showExternalLinkIcon: Bool
+    let title: String
+}
+
+struct ScreenSize {
+    static let iPhone5 = CGSize(width: 320, height: 568)
+    static let iPhone7 = CGSize(width: 375, height: 667)
+    static let iPhone7plus = CGSize(width: 414, height: 736)
+    static let iPad = CGSize(width: 703, height: 1024)
+    static let iPadPro = CGSize(width: 1024, height: 1366)
+}
+
+let image = #imageLiteral(resourceName: "eiendom.png")
+let title = "Eiendom"
+let shouldShowExternalLinkIcon = false
+let eiendom = MarketGridDataModel(iconImage: image, showExternalLinkIcon: shouldShowExternalLinkIcon, title: title)
+
+var presentables = [MarketGridDataModel]()
+let collectionView = MarketGridCollectionView(frame: .zero)
+let screenSize = ScreenSize.iPad
+
+for _ in 1...7 {
+    presentables.append(eiendom)
+}
+for _ in 1...5 {
+    presentables.append(MarketGridDataModel(iconImage: image, showExternalLinkIcon: true, title: title))
+}
+
+collectionView.marketGridPresentables = presentables
+collectionView.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+collectionView.backgroundColor = .white
+
+PlaygroundPage.current.liveView = collectionView
