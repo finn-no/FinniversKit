0a16500f60ec67eab993fceba8421c2b5b61993a
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-10-04 13:43:02 +0200

    Updates font extension to use FINN fonts

--- Troika/Extensions/UIFont+Troika.swift
+++ Troika/Extensions/UIFont+Troika.swift
@@ -3,30 +3,30 @@ import UIKit
 public extension UIFont {
 
     public static var h1: UIFont {
-        return UIFont.systemFont(ofSize: 36, weight: .medium)
+        return UIFont(name: "FINNTypeWebStrippet-Medium", size: 36.0)!
     }
 
     public static var h2: UIFont {
-        return UIFont.systemFont(ofSize: 28, weight: .light)
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 28.0)!
     }
 
     public static var h3: UIFont {
-        return UIFont.systemFont(ofSize: 22.5, weight: .medium)
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 22.5)!
     }
 
     public static var h4: UIFont {
-        return UIFont.systemFont(ofSize: 18, weight: .medium)
+        return UIFont(name: "FINNTypeWebStrippet-Medium", size: 18.0)!
     }
     
     public static var h5: UIFont {
-        return UIFont.systemFont(ofSize: 14, weight: .medium)
+        return UIFont(name: "FINNTypeWebStrippet-Medium", size: 14.0)!
     }
     
     public static var body: UIFont {
-        return UIFont.systemFont(ofSize: 18, weight: .light)
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 18.0)!
     }
     
     public static var detail: UIFont {
-        return UIFont.systemFont(ofSize: 14, weight: .light)
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 14.0)!
     }
 }
