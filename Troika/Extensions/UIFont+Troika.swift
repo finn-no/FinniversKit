930025c5cf2170c1301636dc156246ec2aa0e181
Ulrik Utheim Sverdrup <ulrik.utheim.sverdrup@gmail.com>
2017-10-04 14:27:10 +0200

    Font changes (#6)
    * Changes the font extension file to include all the different sint names, sizes and weights
    
    * Adds the FINNTypeWebStrippet fonts
    
    * Updates font extension to use FINN fonts
    
    * Changes font names from h to t
    
    * Downsizes the useed font in market grid cell

--- Troika/Extensions/UIFont+Troika.swift
+++ Troika/Extensions/UIFont+Troika.swift
@@ -3,18 +3,30 @@ import UIKit
 public extension UIFont {
 
     public static var t1: UIFont {
-        return UIFont.systemFont(ofSize: 16)
+        return UIFont(name: "FINNTypeWebStrippet-Medium", size: 36.0)!
     }
 
     public static var t2: UIFont {
-        return UIFont.systemFont(ofSize: 12)
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 28.0)!
     }
 
     public static var t3: UIFont {
-        return UIFont.systemFont(ofSize: 18, weight: .medium)
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 22.5)!
     }
 
     public static var t4: UIFont {
-        return UIFont.systemFont(ofSize: 14, weight: .light)
+        return UIFont(name: "FINNTypeWebStrippet-Medium", size: 18.0)!
+    }
+    
+    public static var t5: UIFont {
+        return UIFont(name: "FINNTypeWebStrippet-Medium", size: 14.0)!
+    }
+    
+    public static var body: UIFont {
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 18.0)!
+    }
+    
+    public static var detail: UIFont {
+        return UIFont(name: "FINNTypeWebStrippet-Light", size: 14.0)!
     }
 }
