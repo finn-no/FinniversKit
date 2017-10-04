9a165fa47680f3f406e0be24cbc660e964606a1f
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-10-04 12:51:42 +0200

    Generates a UIColor extension from Zeplin

--- Troika/Extensions/UIColor+Troika.swift
+++ Troika/Extensions/UIColor+Troika.swift
@@ -1,29 +1,59 @@
 import UIKit
 
-public extension UIColor {
-    private class func colorFromRGB(_ r:CGFloat, _ g:CGFloat, _ b:CGFloat) -> UIColor {
-        return UIColor(red:r/255.0, green:g/255.0, blue:b/255.0, alpha:1)
-    }
-    
-    // Logo colors
-    static let primaryBlue = colorFromRGB(0, 99, 251)
-    static let primaryTurquoise = colorFromRGB(6, 190, 251)
-    
-    // Text colors
-    static let licorice = colorFromRGB(71, 68, 69)
-    static let stone = colorFromRGB(118, 118, 118)
-    static let sardine = colorFromRGB(223, 228, 232)
-    
-    // Background colors
-    static let ice = colorFromRGB(241, 249, 255)
-    static let mint = colorFromRGB(204, 255, 236)
-    static let salmon = colorFromRGB(255, 206, 215)
-    static let milk = colorFromRGB(255, 255, 255)
-    static let toothPaste = colorFromRGB(182, 240, 255)
-    static let banana = colorFromRGB(255, 245, 200)
-    
-    // Detail colors
-    static let pea = colorFromRGB(104, 226, 186)
-    static let watermelon = colorFromRGB(255, 88, 68)
-    static let cherry = colorFromRGB(218, 36, 0)
+extension UIColor {
+	@nonobjc class var ice: UIColor { 
+		return UIColor(named: "ice")!
+	}
+
+	@nonobjc class var milk: UIColor { 
+		return UIColor(named: "milk")!
+	}
+
+	@nonobjc class var licorice: UIColor { 
+		return UIColor(named: "licorice")!
+	}
+
+	@nonobjc class var primaryBlue: UIColor { 
+		return UIColor(named: "primaryBlue")!
+	}
+
+	@nonobjc class var secondaryBlue: UIColor { 
+		return UIColor(named: "secondaryBlue")!
+	}
+
+	@nonobjc class var stone: UIColor { 
+		return UIColor(named: "stone")!
+	}
+
+	@nonobjc class var sardine: UIColor { 
+		return UIColor(named: "sardine")!
+	}
+
+	@nonobjc class var salmon: UIColor { 
+		return UIColor(named: "salmon")!
+	}
+
+	@nonobjc class var mint: UIColor { 
+		return UIColor(named: "mint")!
+	}
+
+	@nonobjc class var toothPaste: UIColor { 
+		return UIColor(named: "toothPaste")!
+	}
+
+	@nonobjc class var banana: UIColor { 
+		return UIColor(named: "banana")!
+	}
+
+	@nonobjc class var cherry: UIColor { 
+		return UIColor(named: "cherry")!
+	}
+
+	@nonobjc class var watermelon: UIColor { 
+		return UIColor(named: "watermelon")!
+	}
+
+	@nonobjc class var pea: UIColor { 
+		return UIColor(named: "pea")!
+	}
 }
