50c3cfda456b3576b5be561344119ddf6c02cdff
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-10-04 15:38:14 +0200

    Enables registering of fonts in the framework

--- Troika/Extensions/UIFont+Troika.swift
+++ Troika/Extensions/UIFont+Troika.swift
@@ -30,3 +30,45 @@ public extension UIFont {
         return UIFont(name: "FINNTypeWebStrippet-Light", size: 14.0)!
     }
 }
+
+extension UIFont {
+    
+    public static func registerFont(with filenameString: String, bundleIdentifierString: String) {
+        guard let bundle = Bundle(identifier: bundleIdentifierString) else {
+            print("UIFont+:  Failed to register font - bundle identifier invalid.")
+            return
+        }
+        print(bundle)
+        guard let pathForResourceString = bundle.path(forResource: filenameString, ofType: "ttf") else {
+            print("UIFont+:  Failed to register font - path for resource not found.")
+            return
+        }
+        
+        guard let fontData = NSData(contentsOfFile: pathForResourceString) else {
+            print("UIFont+:  Failed to register font - font data could not be loaded.")
+            return
+        }
+        
+        guard let dataProvider = CGDataProvider(data: fontData) else {
+            print("UIFont+:  Failed to register font - data provider could not be loaded.")
+            return
+        }
+        
+        guard let fontRef = CGFont(dataProvider) else {
+            print("UIFont+:  Failed to register font - font could not be loaded.")
+            return
+        }
+        
+        var errorRef: Unmanaged<CFError>? = nil
+        if (CTFontManagerRegisterGraphicsFont(fontRef, &errorRef) == false) {
+            print("UIFont+:  Failed to register font - register graphics font failed - this font may have already been registered in the main bundle.")
+        }
+    }
+    
+    public static func registerTroikaFonts() {
+        registerFont(with: "FINNTypeWebStrippet-Light", bundleIdentifierString: "no.finn.Troika")
+        registerFont(with: "FINNTypeWebStrippet-Medium", bundleIdentifierString: "no.finn.Troika")
+        registerFont(with: "FINNTypeWebStrippet-Regular", bundleIdentifierString: "no.finn.Troika")
+        registerFont(with: "FINNTypeWebStrippet-Bold", bundleIdentifierString: "no.finn.Troika")
+    }
+}
