d0b5f005ca95c1882e77b9e72309bb85952068f6
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-10-04 13:00:14 +0200

    Changes the font extension file to include all the different sint names, sizes and weights

--- Troika/Extensions/UIFont+Troika.swift
+++ Troika/Extensions/UIFont+Troika.swift
@@ -2,19 +2,31 @@ import UIKit
 
 public extension UIFont {
 
-    public static var t1: UIFont {
-        return UIFont.systemFont(ofSize: 16)
+    public static var h1: UIFont {
+        return UIFont.systemFont(ofSize: 36, weight: .medium)
     }
 
-    public static var t2: UIFont {
-        return UIFont.systemFont(ofSize: 12)
+    public static var h2: UIFont {
+        return UIFont.systemFont(ofSize: 28, weight: .light)
     }
 
-    public static var t3: UIFont {
+    public static var h3: UIFont {
+        return UIFont.systemFont(ofSize: 22.5, weight: .medium)
+    }
+
+    public static var h4: UIFont {
         return UIFont.systemFont(ofSize: 18, weight: .medium)
     }
-
-    public static var t4: UIFont {
+    
+    public static var h5: UIFont {
+        return UIFont.systemFont(ofSize: 14, weight: .medium)
+    }
+    
+    public static var body: UIFont {
+        return UIFont.systemFont(ofSize: 18, weight: .light)
+    }
+    
+    public static var detail: UIFont {
         return UIFont.systemFont(ofSize: 14, weight: .light)
     }
 }
