1ddf30f71660f75a66e5bbd4005b8d3d9b48d17d
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-10-05 09:00:04 +0200

    Extracting the configuration of the market grid collection view into its own file

--- Troika/Components/Market Grid/Market Grid Collection View/MarketGridCollectionView.swift
+++ Troika/Components/Market Grid/Market Grid Collection View/MarketGridCollectionView.swift
@@ -8,86 +8,6 @@ protocol MarketGridCollectionViewDelegate: NSObjectProtocol {
     func didSelect(item: MarketGridPresentable, in gridView: MarketGridCollectionView)
 }
 
-enum ScreenSizeCategory {
-    case small
-    case medium
-    case large(CGFloat)
-    
-    static let mediumRange: Range<CGFloat> = (375.0..<415.0)
-    static let portraitModeScreenWidth = CGFloat(768)
-    
-    init(width: CGFloat) {
-        switch width {
-        case let width where width > ScreenSizeCategory.mediumRange.upperBound:
-            self = .large(width)
-        case let width where width < ScreenSizeCategory.mediumRange.lowerBound:
-            self = .small
-        default:
-            self = .medium
-        }
-    }
-    
-    var interimSpacing: CGFloat {
-        switch self {
-        case .large:
-            return 0
-        default:
-            return 0
-        }
-    }
-    
-    var sideMargins: CGFloat {
-        switch self {
-        case .large:
-            return 20
-        default:
-            return 16
-        }
-    }
-    
-    var edgeInsets: UIEdgeInsets {
-        switch self {
-        case .large:
-            return UIEdgeInsets(top: 16, left: self.sideMargins, bottom: 0, right: self.sideMargins)
-        default:
-            return UIEdgeInsets(top: 8, left: self.sideMargins, bottom: 0, right: self.sideMargins)
-        }
-    }
-    
-    var lineSpacing: CGFloat {
-        switch self {
-        case .large:
-            return 30
-        default:
-            return 16
-        }
-    }
-    
-    var itemsPerRow: CGFloat {
-        switch self {
-        case .large(let width):
-            if width > ScreenSizeCategory.portraitModeScreenWidth {
-                return 6
-            } else {
-                return 5
-            }
-        case .medium:
-            return 4
-        case .small:
-            return 3
-        }
-    }
-    
-    var itemHeight: CGFloat {
-        switch self {
-        case .large:
-            return 80
-        default:
-            return 60
-        }
-    }
-}
-
 public class MarketGridCollectionView: UIView {
     
     // Mark: - Internal properties
@@ -100,7 +20,7 @@ public class MarketGridCollectionView: UIView {
         collectionView.backgroundColor = .clear
         return collectionView
     }()
-    private weak var dataSource: MarketGridCollectionViewDataSource?
+//    private weak var dataSource: MarketGridCollectionViewDataSource?
     private weak var delegate: MarketGridCollectionViewDelegate?
     
     // Mark: - External properties
@@ -110,7 +30,7 @@ public class MarketGridCollectionView: UIView {
     init(frame: CGRect = .zero, dataSource: MarketGridCollectionViewDataSource, delegate: MarketGridCollectionViewDelegate) {
         super.init(frame: frame)
         
-        self.dataSource = dataSource
+//        self.dataSource = dataSource
         self.delegate = delegate
         
         setup()
