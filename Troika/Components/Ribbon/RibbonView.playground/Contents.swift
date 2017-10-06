b2872ee714325f47a8a1c940ff7e90ba5d4a7a04
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-10-06 14:26:17 +0200

    Sets up the view for the ribbon view playground

--- Troika/Components/Ribbon/RibbonView.playground/Contents.swift
+++ Troika/Components/Ribbon/RibbonView.playground/Contents.swift
@@ -12,24 +12,64 @@ struct ScreenSize {
     static let iPad = CGSize(width: 768, height: 1024)
 }
 
-struct RibbonDataModel: RibbonPresentable {
-    let type: RibbonType
-    let title: String
-}
-
 let screenSize = ScreenSize.iPhone7
-let ribbonView = RibbonView(frame: .init(x: 10, y: 10, width: 130, height: 20))
-let view = UIView(frame: .zero)
-let ribbonText = "Denne er solgt!"
-let presentable = RibbonDataModel(type: RibbonType.error, title: ribbonText)
+
+let successRibbonView = RibbonView()
+let errorRibbonView = RibbonView()
+let warningRibbonView = RibbonView()
+let defaultRibbonView = RibbonView()
+let disabledRibbonView = RibbonView()
+let sponsoredRibbonView = RibbonView()
+
+let interimSpacing: CGFloat = 8
+let lineSpacing: CGFloat = 10
+let margin: CGFloat = 16
+let view = UIView()
 
 view.backgroundColor = .white
 view.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
 
-ribbonView.presentable = presentable
-ribbonView.frame
+successRibbonView.presentable = RibbonDataModel.success
+errorRibbonView.presentable = RibbonDataModel.error
+warningRibbonView.presentable = RibbonDataModel.warning
+defaultRibbonView.presentable = RibbonDataModel.ordinary
+disabledRibbonView.presentable = RibbonDataModel.disabled
+sponsoredRibbonView.presentable = RibbonDataModel.sponsored
 
-view.addSubview(ribbonView)
-view.bringSubview(toFront: ribbonView)
+defaultRibbonView.translatesAutoresizingMaskIntoConstraints = false
+successRibbonView.translatesAutoresizingMaskIntoConstraints = false
+warningRibbonView.translatesAutoresizingMaskIntoConstraints = false
+errorRibbonView.translatesAutoresizingMaskIntoConstraints = false
+disabledRibbonView.translatesAutoresizingMaskIntoConstraints = false
+sponsoredRibbonView.translatesAutoresizingMaskIntoConstraints = false
+
+view.addSubview(headerLabel)
+view.addSubview(successRibbonView)
+view.addSubview(errorRibbonView)
+view.addSubview(warningRibbonView)
+view.addSubview(defaultRibbonView)
+view.addSubview(disabledRibbonView)
+view.addSubview(sponsoredRibbonView)
+
+headerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin).isActive = true
+headerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: margin).isActive = true
+
+defaultRibbonView.leadingAnchor.constraint(equalTo: headerLabel.leadingAnchor).isActive = true
+defaultRibbonView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 16).isActive = true
+
+successRibbonView.leadingAnchor.constraint(equalTo: defaultRibbonView.trailingAnchor, constant: interimSpacing).isActive = true
+successRibbonView.topAnchor.constraint(equalTo: defaultRibbonView.topAnchor).isActive = true
+
+warningRibbonView.leadingAnchor.constraint(equalTo: successRibbonView.trailingAnchor, constant: interimSpacing).isActive = true
+warningRibbonView.topAnchor.constraint(equalTo: successRibbonView.topAnchor).isActive = true
+
+errorRibbonView.leadingAnchor.constraint(equalTo: warningRibbonView.trailingAnchor, constant: interimSpacing).isActive = true
+errorRibbonView.topAnchor.constraint(equalTo: warningRibbonView.topAnchor).isActive = true
+
+disabledRibbonView.leadingAnchor.constraint(equalTo: defaultRibbonView.leadingAnchor).isActive = true
+disabledRibbonView.topAnchor.constraint(equalTo: defaultRibbonView.bottomAnchor, constant: lineSpacing).isActive = true
+
+sponsoredRibbonView.leadingAnchor.constraint(equalTo: disabledRibbonView.trailingAnchor, constant: interimSpacing).isActive = true
+sponsoredRibbonView.topAnchor.constraint(equalTo: disabledRibbonView.topAnchor).isActive = true
 
 PlaygroundPage.current.liveView = view
