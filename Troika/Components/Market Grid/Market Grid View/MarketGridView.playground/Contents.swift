7ff1dae2bfeb2c53da3f4dd535aa10c655fcc156
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-10-06 16:03:14 +0200

    Creating a separate file for screen sizes to use in playgrounds

--- Troika/Components/Market Grid/Market Grid View/MarketGridView.playground/Contents.swift
+++ Troika/Components/Market Grid/Market Grid View/MarketGridView.playground/Contents.swift
@@ -5,19 +5,11 @@ import TroikaDemoKit
 
 TroikaDemoKit.setupPlayground()
 
-struct ScreenSize {
-    static let iPhone5 = CGSize(width: 320, height: 568)
-    static let iPhone7 = CGSize(width: 375, height: 667)
-    static let iPhone7plus = CGSize(width: 414, height: 736)
-    static let iPad = CGSize(width: 768, height: 1024)
-}
-
 var presentables = Market.allMarkets
 let collectionView = MarketGridView(frame: .zero)
-let screenSize = ScreenSize.iPad
 
 collectionView.marketGridPresentables = presentables
-collectionView.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+collectionView.frame = ScreenSize.medium
 collectionView.backgroundColor = .white
 
 PlaygroundPage.current.liveView = collectionView
