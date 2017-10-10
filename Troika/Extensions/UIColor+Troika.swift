91966193a3d824284bee043fad297baf8c710506
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-10-10 10:07:08 +0200

    Makes colors public to access them in playground

--- Troika/Extensions/UIColor+Troika.swift
+++ Troika/Extensions/UIColor+Troika.swift
@@ -1,59 +1,59 @@
 import UIKit
 
 extension UIColor {
-	@nonobjc class var ice: UIColor { 
+	@nonobjc public class var ice: UIColor {
 		return UIColor(named: "ice", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var milk: UIColor { 
+	@nonobjc public class var milk: UIColor {
 		return UIColor(named: "milk", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var licorice: UIColor {
+	@nonobjc public class var licorice: UIColor {
         return UIColor(named: "licorice", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var primaryBlue: UIColor { 
+	@nonobjc public class var primaryBlue: UIColor {
 		return UIColor(named: "primaryBlue", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var secondaryBlue: UIColor { 
+	@nonobjc public class var secondaryBlue: UIColor {
 		return UIColor(named: "secondaryBlue", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var stone: UIColor { 
+	@nonobjc public class var stone: UIColor {
 		return UIColor(named: "stone", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var sardine: UIColor { 
+	@nonobjc public class var sardine: UIColor {
 		return UIColor(named: "sardine", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var salmon: UIColor { 
+	@nonobjc public class var salmon: UIColor {
 		return UIColor(named: "salmon", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var mint: UIColor { 
+	@nonobjc public class var mint: UIColor {
 		return UIColor(named: "mint", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var toothPaste: UIColor { 
+	@nonobjc public class var toothPaste: UIColor {
 		return UIColor(named: "toothPaste", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var banana: UIColor { 
+	@nonobjc public class var banana: UIColor {
 		return UIColor(named: "banana", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var cherry: UIColor { 
+	@nonobjc public class var cherry: UIColor {
 		return UIColor(named: "cherry", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var watermelon: UIColor { 
+	@nonobjc public class var watermelon: UIColor {
 		return UIColor(named: "watermelon", in: .troika, compatibleWith: nil)!
 	}
 
-	@nonobjc class var pea: UIColor { 
+	@nonobjc public class var pea: UIColor { 
 		return UIColor(named: "pea", in: .troika, compatibleWith: nil)!
 	}
 }
