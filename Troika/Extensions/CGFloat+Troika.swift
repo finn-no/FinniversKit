ec9cdcd46b655ae1c2587a17831f46b90f1d2061
Ulrik Utheim Sverdrup <ulrik.utheim.sverdrup@gmail.com>
2017-10-16 15:43:58 +0200

    Adds an extension on CGFloat to define static values for spacing (#14)

---
+++ Troika/Extensions/CGFloat+Troika.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension CGFloat {
+    static let verySmallSpacing: CGFloat = 2
+    static let smallSpacing: CGFloat = 4
+    static let mediumSpacing: CGFloat = 8
+    static let mediumLargeSpacing: CGFloat = 16
+    static let largeSpacing: CGFloat = 32
+    static let veryLargeSpacing: CGFloat = 64
+}
