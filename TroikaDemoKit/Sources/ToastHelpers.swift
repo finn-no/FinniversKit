8f5230ddec111d81c9205486deb4a468596d85cf
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-10-19 09:35:14 +0200

    Adds a helper file for the Toast playground

--- TroikaDemoKit/Sources/ToastHelpers.swift
+++ TroikaDemoKit/Sources/ToastHelpers.swift
@@ -1,9 +1,37 @@
-//
-//  ToastHelpers.swift
-//  TroikaDemoKit
-//
-//  Created by Utheim Sverdrup, Ulrik on 18.10.2017.
-//  Copyright © 2017 FINN AS. All rights reserved.
-//
-
 import Foundation
+import Troika
+
+public enum ToastDataModel: ToastPresentable {
+    case success
+    case successImage
+    case error
+    case button
+    case multiline
+    
+    public var type: ToastType {
+        switch self {
+        case .success: return ToastType.success
+        case .successImage: return ToastType.sucesssImage
+        case .error: return ToastType.error
+        case .button: return ToastType.button
+        case .multiline: return ToastType.success
+        }
+    }
+    
+    public var messageTitle: String {
+        switch self {
+        case .success: return "Success message with icon"
+        case .successImage: return "Success message with thumbnail"
+        case .error: return "Error message with icon"
+        case .button: return "Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action Toast message with action"
+        case .multiline: return "Toast with a message that spans over multiple lines"
+        }
+    }
+    
+    public var actionButtonTitle: String? {
+        switch self {
+        case .button: return "Action"
+        default: return nil
+        }
+    }
+}
