42a39b5870cc770240bab0cf3504ae479cb30436
Elvis Nuñez <elvisnunez@me.com>
2017-11-05 22:43:45 +0100

    Add project

---
+++ Troika/Components/Login/LoginApp.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = LoginViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ Troika/Components/Login/LoginViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
