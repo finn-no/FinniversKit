475cb2768f1d8104d3c57c10aec9528f6c3602e4
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-11-14 10:11:23 +0100

    Adds buttons and labels for login, forgot password, new user and user terms

--- Project/LoginViewController.swift
+++ Project/LoginViewController.swift
@@ -20,31 +20,54 @@ class LoginViewController: UIViewController {
         return textField
     }()
 
-    fileprivate lazy var infoText: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.textAlignment = .center
-        textView.isScrollEnabled = false
-        textView.isEditable = false
-        textView.font = .body
-        textView.textColor = .licorice
-        textView.text = "Logg inn for å sende meldinger, lagre favoritter og søk. Du får også varsler når det skjer noe nytt!"
-        return textView
-    }()
-
-    fileprivate lazy var forgotPasswordLabel: Label = {
-        let label = Label(style: .detail(.primaryBlue))
+    fileprivate lazy var infoText: Label = {
+        let label = Label(style: .body(.licorice))
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Glemt passord"
         label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "Logg inn for å sende meldinger, lagre favoritter og søk. Du får også varsler når det skjer noe nytt!"
         return label
     }()
 
     fileprivate lazy var loginButton: Button = {
-        let button = Button()
+        let button = Button(style: .flat)
+        button.setTitle("Logg inn", for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
         return button
+    }()
+
+    fileprivate lazy var forgotPasswordButton: Button = {
+        let button = Button(style: .normal)
+        button.setTitle("Glemt passord", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(forgotPasswordTapped), for: .touchUpInside)
+        return button
+    }()
+
+    fileprivate lazy var newUserButton: Button = {
+        let button = Button(style: .normal)
+        button.setTitle("Ny bruker", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(newUserTapped), for: .touchUpInside)
+        return button
+    }()
+
+    fileprivate lazy var userTermsButton: Button = {
+        let button = Button(style: .link)
+        button.setTitle("Brukervilkår", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(userTermsTapped), for: .touchUpInside)
+        return button
+    }()
+
+    fileprivate lazy var userTermsIntro: Label = {
+        let label = Label(style: .detail(.licorice))
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Ved å logge inn aksepterer du burkervilkårene våres"
+        return label
     }()
 
     override func viewDidLoad() {
@@ -56,20 +79,24 @@ class LoginViewController: UIViewController {
         view.addSubview(infoText)
         view.addSubview(emailTextField)
         view.addSubview(passwordTextField)
-        view.addSubview(forgotPasswordLabel)
         view.addSubview(loginButton)
+        view.addSubview(forgotPasswordButton)
+        view.addSubview(newUserButton)
+        view.addSubview(userTermsIntro)
+        view.addSubview(userTermsButton)
 
         emailTextField.presentable = TextFieldDataModel.email
         passwordTextField.presentable = TextFieldDataModel.password
-        loginButton.presentable = ButtonDataModel.flat
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tap)
 
         view.backgroundColor = .white
 
         NSLayoutConstraint.activate([
-            infoText.topAnchor.constraint(equalTo: view.topAnchor, constant: .largeSpacing + 64),
+            infoText.topAnchor.constraint(equalTo: view.topAnchor, constant: .largeSpacing + navigationController!.navigationBar.frame.height + UIApplication.shared.statusBarFrame.height),
             infoText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
             infoText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.largeSpacing),
-            infoText.heightAnchor.constraint(equalToConstant: 85),
 
             emailTextField.topAnchor.constraint(equalTo: infoText.bottomAnchor, constant: .largeSpacing),
             emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
@@ -79,18 +106,45 @@ class LoginViewController: UIViewController {
             passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
             passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.largeSpacing),
 
-            forgotPasswordLabel.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: .mediumLargeSpacing),
-            forgotPasswordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
-            forgotPasswordLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.largeSpacing),
-
-            loginButton.topAnchor.constraint(equalTo: forgotPasswordLabel.bottomAnchor, constant: .largeSpacing),
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: .largeSpacing),
             loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
             loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.largeSpacing),
-            loginButton.heightAnchor.constraint(equalToConstant: 42),
+
+            forgotPasswordButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: .mediumSpacing),
+            forgotPasswordButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
+            forgotPasswordButton.widthAnchor.constraint(equalToConstant: view.frame.size.width / 2 - .largeSpacing - .smallSpacing),
+
+            newUserButton.topAnchor.constraint(equalTo: forgotPasswordButton.topAnchor),
+            newUserButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.largeSpacing),
+            newUserButton.widthAnchor.constraint(equalToConstant: view.frame.size.width / 2 - .largeSpacing - .smallSpacing),
+
+            userTermsIntro.topAnchor.constraint(equalTo: forgotPasswordButton.bottomAnchor, constant: .mediumLargeSpacing),
+            userTermsIntro.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
+            userTermsIntro.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.largeSpacing),
+
+            userTermsButton.topAnchor.constraint(equalTo: userTermsIntro.bottomAnchor, constant: .mediumSpacing),
+            userTermsButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .largeSpacing),
+            userTermsButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.largeSpacing),
         ])
     }
 
     @objc func loginTapped() {
         print("Login tapped!")
+    }
+
+    @objc func forgotPasswordTapped() {
+        print("Forgott password button tapped!")
+    }
+
+    @objc func newUserTapped() {
+        print("New user tapped")
+    }
+
+    @objc func userTermsTapped() {
+        print("User terms tapped")
+    }
+
+    @objc func handleTap() {
+        view.endEditing(true)
     }
 }
