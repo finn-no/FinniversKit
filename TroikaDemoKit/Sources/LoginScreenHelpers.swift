4646435305f62ccbe86b70912655e12e4175f7f1
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-11-15 15:10:20 +0100

    Adds a helper file for the login screen in the playground support framework

---
+++ TroikaDemoKit/Sources/LoginScreenHelpers.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import Foundation
+import Troika
+
+public struct LoginScreenData: LoginScreenModel {
+    public let headerText = "Logg inn for å sende meldinger, lagre favoritter og søk. Du får også varsler når det skjer noe nytt!"
+
+    public let emailPlaceholder = "E-post"
+
+    public let passwordPlaceholder = "Passord"
+
+    public let forgotPasswordButtonTitle = "Glemt passord"
+
+    public let loginButtonTitle = "Logg inn"
+
+    public let newUserButtonTitle = "Ny bruker"
+
+    public let userTermsIntroText = "Ved å logge inn aksepterer du brukervilkårene våre"
+
+    public let userTermsButtonTitle = "Brukervilkår"
+}
