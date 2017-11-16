75679c460e59ccce888f00935978df54d62a8b3e
Elvis Nuñez <elvisnunez@me.com>
2017-11-16 19:44:04 +0100

    Fix playground

--- Troika/Components/Market Grid/Market Grid View/MarketGridView.playground/Contents.swift
+++ Troika/Components/Market Grid/Market Grid View/MarketGridView.playground/Contents.swift
@@ -7,13 +7,32 @@ import PlaygroundSupport
 import Troika
 import TroikaDemoKit
 
+class MarketData: NSObject {
+    var models = Market.allMarkets
+}
+
+extension MarketData: MarketGridViewDataSource {
+    func numberOfItems(inMarketGridView marketGridView: MarketGridView) -> Int {
+        return models.count
+    }
+
+    func marketGridView(_ marketGridView: MarketGridView, modelAtIndex index: Int) ->  MarketGridModel {
+        return models[index]
+    }
+}
+
+extension MarketData: MarketGridViewDelegate {
+    func didSelect(itemAtIndex index: Int, inMarketGridView gridView: MarketGridView) {
+
+    }
+}
+
 TroikaDemoKit.setupPlayground()
 
-var models = Market.allMarkets
-let collectionView = MarketGridView(frame: .zero)
-
-collectionView.marketGridModels = models
+let data = MarketData()
+let collectionView = MarketGridView(delegate: data, dataSource: data)
 collectionView.frame = ScreenSize.medium
 collectionView.backgroundColor = .white
 
 PlaygroundPage.current.liveView = collectionView
+
