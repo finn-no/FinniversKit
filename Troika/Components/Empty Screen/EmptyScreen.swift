f03a370167d00b49a242f99b58df064fb150317a
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-11-20 13:30:46 +0100

    Adds a empty screen view component file

---
+++ Troika/Components/Empty Screen/EmptyScreen.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class EmptyScreen: UIView {
+
+    // MARK: - Internal properties
+
+    // MARK: - External properties / Dependency injection
+
+    // MARK: - Setup
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        // Perform setup
+        // Add child views as subviews
+    }
+
+    // MARK: - Superclass Overrides
+
+    // MARK: - Layout
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        // Perform layout
+        // Setup constraints/frames
+    }
+
+    // MARK: - Private
+
+}
