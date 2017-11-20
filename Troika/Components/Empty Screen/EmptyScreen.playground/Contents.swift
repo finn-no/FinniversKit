847720d935ebd125d204fb8090dce070eb8116bf
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-11-20 14:10:13 +0100

    Adds playground file for empty screen

---
+++ Troika/Components/Empty Screen/EmptyScreen.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+import PlaygroundSupport
+import Troika
+import TroikaDemoKit
+
+TroikaDemoKit.setupPlayground()
+
+let view = UIView()
+view.backgroundColor = .white
+view.frame = ScreenSize.medium
+
+
+
+PlaygroundPage.current.liveView = view
