f82715da624f0fb8d48a4d78d11d205b326f08da
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-11-21 15:29:47 +0100

    Sets up a view controller in the demo app with bouncy squares

--- Project/EmptyScreeenViewController.swift
+++ Project/EmptyScreeenViewController.swift
@@ -10,17 +10,16 @@ class EmptyScreenViewController: UIViewController {
 
     // MARK: - Internal properties
 
-    //    private lazy var emptyView: EmptyScreen = {
-    //        let view = EmptyScreen(frame: .zero)
-    //        view.translatesAutoresizingMaskIntoConstraints = false
-    //        return view
-    //    }()
+    var square1 = UIView(frame: CGRect(x: 70, y: 130, width: 100, height: 100))
+    var square2 = UIView(frame: CGRect(x: 230, y: 130, width: 90, height: 90))
+    var square3 = UIView(frame: CGRect(x: 60, y: 45, width: 50, height: 50))
+    var square4 = UIView(frame: CGRect(x: 260, y: 45, width: 55, height: 55))
 
-    var square1 = UIView(frame: CGRect(x: 70, y: 100, width: 100, height: 100))
-    var square2 = UIView(frame: CGRect(x: 230, y: 100, width: 90, height: 90))
-    var square3 = UIView(frame: CGRect(x: 60, y: 50, width: 50, height: 50))
-    var square4 = UIView(frame: CGRect(x: 260, y: 50, width: 40, height: 40))
     var animator: UIDynamicAnimator?
+    var gravity: UIGravityBehavior?
+    var collision: UICollisionBehavior?
+    var attach: UIAttachmentBehavior?
+    var itemBehaviour: UIDynamicItemBehavior?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +28,6 @@ class EmptyScreenViewController: UIViewController {
 
     private func setupView() {
         view.backgroundColor = .milk
-        //        view.addSubview(emptyView)
 
         square1.backgroundColor = .salmon
         square2.backgroundColor = .banana
@@ -38,62 +36,63 @@ class EmptyScreenViewController: UIViewController {
 
         view.addSubview(square1)
         view.addSubview(square2)
-        //        view.addSubview(square3)
-        //        view.addSubview(square4)
+        view.addSubview(square3)
+        view.addSubview(square4)
 
-        //        NSLayoutConstraint.activate([
-        //            emptyView.topAnchor.constraint(equalTo: view.topAnchor),
-        //            emptyView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        //            emptyView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        //            emptyView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        //            ])
-
-        let allSquares = [square1, square2] // , square3, square4]
+        let allSquares = [square1, square2, square3, square4]
 
         animator = UIDynamicAnimator(referenceView: view)
 
         // Setup gravity
-        let gravity = UIGravityBehavior(items: allSquares)
+        gravity = UIGravityBehavior(items: allSquares)
         let direction = CGVector(dx: 0, dy: 1.0)
-        gravity.gravityDirection = direction
+        gravity?.gravityDirection = direction
 
         // Setup boundries for collision
-        let boundries = UICollisionBehavior(items: allSquares)
-        boundries.translatesReferenceBoundsIntoBoundary = true
+        collision = UICollisionBehavior(items: allSquares)
+        collision?.translatesReferenceBoundsIntoBoundary = true
 
         // Setup elasticity for bounce
-        let bounce = UIDynamicItemBehavior(items: allSquares)
-        bounce.elasticity = 0.6
+        itemBehaviour = UIDynamicItemBehavior(items: allSquares)
+        itemBehaviour?.elasticity = 0.6
+
+        // Setup pan gesture
+        let pan1 = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+        let pan2 = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+        let pan3 = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+        let pan4 = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+
+        square1.addGestureRecognizer(pan1)
+        square2.addGestureRecognizer(pan2)
+        square3.addGestureRecognizer(pan3)
+        square4.addGestureRecognizer(pan4)
 
         // Add behaviour to animator
-        animator?.addBehavior(gravity)
-        animator?.addBehavior(boundries)
-        animator?.addBehavior(bounce)
+        animator?.addBehavior(gravity!)
+        animator?.addBehavior(collision!)
+        animator?.addBehavior(itemBehaviour!)
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: view)
-            if square1.frame.contains(location) {
-                square1.center = location
-            } else if square2.frame.contains(location) {
-                square2.center = location
-            }
+    @objc func panAction(sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let touchLocation = sender.location(in: sender.view)
+
+        if sender.state == .began {
+            let touchOffset = UIOffsetMake(touchLocation.x - sender.view!.bounds.midX, touchLocation.y - sender.view!.bounds.midY)
+            attach = UIAttachmentBehavior(item: sender.view!, offsetFromCenter: touchOffset, attachedToAnchor: location)
+            animator?.addBehavior(attach!)
+        } else if sender.state == .changed {
+            attach?.anchorPoint = location
+        } else if sender.state == .ended {
+            animator?.removeBehavior(attach!)
+
+            let itemBehaviour = UIDynamicItemBehavior(items: [sender.view!])
+            itemBehaviour.addLinearVelocity(sender.velocity(in: view), for: sender.view!)
+            itemBehaviour.angularResistance = 0
+
+            animator?.addBehavior(gravity!)
+            animator?.addBehavior(collision!)
+            animator?.addBehavior(itemBehaviour)
         }
-    }
-
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: view)
-            if square1.frame.contains(location) {
-                square1.center = location
-            } else if square2.frame.contains(location) {
-                square2.center = location
-            }
-        }
-    }
-
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        viewDidLoad()
     }
 }
