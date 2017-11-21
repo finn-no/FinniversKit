4dc5f87aefee6cbff83fbeb83fd7d0f08a8e86d6
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-11-21 15:41:01 +0100

    Swift lint removing white space

--- Troika/Components/Empty Screen/EmptyScreen.playground/Contents.swift
+++ Troika/Components/Empty Screen/EmptyScreen.playground/Contents.swift
@@ -12,7 +12,7 @@ TroikaDemoKit.setupPlayground()
 let view = EmptyScreen(frame: ScreenSize.medium)
 view.backgroundColor = .white
 
- view.header = "Her var det stille gitt"
- view.message = "Når du prater med andre på FINN, vil meldingene dine dukke opp her.\n\n Søk på noe du har lyst på, send en melding til selgeren og bli enige om en handel på én-to-tre!"
+view.header = "Her var det stille gitt"
+view.message = "Når du prater med andre på FINN, vil meldingene dine dukke opp her.\n\n Søk på noe du har lyst på, send en melding til selgeren og bli enige om en handel på én-to-tre!"
 
 PlaygroundPage.current.liveView = view
