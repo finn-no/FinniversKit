14fb9e56c71f9980e4b8811fb1f00a0224a385f5
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-11-23 15:28:58 +0100

    Adds separate files for a circle and a triangle view

---
+++ Troika/Components/EmptyScreen/MovableBlockView/TriangleView.swift
@@ -0,0 +1,63 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+internal class TriangleView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        backgroundColor = .clear
+    }
+
+    // MARK: - Override methods
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        guard let context = UIGraphicsGetCurrentContext() else {
+            print("No context available")
+            return
+        }
+
+        let path = createTrianglePath(from: rect)
+
+        context.addPath(path)
+        context.setFillColor(UIColor.banana.cgColor)
+        context.fillPath()
+    }
+
+    override var collisionBoundsType: UIDynamicItemCollisionBoundsType {
+        return .path
+    }
+
+    override var collisionBoundingPath: UIBezierPath {
+        let path = createTrianglePath(from: frame)
+
+        var translation = CGAffineTransform(translationX: -bounds.size.width / 2, y: -bounds.size.height / 2)
+        let movedPath = path.copy(using: &translation)
+        let mask = UIBezierPath(cgPath: movedPath!)
+        return mask
+    }
+
+    // MARK: - Methods
+
+    private func createTrianglePath(from shapeFrame: CGRect) -> CGPath {
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: shapeFrame.width, y: shapeFrame.height))
+        path.addLine(to: CGPoint(x: 0, y: shapeFrame.height))
+        path.addLine(to: CGPoint(x: 0, y: 0))
+
+        return path
+    }
+}
