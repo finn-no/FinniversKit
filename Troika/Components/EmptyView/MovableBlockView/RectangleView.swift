799832bd118685f215b3d6ff3330640c0fc39e00
Ulrik Utheim Sverdrup <ulrik.utheim.sverdrup@gmail.com>
2017-11-28 13:50:39 +0100

    Empty screen component (#42)
    * Adds a empty screen view component file
    
    * Adds playground file for empty screen
    
    * Adds an empty screen view controller
    
    * Swift lint auto updating
    
    * Sets up the empty screen class in the Troika framework
    
    * Sets up a view controller in the demo app with bouncy squares
    
    * Sets up a playground for the empty screen
    
    * Swift lint removing white space
    
    * Changes color to use finn color in playground
    
    * Moves the moving blocks into empty screen component and implements it on demo app
    
    * Makes the gravity affecting the blocks dependant on the acceslerometer data
    
    * Sets an offset on the top of the view so that the blocks can slide out of the view
    
    * Unwrapping and refactoring the to prevent force unwrapping
    
    * Cleans up after merging master into branch
    
    * Converts empty screen into new playground setup
    
    * Sets automatic signing and development team
    
    * Adds separate files for a circle and a triangle view
    
    * Changing two of the blocks on the empty screen to a circle and a triangle
    
    * Guards the translation transformation of the path
    
    * Adds a rounded rectangle view to the movable view folder
    
    * Adds the rounded square to the playground app and rearranges the movable views to start at the bottom of the screen
    
    * Centers the startposition to make it not look too weird on iPad
    
    * Sets the accessibility label for the header and the message labels on the empty screen
    
    * Creates a separate class file for the rectangle view
    
    * Implements an attachable view protocol for the movable object so that the pan action is capable to manage several views
    
    * Various renaming and adds density to the shapes so they behave different to eachother
    
    * Removes unused MARK comment
    
    * Changes the update interval for the motion manager to 60 frames per second
    
    * Renames a variable in the get accelerometer data method
    
    * Changes from if/else check to a switch case in orientation correction
    
    * Removes print statements used for debugging
    
    * Add default background color
    
    * Renaming the EmptyScreen to EmptyView
    
    * Renames the group folder to EmptyView
    
    * Adds some frictoin between objects

---
+++ Troika/Components/EmptyView/MovableBlockView/RectangleView.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+internal class RectangleView: UIView, AttachableView {
+
+    var attach: UIAttachmentBehavior?
+    private let cornerRadius: CGFloat = 4.0
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        backgroundColor = .salmon
+        layer.cornerRadius = cornerRadius
+    }
+
+    // MARK: - Override methods
+
+    override var collisionBoundsType: UIDynamicItemCollisionBoundsType {
+        return .rectangle
+    }
+}
---
+++ Troika/Components/EmptyView/MovableBlockView/TriangleView.swift
@@ -0,0 +1,68 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+internal class TriangleView: UIView, AttachableView {
+
+    var attach: UIAttachmentBehavior?
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        backgroundColor = .clear
+    }
+
+    // MARK: - Override methods
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+
+        let path = createTrianglePath(from: rect)
+
+        context.addPath(path)
+        context.setFillColor(UIColor.banana.cgColor)
+        context.fillPath()
+    }
+
+    override var collisionBoundsType: UIDynamicItemCollisionBoundsType {
+        return .path
+    }
+
+    override var collisionBoundingPath: UIBezierPath {
+        let path = createTrianglePath(from: frame)
+
+        var translation = CGAffineTransform(translationX: -bounds.size.width / 2, y: -bounds.size.height / 2)
+        guard let movedPath = path.copy(using: &translation) else {
+            return UIBezierPath(cgPath: path)
+        }
+        let mask = UIBezierPath(cgPath: movedPath)
+        return mask
+    }
+
+    // MARK: - Methods
+
+    private func createTrianglePath(from shapeFrame: CGRect) -> CGPath {
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: shapeFrame.width, y: shapeFrame.height))
+        path.addLine(to: CGPoint(x: 0, y: shapeFrame.height))
+        path.addLine(to: CGPoint(x: 0, y: 0))
+        path.closeSubpath()
+
+        return path
+    }
+}
