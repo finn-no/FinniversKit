072707887140dcc1abce1c8ee22e9b7b71e743dc
Elvis Nuñez <elvisnunez@me.com>
2017-12-07 08:54:34 +0100

    Might be useful as reference when moving hardcoded spacing in the FINN app

--- Troika/Extensions/CGFloat+Troika.swift
+++ Troika/Extensions/CGFloat+Troika.swift
@@ -5,10 +5,22 @@
 import Foundation
 
 public extension CGFloat {
+
+    /// Separation of 2 points.
     static let verySmallSpacing: CGFloat = 2
+
+    /// Separation of 4 points.
     static let smallSpacing: CGFloat = 4
+
+    /// Separation of 8 points.
     static let mediumSpacing: CGFloat = 8
+
+    /// Separation of 16 points.
     static let mediumLargeSpacing: CGFloat = 16
+
+    /// Separation of 32 points.
     static let largeSpacing: CGFloat = 32
+
+    /// Separation of 64 points.
     static let veryLargeSpacing: CGFloat = 64
 }
