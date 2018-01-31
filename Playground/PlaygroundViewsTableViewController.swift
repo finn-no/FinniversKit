e9f432238e11a4ee5fbfc5fca2ef2146ec79a125
ag12 <amirg12@hotmail.no>
2018-01-31 09:47:58 +0100

    Added dismiss view controller, and some clean up (#68)

--- Playground/PlaygroundViewsTableViewController.swift
+++ Playground/PlaygroundViewsTableViewController.swift
@@ -3,95 +3,41 @@
 //
 
 import UIKit
+import Troika
 
-let views = [
-    ButtonPlayground(),
-    LabelPlayground(),
-    LoginViewPlayground(),
-    MarketGridViewPlayground(),
-    MarketGridCellPlayground(),
-    PreviewGridViewPlayground(),
-    PreviewGridCellPlayground(),
-    RibbonPlayground(),
-    TextFieldPlayground(),
-    ToastPlayground(),
-    EmptyViewPlayground(),
-]
-
-let viewNames: [String] = [
-    "ButtonPlayground",
-    "LabelPlayground",
-    "LoginViewPlayground",
-    "MarketGridViewPlayground",
-    "MarketGridCellPlayground",
-    "PreviewGridViewPlayground",
-    "PreviewGridCellPlayground",
-    "RibbonPlayground",
-    "TextFieldPlayground",
-    "ToastPlayground",
-    "EmptyViewPlayground",
-]
+// MARK: - PlaygroundViewsTableViewController
 
 class PlaygroundViewsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
 
+    private func setup() {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.backgroundColor = .white
         tableView.delegate = self
     }
+}
+
+// MARK: - UITableViewDelegate
+
+extension PlaygroundViewsTableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return views.count
+        return TroikaViews.all.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = viewNames[indexPath.row]
+        cell.textLabel?.text = TroikaViews.all[indexPath.row].rawValue
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let selectedView = views[indexPath.row]
-
-        switch selectedView {
-        case is ButtonPlayground:
-            typealias View = ButtonPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is LabelPlayground:
-            typealias View = LabelPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is LoginViewPlayground:
-            typealias View = LoginViewPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is MarketGridViewPlayground:
-            typealias View = MarketGridViewPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is MarketGridCellPlayground:
-            typealias View = MarketGridCellPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is PreviewGridViewPlayground:
-            typealias View = PreviewGridViewPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is PreviewGridCellPlayground:
-            typealias View = PreviewGridCellPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is RibbonPlayground:
-            typealias View = RibbonPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is TextFieldPlayground:
-            typealias View = TextFieldPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is ToastPlayground:
-            typealias View = ToastPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        case is EmptyViewPlayground:
-            typealias View = EmptyViewPlayground
-            present(ViewController<View>(), animated: true, completion: nil)
-        default:
-            break
-        }
+        let selectedView = TroikaViews.all[indexPath.row]
+        present(selectedView.viewController(), animated: true)
     }
 }
