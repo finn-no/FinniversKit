128b507affd8c9a807fbb898eb7540f56326ae65
Elvis Nuñez <elvisnunez@me.com>
2018-02-05 00:37:35 +0100

    Add color demo

--- Sources/DNA/Color/Color.swift
+++ Sources/DNA/Color/Color.swift
@@ -5,59 +5,59 @@
 import UIKit
 
 extension UIColor {
-    @nonobjc public class var ice: UIColor {
+    public class var ice: UIColor {
         return UIColor(r: 241, g: 249, b: 255)!
     }
 
-    @nonobjc public class var milk: UIColor {
+    public class var milk: UIColor {
         return UIColor(r: 255, g: 255, b: 255)!
     }
 
-    @nonobjc public class var licorice: UIColor {
+    public class var licorice: UIColor {
         return UIColor(r: 71, g: 68, b: 69)!
     }
 
-    @nonobjc public class var primaryBlue: UIColor {
+    public class var primaryBlue: UIColor {
         return UIColor(r: 0, g: 99, b: 251)!
     }
 
-    @nonobjc public class var secondaryBlue: UIColor {
+    public class var secondaryBlue: UIColor {
         return UIColor(r: 6, g: 190, b: 251)!
     }
 
-    @nonobjc public class var stone: UIColor {
+    public class var stone: UIColor {
         return UIColor(r: 118, g: 118, b: 118)!
     }
 
-    @nonobjc public class var sardine: UIColor {
+    public class var sardine: UIColor {
         return UIColor(r: 223, g: 228, b: 232)!
     }
 
-    @nonobjc public class var salmon: UIColor {
+    public class var salmon: UIColor {
         return UIColor(r: 255, g: 206, b: 215)!
     }
 
-    @nonobjc public class var mint: UIColor {
+    public class var mint: UIColor {
         return UIColor(r: 204, g: 255, b: 236)!
     }
 
-    @nonobjc public class var toothPaste: UIColor {
+    public class var toothPaste: UIColor {
         return UIColor(r: 182, g: 240, b: 255)!
     }
 
-    @nonobjc public class var banana: UIColor {
+    public class var banana: UIColor {
         return UIColor(r: 255, g: 245, b: 200)!
     }
 
-    @nonobjc public class var cherry: UIColor {
+    public class var cherry: UIColor {
         return UIColor(r: 218, g: 36, b: 0)!
     }
 
-    @nonobjc public class var watermelon: UIColor {
+    public class var watermelon: UIColor {
         return UIColor(r: 255, g: 88, b: 68)!
     }
 
-    @nonobjc public class var pea: UIColor {
+    public class var pea: UIColor {
         return UIColor(r: 104, g: 226, b: 184)!
     }
 
