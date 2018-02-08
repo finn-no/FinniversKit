040bd68985b2d6b9b7b4cf048678f21b9e22e12b
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-02-08 10:34:59 +0100

    Adds a demo view and a default data model for the consent

---
+++ Sources/Components/New Group/Demo/ConsentViewDefaultData.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import FinniversKit
+import Foundation
+
+public struct ConsentViewDefaultData: ConsentViewModel {
+    public var yesButtonTitle = "Ja, takk"
+    public var noButtonTitle = "Nei, takk"
+    public var cancelButtonTitle = "Avbryt"
+    public var descriptionTitle = "Svar ja er du snill"
+    public var descriptionBodyText = "Blablabla"
+
+    public init() {}
+}
