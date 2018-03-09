015cfc47a214d11ce20c1f4ffa05374e49f86c23
Henrik Holmsen <henrik.holmsen@gmail.com>
2018-03-09 17:41:27 +0100

    Refactor replacing html links with attributed string

--- Sources/Components/BroadcastView/BroadcastViewModel.swift
+++ Sources/Components/BroadcastView/BroadcastViewModel.swift
@@ -12,78 +12,15 @@ public struct BroadcastViewModel {
     }
 
     var messageWithHTMLLinksReplacedByAttributedStrings: NSAttributedString {
-        var result = NSMutableAttributedString(string: message)
-
-        while let htmlLink = HTMLLink.links(in: result.string).first {
-            var string = result.string
-            string.replaceSubrange(htmlLink.range, with: htmlLink.text)
-
-            let linkRange = NSString(string: string).range(of: htmlLink.text)
-
-            let attributedString = NSMutableAttributedString(string: string)
-            attributedString.addAttribute(NSAttributedStringKey.link, value: htmlLink.url, range: linkRange)
-
-            result = attributedString
+        guard let messageData = message.data(using: .utf16) else {
+            return NSAttributedString(string: message)
         }
 
-        return result
-    }
-}
-
-extension BroadcastViewModel {
-    var htmlLinksInMessage: [HTMLLink] {
-        return HTMLLink.links(in: message)
-    }
-}
-
-extension BroadcastViewModel {
-    struct HTMLLink {
-        let range: Range<String.Index>
-        let text: String
-        let url: URL
-
-        static var htmlAnchorRegexPattern: String {
-            // <\s*a\s+[^>]*href\s*=\s*["']?([^\"' >]+)["']\s*>(.+?(?=<))<\s*\/a\s*>
-            let openingTagRegex = "<\\s*a\\s+[^>]*href\\s*=\\s*[\"']?([^\"' >]+)[\"']\\s*>"
-            let linkTextRegex = "(.+?(?=<))"
-            let closingTagRegex = "<\\s*\\/a\\s*>"
-
-            return openingTagRegex + linkTextRegex + closingTagRegex
+        let options = [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html]
+        guard let attributedString = try? NSMutableAttributedString(data: messageData, options: options, documentAttributes: nil) else {
+            return NSAttributedString(string: message)
         }
 
-        init?(from textCheckingResult: NSTextCheckingResult, in searchString: String) {
-            guard textCheckingResult.numberOfRanges == 3 else {
-                return nil
-            }
-
-            let urlCaptureGroupRange = textCheckingResult.range(at: 1)
-            let urlStartIndex = String.Index(encodedOffset: urlCaptureGroupRange.lowerBound)
-            let urlEndIndex = String.Index(encodedOffset: urlCaptureGroupRange.upperBound)
-            let urlString = String(searchString[urlStartIndex ..< urlEndIndex])
-
-            let linkTextCaptureGroupRange = textCheckingResult.range(at: 2)
-            let linkTextStartIndex = String.Index(encodedOffset: linkTextCaptureGroupRange.lowerBound)
-            let linkTextEndIndex = String.Index(encodedOffset: linkTextCaptureGroupRange.upperBound)
-            let linkText = String(searchString[linkTextStartIndex ..< linkTextEndIndex])
-
-            guard let url = URL(string: urlString), let range = Range<String.Index>(textCheckingResult.range, in: searchString) else {
-                return nil
-            }
-
-            self.range = range
-            text = linkText
-            self.url = url
-        }
-
-        static func links(in searchString: String) -> [HTMLLink] {
-            guard let regex = try? NSRegularExpression(pattern: HTMLLink.htmlAnchorRegexPattern, options: .caseInsensitive) else {
-                return []
-            }
-
-            let matches = regex.matches(in: searchString, range: NSMakeRange(0, searchString.utf16.count))
-            let links = matches.flatMap { HTMLLink(from: $0, in: searchString) }
-
-            return links
-        }
+        return attributedString
     }
 }
--- UnitTests/BroadcastViewModelTests.swift
+++ UnitTests/BroadcastViewModelTests.swift
@@ -6,92 +6,31 @@
 import XCTest
 
 class BroadcastViewModelTests: XCTestCase {
-    func testBroadcastViewModelCanFindHTMLLinksInMessage() {
-        // Given
-        let linkURL = "https://www.finn.no"
-        let linkText = "Besøk oss på vår nettside"
-        let link = "<a href=\"\(linkURL)\">\(linkText)</a>"
-        let testMessage = "Dette er en melding som inneholder en link \(link)"
-        let rangeOfLink = testMessage.range(of: link)
-
-        // When
-        let viewModel = BroadcastViewModel(with: testMessage)
-        let htmLinkInMessage = viewModel.htmlLinksInMessage.first
-
-        // Then
-        XCTAssertNotNil(htmLinkInMessage)
-        XCTAssertEqual(htmLinkInMessage?.range, rangeOfLink)
-        XCTAssertEqual(htmLinkInMessage?.text, linkText)
-        XCTAssertEqual(htmLinkInMessage?.url.absoluteString, linkURL)
-    }
-
-    func testBroadcastViewModelCanFindMultipleHTMLLinksInMessage() {
-        // Given
-        let firstLinkURL = "https://www.finn.no"
-        let firstLinkText = "Besøk oss på vår nettside"
-        let firstLink = "<a href=\"\(firstLinkURL)\">\(firstLinkText)</a>"
-
-        let secondLinkURL = "www.aftenposten.no"
-        let secondLinkText = "Aftenposten"
-        let secondLink = "<a href=\"\(secondLinkURL)\">\(secondLinkText)</a>"
-
-        let testMessage = "Dette er en melding som inneholder to linker \(firstLink) \(secondLink)"
-        let rangeOfFirstLink = testMessage.range(of: firstLink)
-        let rangeOfLastLink = testMessage.range(of: secondLink)
-
-        // When
-        let viewModel = BroadcastViewModel(with: testMessage)
-        let firstHTTMLinkInMessage = viewModel.htmlLinksInMessage.first
-        let secondHTTMLinkInMessage = viewModel.htmlLinksInMessage.last
-
-        // Then
-        XCTAssertNotNil(firstHTTMLinkInMessage)
-        XCTAssertEqual(firstHTTMLinkInMessage?.range, rangeOfFirstLink)
-        XCTAssertEqual(firstHTTMLinkInMessage?.text, firstLinkText)
-        XCTAssertEqual(firstHTTMLinkInMessage?.url.absoluteString, firstLinkURL)
-
-        XCTAssertNotNil(secondHTTMLinkInMessage)
-        XCTAssertEqual(secondHTTMLinkInMessage?.range, rangeOfLastLink)
-        XCTAssertEqual(secondHTTMLinkInMessage?.text, secondLinkText)
-        XCTAssertEqual(secondHTTMLinkInMessage?.url.absoluteString, secondLinkURL)
-    }
-
-    func testBroadcastViewModelCanFindHTMLLinksInMessageWhenLinkSyntaxHasIncorrectSpacings() {
-        // Given
-        let linkURL = "https://www.finn.no"
-        let linkText = " Besøk oss på vår nettside "
-        let linkWithIncorrectSpacings = "< a href = \"\(linkURL)\" >\(linkText)< /a >"
-        let testMessage = "Dette er en melding som inneholder en link \(linkWithIncorrectSpacings)"
-        let rangeOfLink = testMessage.range(of: linkWithIncorrectSpacings)
-
-        // When
-        let viewModel = BroadcastViewModel(with: testMessage)
-        let htmLinkInMessage = viewModel.htmlLinksInMessage.first
-
-        // Then
-        XCTAssertNotNil(htmLinkInMessage)
-        XCTAssertEqual(htmLinkInMessage?.range, rangeOfLink)
-        XCTAssertEqual(htmLinkInMessage?.text, linkText)
-        XCTAssertEqual(htmLinkInMessage?.url.absoluteString, linkURL)
-    }
-
     func testBroadcastViewModelCanReplaceHTMLLinkWithAttributedStringWithLinkAttrbutes() {
         // Given
-        let linkURL = URL(string: "https://www.finn.no")!
+        let linkURL = URL(string: "https://www.finn.no/")!
         let linkText = "Besøk oss på vår nettside"
-        let link = "<a href=\"\(linkURL.absoluteString)\">\(linkText)</a>"
+        let link = "<a href=\"\(linkURL.absoluteString)\" title=\"some title\" target=\"\">\(linkText)</a>"
         let testMessage = "Dette er en melding som inneholder en link \(link)"
 
         // When
         let viewModel = BroadcastViewModel(with: testMessage)
         let expectedResult = "Dette er en melding som inneholder en link \(linkText)"
         let result = viewModel.messageWithHTMLLinksReplacedByAttributedStrings
+        let resultRange = NSMakeRange(0, result.string.count)
         let linkTextRangeInResult = NSString(string: result.string).range(of: linkText)
-        let attributes = result.attributes(at: linkTextRangeInResult.location, longestEffectiveRange: nil, in: linkTextRangeInResult)
 
         // Then
         XCTAssertEqual(result.string, expectedResult)
-        XCTAssertNotNil(attributes[NSAttributedStringKey.link])
-        XCTAssertEqual(attributes[NSAttributedStringKey.link] as? URL, linkURL)
+
+        for location in resultRange.lowerBound ..< resultRange.upperBound {
+            let attributes = result.attributes(at: location, longestEffectiveRange: nil, in: resultRange)
+            if linkTextRangeInResult.contains(location) {
+                XCTAssertNotNil(attributes[NSAttributedStringKey.link])
+                XCTAssertEqual(attributes[NSAttributedStringKey.link] as? URL, linkURL)
+            } else {
+                XCTAssertNil(attributes[NSAttributedStringKey.link])
+            }
+        }
     }
 }
