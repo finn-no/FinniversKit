eada97c10b262d11dd5c2c66b3a65107df8b3218
Elvis Nuñez <elvisnunez@me.com>
2018-04-06 15:27:12 +0200

    Move files to a different folder

---
+++ Sources/Fullscreen/Login/Demo/LoginViewDefaultData.swift
@@ -0,0 +1,22 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import FinniversKit
+import Foundation
+
+public struct LoginViewDefaultData: LoginViewModel {
+    public let headerText = "Logg inn for å sende meldinger, lagre favoritter og søk. Du får også varsler når det skjer noe nytt!"
+    public let emailPlaceholder = "E-post"
+    public let passwordPlaceholder = "Passord"
+    public let forgotPasswordButtonTitle = "Glemt passord?"
+    public let loginButtonTitle = "Logg inn"
+    public let newUserButtonTitle = "Ny bruker?"
+    public let userTermsIntroText = "Ved å logge inn aksepterer du"
+    public let userTermsButtonTitle = "brukervilkårene."
+    public let customerServiceTitle = "kundeservice"
+    public let customerServiceIntro = "Trenger du hjelp? Ta kontakt med"
+    public let privacyPolicyTitle = "Personvernpolicy"
+
+    public init() {}
+}
---
+++ Sources/Fullscreen/Login/Demo/LoginViewDemoView.swift
@@ -0,0 +1,35 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import FinniversKit
+import UIKit
+
+public class LoginViewDemoView: UIView {
+    private lazy var loginView: LoginView = {
+        let loginView = LoginView()
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        return loginView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) { fatalError() }
+
+    private func setup() {
+        loginView.model = LoginViewDefaultData()
+
+        addSubview(loginView)
+
+        NSLayoutConstraint.activate([
+            loginView.topAnchor.constraint(equalTo: topAnchor),
+            loginView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            loginView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            loginView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+}
