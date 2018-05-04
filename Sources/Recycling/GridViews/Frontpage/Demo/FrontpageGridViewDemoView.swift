7efaac00a70dda3751a5ccdbf1bca48ba92f22d7
Elvis Nuñez <elvisnunez@me.com>
2018-05-04 04:50:40 +0200

    Reoder code

--- Sources/Recycling/GridViews/Frontpage/Demo/FrontpageGridViewDemoView.swift
+++ Sources/Recycling/GridViews/Frontpage/Demo/FrontpageGridViewDemoView.swift
@@ -30,17 +30,22 @@ public class FrontpageGridViewDemoView: UIView {
     }
 }
 
-// MARK: - AdsGridViewDelegate
+// MARK: - MarketsGridViewDataSource
 
-extension FrontpageGridViewDemoView: AdsGridViewDelegate {
-    public func adsGridView(_ adsGridView: AdsGridView, willDisplayItemAtIndex index: Int) {
+extension FrontpageGridViewDemoView: MarketsGridViewDataSource {
+    public func numberOfItems(inMarketsGridView marketsGridView: MarketsGridView) -> Int {
+        return dataSource.markets.count
     }
+    
+    public func marketsGridView(_ marketsGridView: MarketsGridView, modelAtIndex index: Int) -> MarketsGridViewModel {
+        return dataSource.markets[index]
+    }
+}
 
-    public func adsGridView(_ adsGridView: AdsGridView, didScrollInScrollView scrollView: UIScrollView) {
-    }
+// MARK: - MarketsGridViewDelegate
 
-    public func adsGridView(_ adsGridView: AdsGridView, didSelectItemAtIndex index: Int) {
-    }
+extension FrontpageGridViewDemoView: MarketsGridViewDelegate {
+    public func marketsGridView(_ marketsGridView: MarketsGridView, didSelectItemAtIndex index: Int) {}
 }
 
 // MARK: - AdsGridViewDataSource
@@ -49,17 +54,17 @@ extension FrontpageGridViewDemoView: AdsGridViewDataSource {
     public func numberOfItems(inAdsGridView adsGridView: AdsGridView) -> Int {
         return dataSource.ads.count
     }
-
+    
     public func adsGridView(_ adsGridView: AdsGridView, modelAtIndex index: Int) -> AdsGridViewModel {
         return dataSource.ads[index]
     }
-
+    
     public func adsGridView(_ adsGridView: AdsGridView, loadImageForModel model: AdsGridViewModel, imageWidth: CGFloat, completion: @escaping ((UIImage?) -> Void)) {
         guard let path = model.imagePath, let url = URL(string: path) else {
             completion(nil)
             return
         }
-
+        
         // Demo code only.
         let task = URLSession.shared.dataTask(with: url) { data, _, _ in
             DispatchQueue.main.async {
@@ -70,27 +75,17 @@ extension FrontpageGridViewDemoView: AdsGridViewDataSource {
                 }
             }
         }
-
+        
         task.resume()
     }
-
+    
     public func adsGridView(_ adsGridView: AdsGridView, cancelLoadingImageForModel model: AdsGridViewModel, imageWidth: CGFloat) {}
 }
 
-// MARK: - MarketsGridViewDelegate
+// MARK: - AdsGridViewDelegate
 
-extension FrontpageGridViewDemoView: MarketsGridViewDelegate {
-    public func marketsGridView(_ marketsGridView: MarketsGridView, didSelectItemAtIndex index: Int) {}
-}
-
-// MARK: - MarketsGridViewDataSource
-
-extension FrontpageGridViewDemoView: MarketsGridViewDataSource {
-    public func numberOfItems(inMarketsGridView marketsGridView: MarketsGridView) -> Int {
-        return dataSource.markets.count
-    }
-
-    public func marketsGridView(_ marketsGridView: MarketsGridView, modelAtIndex index: Int) -> MarketsGridViewModel {
-        return dataSource.markets[index]
-    }
+extension FrontpageGridViewDemoView: AdsGridViewDelegate {
+    public func adsGridView(_ adsGridView: AdsGridView, willDisplayItemAtIndex index: Int) {}
+    public func adsGridView(_ adsGridView: AdsGridView, didScrollInScrollView scrollView: UIScrollView) {}
+    public func adsGridView(_ adsGridView: AdsGridView, didSelectItemAtIndex index: Int) {}
 }
