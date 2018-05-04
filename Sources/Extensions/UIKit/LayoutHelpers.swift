6c867f606183379db52c1741d0ce2d6880f43d34
Elvis Nuñez <elvisnunez@me.com>
2018-05-04 11:41:47 +0200

    Add leading and trailing helper

--- Sources/Extensions/UIKit/LayoutHelpers.swift
+++ Sources/Extensions/UIKit/LayoutHelpers.swift
@@ -5,6 +5,15 @@
 import UIKit
 
 extension UIEdgeInsets {
+
+    /// Helper initializer to reduce the amount of values required to create a UIEdgeInsets, so you can do
+    /// for example UIEdgeInsets(leading: 20).
+    ///
+    /// - Parameters:
+    ///   - top:  Specify an amount to add a margin on the top anchor.
+    ///   - leading: Specify an amount to add a margin on the leading anchor.
+    ///   - bottom: Specify a negative amount to add a margin on the bottom anchor.
+    ///   - trailing: Specify a negative amount to add a margin on the trailing anchor.
     init(top: CGFloat = 0, leading: CGFloat = 0, bottom: CGFloat = 0, trailing: CGFloat = 0) {
         self.init(top: top, left: leading, bottom: bottom, right: trailing)
     }
@@ -15,9 +24,5 @@ extension UIEdgeInsets {
 
     public var trailing: CGFloat {
         return right
-    }
-
-    public static var zero: UIEdgeInsets {
-        return UIEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
     }
 }
