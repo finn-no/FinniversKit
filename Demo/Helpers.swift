449e2d32f303b9845444e032aed535ac1e0f34af
Elvis Nuñez <elvisnunez@me.com>
2018-05-10 09:59:59 +0200

    Move SplitViewController out of Helpers

--- Demo/Helpers.swift
+++ Demo/Helpers.swift
@@ -36,38 +36,3 @@ extension IndexPath {
         }
     }
 }
-
-class SplitViewController: UISplitViewController {
-    lazy var alternativeViewController: UIViewController = {
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .milk
-
-        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(didDoubleTap))
-        doubleTap.numberOfTapsRequired = 2
-        viewController.view.addGestureRecognizer(doubleTap)
-        return viewController
-    }()
-
-    convenience init(masterViewController: UIViewController) {
-        self.init(nibName: nil, bundle: nil)
-
-        viewControllers = [masterViewController, alternativeViewController]
-        setup()
-    }
-
-    convenience init(detailViewController: UIViewController) {
-        self.init(nibName: nil, bundle: nil)
-
-        viewControllers = [alternativeViewController, detailViewController]
-        setup()
-    }
-
-    func setup() {
-        preferredDisplayMode = .allVisible
-    }
-
-    @objc func didDoubleTap() {
-        IndexPath.lastSelected = nil
-        dismiss(animated: true, completion: nil)
-    }
-}
