3b642010dec59d10070df8b81ce9c0a328e4e205
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-05-23 15:13:55 +0200

    Changes the data model and its text

--- Sources/Components/Consent/ConsentTransparency/ConsentTransparencyInfoView/ConsentTransparencyInfoViewModel.swift
+++ Sources/Components/Consent/ConsentTransparency/ConsentTransparencyInfoView/ConsentTransparencyInfoViewModel.swift
@@ -6,22 +6,20 @@ import Foundation
 
 public protocol ConsentTransparencyInfoViewModel {
     var mainHeaderText: String { get }
-    var mainIntroText: String { get }
-    var usageHeaderText: String { get }
-    var usageIntro1Text: String { get }
-    var usageIntro2Text: String { get }
-    var usageBulletPointsText: NSAttributedString { get }
-    var improveHeaderText: String { get }
-    var improveIntroText: String { get }
-    var improveButtonIntroWithSettingsText: String { get }
-    var improveButtonIntroWithoutSettingsText: String { get }
-    var settingsFinnButtonTitle: String { get }
-    var privacyFinnButtonTitle: String { get }
-    var usageSchibstedHeaderText: String { get }
-    var usageSchibstedIntroText: String { get }
-    var usageSchibstedBulletPointsText: NSAttributedString { get }
-    var usageSchibstedButtonIntroWithSettingsText: String { get }
-    var usageSchibstedButtonIntroWithoutSettingsText: String { get }
-    var settingsSchibstedButtonTitle: String { get }
-    var privacySchibstedButtonTitle: String { get }
+
+    var finnHeaderText: String { get }
+    var finnIntroText: String { get }
+    var finnBulletPointsText: NSAttributedString { get }
+    var finnButtonIntroWithSettingsText: String { get }
+    var finnButtonIntroWithoutSettingsText: String { get }
+    var finnSettingsButtonTitle: String { get }
+    var finnPrivacyButtonTitle: String { get }
+
+    var schibstedHeaderText: String { get }
+    var schibstedIntroText: String { get }
+    var schibstedBulletPointsText: NSAttributedString { get }
+    var schibstedButtonIntroWithSettingsText: String { get }
+    var schibstedButtonIntroWithoutSettingsText: String { get }
+    var schibstedSettingsButtonTitle: String { get }
+    var schibstedPrivacyButtonTitle: String { get }
 }
