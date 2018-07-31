44578cd7aa52c58ffd5b53e743a8702f85fb18db
Henrik Brustad <henrik.granheim.brustad@finn.no>
2018-07-31 12:01:45 +0200

    Adds images views for radio and checkbox

---
+++ Sources/Components/SelectionBoxes/RadioButtonImageView.swift
@@ -0,0 +1,46 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+class RadioButtonImageView: AnimatedImageView {
+    private let selectedImageName = "radiobutton-select"
+    private let unselectedImageName = "radiobutton-unselected"
+
+    init() {
+        super.init(frame: .zero)
+        loadImages()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func loadImages() {
+        highlightedAnimationImages = [UIImage]()
+        animationImages = [UIImage]()
+
+        for i in 0 ..< 13 {
+            if let image = UIImage(named: "\(selectedImageName)-\(i)", in: FinniversKit.bundle, compatibleWith: nil) {
+                highlightedAnimationImages?.append(image)
+            }
+        }
+
+        for i in 0 ..< 10 {
+            if let image = UIImage(named: "\(unselectedImageName)-\(i)", in: FinniversKit.bundle, compatibleWith: nil) {
+                animationImages?.append(image)
+            }
+        }
+
+        image = animationImages?.last
+        highlightedImage = highlightedAnimationImages?.last
+
+        if let selected = highlightedAnimationImages {
+            selectedDuration = Double(selected.count) / AnimatedImageView.framesPerSecond
+        }
+        if let unselected = animationImages {
+            unselectedDuration = Double(unselected.count) / AnimatedImageView.framesPerSecond
+        }
+    }
+}
