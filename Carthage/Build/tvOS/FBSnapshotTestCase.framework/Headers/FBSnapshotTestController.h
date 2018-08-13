8f7f532579eb72956559e67fe8daf717521f93e1
Elvis Nuñez <elvisnunez@me.com>
2018-08-13 17:51:09 +0200

    Make Cartfile.private and point to release instead of master

--- Carthage/Build/tvOS/FBSnapshotTestCase.framework/Headers/FBSnapshotTestController.h
+++ Carthage/Build/tvOS/FBSnapshotTestCase.framework/Headers/FBSnapshotTestController.h
@@ -1,6 +1,5 @@
 /*
- *  Copyright (c) 2017-2018, Uber Technologies, Inc.
- *  Copyright (c) 2015-2018, Facebook, Inc.
+ *  Copyright (c) 2015-present, Facebook, Inc.
  *
  *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
