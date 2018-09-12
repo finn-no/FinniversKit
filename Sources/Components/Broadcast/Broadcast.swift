fba84f91966b7336ef932ee0a27db7545a2bc8eb
Henrik Brustad <henrik.granheim.brustad@finn.no>
2018-09-12 16:00:28 +0200

    Adds remove messages method

--- Sources/Components/Broadcast/Broadcast.swift
+++ Sources/Components/Broadcast/Broadcast.swift
@@ -22,6 +22,7 @@ public final class Broadcast: UIStackView {
     private weak var scrollView: UIScrollView?
     private var topConstraint: NSLayoutConstraint?
     private var animationDuration = 0.3
+    private var messages: Set<BroadcastMessage> = []
 
     // MARK: - Setup
 
@@ -41,6 +42,7 @@ public final class Broadcast: UIStackView {
 
     public func presentMessages(_ messages: Set<BroadcastMessage>, in view: UIView? = nil, animated: Bool = true) {
         guard superview == nil else {
+            self.messages.formUnion(messages)
             add(messages, animated: animated)
             return
         }
@@ -63,7 +65,24 @@ public final class Broadcast: UIStackView {
         topConstraint = topAnchor.constraint(equalTo: view.topAnchor)
         topConstraint?.isActive = true
 
+        self.messages.formUnion(messages)
         add(messages, animated: animated)
+    }
+
+    public func removeMessages(_ messages: Set<BroadcastMessage>) {
+        let messagesToRemove = self.messages.intersection(messages)
+
+        let items = arrangedSubviews.filter { (view) -> Bool in
+            guard let item = view as? BroadcastItem else { return false }
+            return messagesToRemove.contains(item.message)
+        }
+
+        for item in items {
+            guard let item = item as? BroadcastItem else { return }
+            remove(item, animated: false)
+        }
+
+        self.messages.subtract(messagesToRemove)
     }
 
     // Can't override scrollView delegate so have to called this method from the outside
@@ -85,11 +104,11 @@ public final class Broadcast: UIStackView {
 // MARK: - Private
 
 private extension Broadcast {
-    func remove(_ item: BroadcastItem) {
+    func remove(_ item: BroadcastItem, animated: Bool = true) {
         item.heightConstraint.constant = 0
         item.heightConstraint.isActive = true
 
-        UIView.animate(withDuration: animationDuration, animations: {
+        UIView.animate(withDuration: animated ? animationDuration : 0, animations: {
             item.alpha = 0
             self.animate(to: nil) // keep current content offset
 
