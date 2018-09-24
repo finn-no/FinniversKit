b11cf4def9babff665a128b9cf954a10e1095667
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2018-09-24 14:21:57 +0200

    Implement ‘WishList’ tableview and cell
    -

---
+++ Sources/Recycling/ListViews/Wishlist/Cell/WishListViewCell.swift
@@ -0,0 +1,220 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+public protocol WishListViewCellDataSource: class {
+    func wishListViewCell(_ wishlistListViewCell: WishListViewCell, loadImageForModel model: WishListViewModel, imageWidth: CGFloat, completion: @escaping ((UIImage?) -> Void))
+    func wishListViewCell(_ wishlistListViewCell: WishListViewCell, cancelLoadingImageForModel model: WishListViewModel, imageWidth: CGFloat)
+}
+
+public class WishListViewCell: UITableViewCell {
+    // MARK: - External properties
+    
+    /// The loading color is used to fill the image view while we load the image.
+    public var loadingColor: UIColor?
+    
+    /// A data source for the loading of the image
+    public weak var dataSource: WishListViewCellDataSource?
+    
+    // MARK: - Internal properties
+    
+    private static let layerOpacity: Float = 0.85
+    private static let cornerRadius: CGFloat = 4
+    
+    private lazy var adImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = WishListViewCell.cornerRadius
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private lazy var leftImageDetail: Label = {
+        let label = Label(style: .title4)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .licorice
+        label.textColor = .milk
+        label.textAlignment = .center
+        label.layer.opacity = WishListViewCell.layerOpacity
+        return label
+    }()
+    
+    private lazy var rightImageDetail: Label = {
+        let label = Label(style: .title4)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .mint
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var leftSubtitleDetail: Label = {
+        let label = Label(style: .detail)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.textColor = .stone
+        return label
+    }()
+    
+    private lazy var rightSubtitleDetail: Label = {
+        let label = Label(style: .detail)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.textColor = .stone
+        label.textAlignment = .right
+        return label
+    }()
+    
+    private lazy var subtitleContainer: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        view.axis = .horizontal
+        view.distribution = .fillProportionally
+        return view
+    }()
+    
+    private lazy var titleLabel: Label = {
+        let label = Label(style: .title3)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    // MARK: - Setup
+    
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    private func setupView() {
+        isAccessibilityElement = true
+        selectionStyle = .none
+        
+        addSubview(adImageView)
+        adImageView.addSubview(leftImageDetail)
+        adImageView.addSubview(rightImageDetail)
+        
+        addSubview(subtitleContainer)
+        subtitleContainer.addArrangedSubview(leftSubtitleDetail)
+        subtitleContainer.addArrangedSubview(rightSubtitleDetail)
+        
+        addSubview(titleLabel)
+        
+        backgroundColor = .milk
+
+        NSLayoutConstraint.activate([
+            adImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .mediumSpacing),
+            adImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.80),
+            adImageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.95),
+            
+            leftImageDetail.leadingAnchor.constraint(equalTo: adImageView.leadingAnchor),
+            leftImageDetail.bottomAnchor.constraint(equalTo: adImageView.bottomAnchor),
+            leftImageDetail.heightAnchor.constraint(equalTo: adImageView.heightAnchor, multiplier: 0.15),
+            leftImageDetail.widthAnchor.constraint(equalTo: adImageView.widthAnchor, multiplier: 0.25),
+            
+            rightImageDetail.trailingAnchor.constraint(equalTo: adImageView.trailingAnchor, constant: -.mediumLargeSpacing),
+            rightImageDetail.bottomAnchor.constraint(equalTo: adImageView.bottomAnchor, constant: -.mediumSpacing),
+            rightImageDetail.heightAnchor.constraint(equalTo: adImageView.heightAnchor, multiplier: 0.10),
+            rightImageDetail.widthAnchor.constraint(equalTo: adImageView.widthAnchor, multiplier: 0.20),
+            
+            subtitleContainer.leadingAnchor.constraint(equalTo: adImageView.leadingAnchor),
+            subtitleContainer.trailingAnchor.constraint(equalTo: adImageView.trailingAnchor),
+            subtitleContainer.topAnchor.constraint(equalTo: adImageView.bottomAnchor, constant: .mediumSpacing),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: subtitleContainer.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: subtitleContainer.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: subtitleContainer.bottomAnchor),
+        ])
+    }
+    
+    public override func layoutSubviews() {
+        let roundedCornerLayer = CAShapeLayer()
+        roundedCornerLayer.bounds = rightImageDetail.frame
+        roundedCornerLayer.position = rightImageDetail.center
+        roundedCornerLayer.path = UIBezierPath(roundedRect: rightImageDetail.bounds, cornerRadius: WishListViewCell.cornerRadius * 2).cgPath
+        rightImageDetail.layer.mask = roundedCornerLayer
+        
+        let roundedRightCornerLayer = CAShapeLayer()
+        roundedRightCornerLayer.bounds = leftImageDetail.frame
+        roundedRightCornerLayer.position = leftImageDetail.center
+        roundedRightCornerLayer.path = UIBezierPath(roundedRect: leftImageDetail.bounds, byRoundingCorners: [UIRectCorner.topRight],
+                                                    cornerRadii: CGSize(width: WishListViewCell.cornerRadius,
+                                                                        height: WishListViewCell.cornerRadius)).cgPath
+        leftImageDetail.layer.mask = roundedRightCornerLayer
+    }
+    
+    // MARK: - Superclass Overrides
+    
+    override public func prepareForReuse() {
+        adImageView.image = nil
+        leftImageDetail.text = ""
+        rightImageDetail.text = ""
+        leftSubtitleDetail.text = ""
+        rightSubtitleDetail.text = ""
+        titleLabel.text = ""
+        
+        if let model = model {
+            dataSource?.wishListViewCell(self, cancelLoadingImageForModel: model, imageWidth: adImageView.frame.size.width)
+        }
+    }
+    
+    // MARK: - Depedency injection
+    
+    /// The model contains data used to populate the view.
+    public var model: WishListViewModel? {
+        didSet {
+            guard let model = model else { return }
+            leftImageDetail.text = model.leftImageDetail
+            rightImageDetail.text = model.rightImageDetail
+            leftSubtitleDetail.text = model.leftSubtitleDetail
+            rightSubtitleDetail.text = model.rightSubtitleDetail
+            titleLabel.text = model.title
+            accessibilityLabel = model.accessibilityLabel
+        }
+    }
+    
+    // MARK: - Public
+    
+    /// Loads the image for the `model` if imagePath is set
+    public func loadImage() {
+        if let model = model {
+            loadImage(model: model)
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func loadImage(model: WishListViewModel) {
+        guard let dataSource = dataSource else {
+            loadingColor = .clear
+            adImageView.image = defaultImage
+            return
+        }
+        
+        adImageView.backgroundColor = loadingColor
+        
+        dataSource.wishListViewCell(self, loadImageForModel: model, imageWidth: adImageView.frame.size.width) { [weak self] image in
+            self?.adImageView.backgroundColor = .clear
+            
+            if let image = image {
+                self?.adImageView.image = image
+            } else {
+                self?.adImageView.image = self?.defaultImage
+            }
+        }
+    }
+    
+    private var defaultImage: UIImage? {
+        return UIImage(named: .noImage)
+    }
+}
---
+++ Sources/Recycling/ListViews/Wishlist/ListView/WishListView.swift
@@ -0,0 +1,134 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+public protocol WishListViewDelegate: NSObjectProtocol {
+    func wishListView(_ wishListView: WishListView, didSelectItemAtIndex index: Int)
+    func wishListView(_ wishListView: WishListView, willDisplayItemAtIndex index: Int)
+    func wishListView(_ wishListView: WishListView, didScrollInScrollView scrollView: UIScrollView)
+}
+
+public protocol WishListViewDataSource: NSObjectProtocol {
+    func numberOfItems(inWishListView wishListView: WishListView) -> Int
+    func wishListView(_ wishListView: WishListView, modelAtIndex index: Int) -> WishListViewModel
+    func wishListViewCell(_ wishlistListViewCell: WishListViewCell, loadImageForModel model: WishListViewModel, imageWidth: CGFloat, completion: @escaping ((UIImage?) -> Void))
+    func wishListViewCell(_ wishlistListViewCell: WishListViewCell, cancelLoadingImageForModel model: WishListViewModel, imageWidth: CGFloat)
+}
+
+public class WishListView: UIView {
+    public static let estimatedRowHeight: CGFloat = 300
+    
+    // MARK: - Internal properties
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .milk
+        tableView.rowHeight = WishListView.estimatedRowHeight
+        tableView.estimatedRowHeight = WishListView.estimatedRowHeight
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    // MARK: - Public properties
+    
+    public weak var delegate: WishListViewDelegate?
+    public weak var dataSource: WishListViewDataSource?
+    
+    // MARK: - Setup
+    
+    public init(delegate: WishListViewDelegate, dataSource: WishListViewDataSource) {
+        super.init(frame: .zero)
+        
+        self.delegate = delegate
+        self.dataSource = dataSource
+        
+        setupView()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    private func setupView() {
+        tableView.register(WishListViewCell.self)
+        addSubview(tableView)
+        tableView.fillInSuperview()
+    }
+    
+    // MARK: - Public
+    
+    public func reloadData() {
+        tableView.reloadData()
+    }
+    
+    public func scrollToTop(animated: Bool = true) {
+        tableView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: animated)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension WishListView: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource?.numberOfItems(inWishListView: self) ?? 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(WishListViewCell.self, for: indexPath)
+        
+        let colors: [UIColor] = [.toothPaste, .mint, .banana, .salmon]
+        let color = colors[indexPath.row % 4]
+        
+        cell.loadingColor = color
+        cell.dataSource = self
+        
+        if let model = dataSource?.wishListView(self, modelAtIndex: indexPath.row) {
+            cell.model = model
+        }
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension WishListView: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.wishListView(self, didSelectItemAtIndex: indexPath.row)
+    }
+    
+    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if let cell = cell as? WishListViewCell {
+            cell.loadImage()
+        }
+        
+        delegate?.wishListView(self, willDisplayItemAtIndex: indexPath.row)
+    }
+    
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        delegate?.wishListView(self, didScrollInScrollView: scrollView)
+    }
+}
+
+// MARK: - WishListViewCellDataSource
+
+extension WishListView: WishListViewCellDataSource {
+    public func wishListViewCell(_ wishlistListViewCell: WishListViewCell, loadImageForModel model: WishListViewModel, imageWidth: CGFloat, completion: @escaping ((UIImage?) -> Void)) {
+        dataSource?.wishListViewCell(wishlistListViewCell, loadImageForModel: model, imageWidth: imageWidth, completion: completion)
+    }
+    
+    public func wishListViewCell(_ wishlistListViewCell: WishListViewCell, cancelLoadingImageForModel model: WishListViewModel, imageWidth: CGFloat) {
+        dataSource?.wishListViewCell(wishlistListViewCell, cancelLoadingImageForModel: model, imageWidth: imageWidth)
+    }
+}
