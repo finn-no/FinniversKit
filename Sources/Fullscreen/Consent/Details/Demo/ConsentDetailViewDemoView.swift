3bfbd18a181a1c4e2195cc0dcdcae10cab092fa6
Henrik Brustad <henrik.granheim.brustad@finn.no>
2018-09-28 10:36:26 +0200

    Fix build error

--- Sources/Fullscreen/Consent/Details/Demo/ConsentDetailViewDemoView.swift
+++ Sources/Fullscreen/Consent/Details/Demo/ConsentDetailViewDemoView.swift
@@ -5,7 +5,7 @@ import FinniversKit
 
 class ConsentDetailViewDemoView: UIView {
 
-    let detailModel = ConsentDetailViewModel(heading: "Få nyhetsbrev fra FINN", definition: "FINN sender deg nyhetsbrev med for eksempel reisetips, jobbtrender, morsomme konkurranser og smarte råd til deg som kjøper og selger.\nFor å gjøre dette bruker vi kontaktinformasjonen knyttet til brukeren din på FINN.", purpose: "")
+    let detailModel = ConsentDetailViewModel(heading: "Få nyhetsbrev fra FINN", definition: "FINN sender deg nyhetsbrev med for eksempel reisetips, jobbtrender, morsomme konkurranser og smarte råd til deg som kjøper og selger.\nFor å gjøre dette bruker vi kontaktinformasjonen knyttet til brukeren din på FINN.", indexPath: IndexPath(row: 0, section: 0))
 
     lazy var consentDetailView: ConsentDetailView = {
         let view = ConsentDetailView()
