72dfedb4233219bdd7bb7688a278d1b7a16f9680
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2018-10-04 13:23:22 +0200

    Fix typo

--- Sources/Recycling/ListViews/Wishlist/ListView/WishListView.swift
+++ Sources/Recycling/ListViews/Wishlist/ListView/WishListView.swift
@@ -19,7 +19,7 @@ public protocol WishListViewDataSource: NSObjectProtocol {
 
 public class WishListView: UIView {
     public static let estimatedRowHeight: CGFloat = 300
-    public static let seperatorInset: UIEdgeInsets = UIEdgeInsetsMake(5, 0, 5, 0)
+    public static let separatorInset: UIEdgeInsets = UIEdgeInsetsMake(5, 0, 5, 0)
     
     // MARK: - Internal properties
     
@@ -32,7 +32,7 @@ public class WishListView: UIView {
         tableView.rowHeight = WishListView.estimatedRowHeight
         tableView.estimatedRowHeight = WishListView.estimatedRowHeight
         tableView.separatorStyle = .none
-        tableView.separatorInset = WishListView.seperatorInset
+        tableView.separatorInset = WishListView.separatorInset
         return tableView
     }()
     
