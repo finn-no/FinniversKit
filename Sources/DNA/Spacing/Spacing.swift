3f7f24a26ecd9430107a34d26f956b15d34469f7
Elvis Nuñez <elvisnunez@me.com>
2018-10-10 17:26:31 +0200

    Make spacing available in Objective-C

--- Sources/DNA/Spacing/Spacing.swift
+++ Sources/DNA/Spacing/Spacing.swift
@@ -6,41 +6,20 @@ import Foundation
 
 public extension CGFloat {
     /// Separation of 2 points.
-    static let verySmallSpacing: CGFloat = 2
+    static let verySmallSpacing = Spacing.verySmallSpacing()
 
     /// Separation of 4 points.
-    static let smallSpacing: CGFloat = 4
+    static let smallSpacing = Spacing.smallSpacing()
 
     /// Separation of 8 points.
-    static let mediumSpacing: CGFloat = 8
+    static let mediumSpacing = Spacing.mediumSpacing()
 
     /// Separation of 16 points.
-    static let mediumLargeSpacing: CGFloat = 16
+    static let mediumLargeSpacing = Spacing.mediumLargeSpacing()
 
     /// Separation of 32 points.
-    static let largeSpacing: CGFloat = 32
+    static let largeSpacing = Spacing.largeSpacing()
 
     /// Separation of 64 points.
-    static let veryLargeSpacing: CGFloat = 64
-}
-
-// Helper method for Objective-C compatibility.
-@objc public class Spacing: NSObject {
-    /// Separation of 2 points.
-    @objc static let verySmallSpacing = CGFloat.veryLargeSpacing
-
-    /// Separation of 4 points.
-    @objc static let smallSpacing = CGFloat.smallSpacing
-
-    /// Separation of 8 points.
-    @objc static let mediumSpacing = CGFloat.mediumSpacing
-
-    /// Separation of 16 points.
-    @objc static let mediumLargeSpacing = CGFloat.mediumLargeSpacing
-
-    /// Separation of 32 points.
-    @objc static let largeSpacing = CGFloat.largeSpacing
-
-    /// Separation of 64 points.
-    @objc static let veryLargeSpacing = CGFloat.veryLargeSpacing
+    static let veryLargeSpacing = Spacing.veryLargeSpacing()
 }
