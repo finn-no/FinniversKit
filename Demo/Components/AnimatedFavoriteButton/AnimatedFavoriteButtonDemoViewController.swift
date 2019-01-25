b0ede51ecb10bf1755d75fca91007fae62a5327d
Elvis Nuñez <elvis@Elviss-iMac.local>
2019-01-25 13:36:03 +0100

    Add label

--- Demo/Components/AnimatedFavoriteButton/AnimatedFavoriteButtonDemoViewController.swift
+++ Demo/Components/AnimatedFavoriteButton/AnimatedFavoriteButtonDemoViewController.swift
@@ -5,8 +5,24 @@
 import FinniversKit
 
 class AnimatedFavoriteButtonDemoViewController: UIViewController {
+    lazy var label: Label = {
+        let tapMeLabel = Label(style: .title2)
+        tapMeLabel.translatesAutoresizingMaskIntoConstraints = false
+        tapMeLabel.text = "Tap me ⤴️"
+        tapMeLabel.textAlignment = .center
+        return tapMeLabel
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ])
 
         view.backgroundColor = .white
 
