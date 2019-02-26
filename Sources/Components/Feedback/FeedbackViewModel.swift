e6be5b2d1994b2ac1a744d69cdf6a2bdcba04bf6
Bastian Stien <bastian.stien@gmail.com>
2019-02-26 10:41:15 +0100

    Remove message from feedbackViewModel

--- Sources/Components/Feedback/FeedbackViewModel.swift
+++ Sources/Components/Feedback/FeedbackViewModel.swift
@@ -6,13 +6,11 @@ import Foundation
 
 public struct FeedbackViewModel {
     let title: String
-    let message: String?
     let positiveButtonTitle: String?
     let negativeButtonTitle: String?
 
-    public init(title: String, message: String? = nil, positiveButtonTitle: String? = nil, negativeButtonTitle: String? = nil) {
+    public init(title: String, positiveButtonTitle: String? = nil, negativeButtonTitle: String? = nil) {
         self.title = title
-        self.message = message
         self.positiveButtonTitle = positiveButtonTitle
         self.negativeButtonTitle = negativeButtonTitle
     }
