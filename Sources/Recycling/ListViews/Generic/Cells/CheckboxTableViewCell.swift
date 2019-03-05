186721d2ea0de04824e84671b005fbe0ba61c47e
Vadym Markov <markov.vadym@gmail.com>
2019-03-05 15:50:11 +0100

    Don't set checkbox size twice

--- Sources/Recycling/ListViews/Generic/Cells/CheckboxTableViewCell.swift
+++ Sources/Recycling/ListViews/Generic/Cells/CheckboxTableViewCell.swift
@@ -57,8 +57,6 @@ open class CheckboxTableViewCell: BasicTableViewCell {
 
         NSLayoutConstraint.activate([
             stackViewToCheckboxConstraint,
-            checkbox.heightAnchor.constraint(equalToConstant: 24),
-            checkbox.widthAnchor.constraint(equalTo: checkbox.heightAnchor),
             checkbox.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: .mediumLargeSpacing),
             checkbox.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
         ])
