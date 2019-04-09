807746086f9e78c9be8e4928b386a7ece374007b
Jonas Olsson <jonas.olsson@finn.no>
2019-04-09 10:48:55 +0200

    Added some labels in demo to show styles used. Updated snapshots

--- Demo/Components/Infobox/InfoboxDemoView.swift
+++ Demo/Components/Infobox/InfoboxDemoView.swift
@@ -5,7 +5,24 @@
 import FinniversKit
 
 public class InfoboxDemoView: UIView {
+    private lazy var smallInfoboxLabel: Label = {
+        let label = Label(style: .caption)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "Style: .small(backgroundColor: .ice):"
+        label.textColor = .stone
+        return label
+    }()
     private lazy var smallInfoboxView = InfoboxView(style: .small(backgroundColor: .ice))
+    private lazy var normalInfoboxLabel: Label = {
+        let label = Label(style: .caption)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "Style: .normal(backgroundColor: .white, primaryButtonIcon: UIImage(named: .webview):"
+        label.numberOfLines = 2
+        label.textColor = .stone
+        return label
+    }()
     private lazy var normalInfoboxView = InfoboxView(style: .normal(backgroundColor: .white, primaryButtonIcon: UIImage(named: .webview)))
 
     // MARK: - Init
@@ -28,14 +45,25 @@ public class InfoboxDemoView: UIView {
         addSubview(normalInfoboxView)
         normalInfoboxView.model = InfoboxOpenBrowserData()
 
+        addSubview(smallInfoboxLabel)
+        addSubview(normalInfoboxLabel)
+
         NSLayoutConstraint.activate([
+            smallInfoboxLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            smallInfoboxLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            smallInfoboxLabel.topAnchor.constraint(equalTo: topAnchor, constant: .mediumSpacing),
+
             smallInfoboxView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
             smallInfoboxView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            smallInfoboxView.topAnchor.constraint(equalTo: topAnchor, constant: .largeSpacing),
+            smallInfoboxView.topAnchor.constraint(equalTo: smallInfoboxLabel.bottomAnchor, constant: .smallSpacing),
+
+            normalInfoboxLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            normalInfoboxLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            normalInfoboxLabel.topAnchor.constraint(equalTo: smallInfoboxView.bottomAnchor, constant: .veryLargeSpacing),
 
             normalInfoboxView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
             normalInfoboxView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            normalInfoboxView.topAnchor.constraint(equalTo: smallInfoboxView.bottomAnchor, constant: .largeSpacing),
+            normalInfoboxView.topAnchor.constraint(equalTo: normalInfoboxLabel.bottomAnchor, constant: .smallSpacing),
         ])
     }
 }
