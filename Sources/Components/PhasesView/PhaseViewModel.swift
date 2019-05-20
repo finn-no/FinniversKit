68d8bd703bb21e686d9c701aa980139734274043
Vadym Markov <markov.vadym@gmail.com>
2019-05-20 14:27:20 +0200

    Add a view and view model for phases

---
+++ Sources/Components/PhasesView/PhaseViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct PhaseViewModel {
+    public let title: String
+    public let detailText: String
+    public let isHighlighted: Bool
+
+    public init(title: String, detailText: String, isHighlighted: Bool) {
+        self.title = title
+        self.detailText = detailText
+        self.isHighlighted = isHighlighted
+    }
+}
---
+++ Sources/Components/PhasesView/PhasesView.swift
@@ -0,0 +1,37 @@
+//
+//  Copyright © 2019 FINN AS. All rights reserved.
+//
+
+import UIKit
+
+public final class PhasesView: UIView {
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(withAutoLayout: true)
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        return stackView
+    }()
+
+    // MARK: - Init
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    // MARK: - Setup
+
+    public func configure(with viewModels: [PhaseViewModel]) {
+
+    }
+
+    private func setup() {
+        stackView.fillInSuperview()
+    }
+}
