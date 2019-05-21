26ec5a36fa10772af457716817e2f6235d55ce1c
Vadym Markov <markov.vadym@gmail.com>
2019-05-21 17:02:50 +0200

    Add subviews to the contact form view

--- Sources/Fullscreen/ContactForm/ContactFormView.swift
+++ Sources/Fullscreen/ContactForm/ContactFormView.swift
@@ -5,6 +5,59 @@
 import UIKit
 
 public final class ContactFormView: UIView {
+    private lazy var scrollView = UIScrollView(withAutoLayout: true)
+    private lazy var contentView = UIView(withAutoLayout: true)
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel(withAutoLayout: true)
+        return label
+    }()
+
+    private lazy var detailTextLabel: UILabel = {
+        let label = UILabel(withAutoLayout: true)
+        return label
+    }()
+
+    private lazy var accessoryLabel: UILabel = {
+        let label = UILabel(withAutoLayout: true)
+        return label
+    }()
+
+    private lazy var nameTextField: TextField = {
+        let textField = TextField(inputType: .normal)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.delegate = self
+        return textField
+    }()
+
+    private lazy var emailTextField: TextField = {
+        let textField = TextField(inputType: .email)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.delegate = self
+        return textField
+    }()
+
+    private lazy var showPhoneCheckbox: Checkbox = {
+        let checkbox = Checkbox(withAutoLayout: true)
+        checkbox.title = "Check Box Title"
+        checkbox.delegate = self
+        return checkbox
+    }()
+
+    private lazy var phoneTextField: TextField = {
+        let textField = TextField(inputType: .normal)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.delegate = self
+        return textField
+    }()
+
+    private lazy var submitButton: Button = {
+        let button = Button(style: .callToAction)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(submit), for: .touchUpInside)
+        return button
+    }()
+
     // MARK: - Init
 
     public override init(frame: CGRect) {
@@ -20,10 +73,42 @@ public final class ContactFormView: UIView {
     // MARK: - Setup
 
     func configure(with viewModel: ContactFormViewModel) {
-        
     }
 
     private func setup() {
+
+    }
+
+    // MARK: - Action
+
+    @objc private func submit() {
+    }
+}
+
+// MARK: - TextFieldDelegate
+
+extension ContactFormView: TextFieldDelegate {
+    public func textFieldShouldReturn(_ textField: TextField) -> Bool {
+        return true
+    }
+
+    public func textFieldDidChange(_ textField: TextField) {
+        if nameTextField.text?.isEmpty == true || emailTextField.text?.isEmpty == true {
+            submitButton.isEnabled = false
+        } else {
+            submitButton.isEnabled = true
+        }
+    }
+}
+
+// MARK: - CheckboxDelegate
+
+extension ContactFormView: CheckboxDelegate {
+    public func checkbox(_ checkbox: Checkbox, didSelectItem item: CheckboxItem) {
+
+    }
+
+    public func checkbox(_ checkbox: Checkbox, didUnselectItem item: CheckboxItem) {
 
     }
 }
