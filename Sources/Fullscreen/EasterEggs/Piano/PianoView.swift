2c26ff5fa2c394fb40d359526626f5072d5cf634
Vadym Markov <markov.vadym@gmail.com>
2019-06-14 13:15:46 +0200

    Remove deprecated extensions

--- Sources/Fullscreen/EasterEggs/Piano/PianoView.swift
+++ Sources/Fullscreen/EasterEggs/Piano/PianoView.swift
@@ -136,7 +136,7 @@ public final class PianoView: UIView {
         // start engine, set up audio session
         do {
             try audioEngine.start()
-            try audioSession.swift_setCategory(.playback)
+            try audioSession.setCategory(.playback)
             try audioSession.setActive(true)
         } catch {
             print("set up failed")
