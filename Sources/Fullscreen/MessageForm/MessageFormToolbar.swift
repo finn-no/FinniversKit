a6bc57cb0037f4803c36892b202531bae6d51ba7
Joakim Stien <joakim.stien@gmail.com>
2019-06-24 14:39:41 +0200

    Added an 'add'-button

--- Sources/Fullscreen/MessageForm/MessageFormToolbar.swift
+++ Sources/Fullscreen/MessageForm/MessageFormToolbar.swift
@@ -13,12 +13,14 @@ class MessageFormToolbar: UIView {
         layout.scrollDirection = .horizontal
 
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.register(MessageFormTemplateCell.self)
+        view.register(MessageFormCustomizeCell.self)
         view.translatesAutoresizingMaskIntoConstraints = false
         view.dataSource = self
         view.delegate = self
         view.backgroundColor = .clear
         view.showsHorizontalScrollIndicator = false
-        view.contentInset = UIEdgeInsets(top: 0, leading: .smallSpacing, bottom: 0, trailing: .smallSpacing)
+        view.contentInset = UIEdgeInsets(top: 0, leading: .mediumLargeSpacing, bottom: 0, trailing: .smallSpacing)
         return view
     }()
 
@@ -53,7 +55,6 @@ class MessageFormToolbar: UIView {
         backgroundColor = UIColor(r: 208, g: 212, b: 215)
 
         addSubview(collectionView)
-        collectionView.register(MessageFormToolbarCell.self)
         collectionView.fillInSuperview(insets: UIEdgeInsets(top: toolbarTopPadding, leading: 0, bottom: 0, trailing: 0))
 
         NSLayoutConstraint.activate([
@@ -64,32 +65,61 @@ class MessageFormToolbar: UIView {
 
 extension MessageFormToolbar: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let text = viewModel.messageTemplates[safe: indexPath.row] ?? ""
-        let idealWidth = text.multiLineWidth(withConstrainedHeight: toolbarCellHeight, font: .detail, minimumWidth: 70)
-        let width = CGFloat.minimum(idealWidth + 2 * CGFloat.smallSpacing, toolbarCellMaxWidth)
-        return CGSize(width: width, height: toolbarCellHeight)
+        switch indexPath.section {
+        case 0:
+            return MessageFormCustomizeCell.size
+        case 1:
+            let text = viewModel.messageTemplates[safe: indexPath.row] ?? ""
+            let idealWidth = text.multiLineWidth(withConstrainedHeight: toolbarCellHeight, font: .detail, minimumWidth: 70)
+            let width = CGFloat.minimum(idealWidth + 2 * CGFloat.smallSpacing, toolbarCellMaxWidth)
+            return CGSize(width: width, height: toolbarCellHeight)
+        default:
+            fatalError("Unexpected section: \(indexPath.section)")
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if section == 0 {
+            return UIEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: .mediumLargeSpacing)
+        }
+
+        return UIEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
     }
 }
 
 extension MessageFormToolbar: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return 2
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.messageTemplates.count
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return viewModel.messageTemplates.count
+        default:
+            return 0
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeue(MessageFormToolbarCell.self, for: indexPath)
-        cell.configure(withText: viewModel.messageTemplates[safe: indexPath.row] ?? "")
-        return cell
+        switch indexPath.section {
+        case 0:
+            return collectionView.dequeue(MessageFormCustomizeCell.self, for: indexPath)
+        case 1:
+            let cell = collectionView.dequeue(MessageFormTemplateCell.self, for: indexPath)
+            cell.configure(withText: viewModel.messageTemplates[safe: indexPath.row] ?? "")
+            return cell
+        default:
+            fatalError("Unexpected section: \(indexPath.section)")
+        }
     }
 }
 
 // MARK: - MessageFormToolbarCell
 
-private class MessageFormToolbarCell: UICollectionViewCell {
+private class MessageFormTemplateCell: UICollectionViewCell {
 
     // MARK: - UI properties
 
@@ -114,10 +144,10 @@ private class MessageFormToolbarCell: UICollectionViewCell {
     }
 
     private func setup() {
-        layer.cornerRadius = 4
-        backgroundColor = .toothPaste
+        contentView.layer.cornerRadius = 4
+        contentView.backgroundColor = .toothPaste
 
-        addSubview(label)
+        contentView.addSubview(label)
         label.fillInSuperview(margin: .smallSpacing)
     }
 
@@ -125,5 +155,41 @@ private class MessageFormToolbarCell: UICollectionViewCell {
 
     func configure(withText text: String) {
         label.text = text
+    }
+}
+
+// MARK: - MessageFormCustomizeCell
+
+private class MessageFormCustomizeCell: UICollectionViewCell {
+
+    static let size = CGSize(width: 25, height: 25)
+
+    // MARK: - UI properties
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: .init(named: .plus))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .clear
+        return imageView
+    }()
+
+    // MARK: - Init
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    private func setup() {
+        contentView.layer.cornerRadius = MessageFormCustomizeCell.size.width / 2
+        contentView.backgroundColor = .toothPaste
+
+        contentView.addSubview(imageView)
+        imageView.fillInSuperview()
     }
 }
