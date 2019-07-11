57511833a75e8233b1edd6c4e0c32e0265e6e792
Felipe Espinoza <felipe.espinoza@schibsted.com>
2019-07-11 15:38:10 +0200

    Add `UIEdgeInsets` initializers for simetric margins.

--- Sources/Extensions/UIKit/LayoutHelpers.swift
+++ Sources/Extensions/UIKit/LayoutHelpers.swift
@@ -17,6 +17,22 @@ extension UIEdgeInsets {
         self.init(top: top, left: leading, bottom: bottom, right: trailing)
     }
 
+    /// Helper to quickly initialize the values for all sides with the same value
+    ///
+    /// - Parameter all: Specify an amount for all insets.
+    public init(all: CGFloat) {
+        self.init(top: all, leading: all, bottom: all, trailing: all)
+    }
+
+    /// Helper to quickly initialize the values for vertical and horizontal insets
+    ///
+    /// - Parameters:
+    ///   - vertical: Specify the amount for the top and bottom margins.
+    ///   - horizontal: Specify the amount for the left and right margins.
+    public init(vertical: CGFloat, horizontal: CGFloat) {
+        self.init(top: vertical, leading: horizontal, bottom: vertical, trailing: horizontal)
+    }
+
     public var leading: CGFloat {
         return left
     }
