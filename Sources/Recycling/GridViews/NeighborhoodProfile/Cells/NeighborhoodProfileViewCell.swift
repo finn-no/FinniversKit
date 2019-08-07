75a20ccd82847ab480b95ac5b48258e3181368ec
Vadym Markov <markov.vadym@gmail.com>
2019-08-07 13:34:35 +0200

    Improve card drop shadow

--- Sources/Recycling/GridViews/NeighborhoodProfile/Cells/NeighborhoodProfileViewCell.swift
+++ Sources/Recycling/GridViews/NeighborhoodProfile/Cells/NeighborhoodProfileViewCell.swift
@@ -36,8 +36,8 @@ class NeighborhoodProfileViewCell: UICollectionViewCell {
 
         contentView.layer.cornerRadius = .mediumSpacing
         contentView.layer.borderWidth = 1
-        contentView.layer.borderColor = UIColor.sardine.withAlphaComponent(0.5).cgColor
+        contentView.layer.borderColor = UIColor.sardine.withAlphaComponent(0.1).cgColor
 
-        contentView.dropShadow(color: UIColor.black.withAlphaComponent(0.5), offset: CGSize(width: 0, height: 3), radius: 3)
+        contentView.dropShadow(color: UIColor.black.withAlphaComponent(0.2), offset: CGSize(width: 0, height: 2), radius: 5)
     }
 }
