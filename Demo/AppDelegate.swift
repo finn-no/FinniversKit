465f1c3b6bb508a35e474ed87dfb5d7a18bbde44
Elvis <3lvis@users.noreply.github.com>
2019-08-12 13:28:50 +0200

    Dark Mode iOS 13 in Demo app (#512)
    * Bring iOS 13 dark mode
    
    * Add dark mode to navigation controller
    
    * Add check for moon button
    
    * Add checks to make it build in Xcode 10.3

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -7,66 +7,18 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    private var hairlineView: UIView?
+
+    lazy var navigationController: NavigationController = {
+        let navigationController = NavigationController(rootViewController: DemoViewsTableViewController())
+        return navigationController
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-
-        let navigationController = UINavigationController(rootViewController: DemoViewsTableViewController())
-        navigationController.navigationBar.isTranslucent = false
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
-        updateColors(animated: false)
-        NotificationCenter.default.addObserver(self, selector: #selector(userInterfaceStyleDidChange(_:)), name: .didChangeUserInterfaceStyle, object: nil)
-
         return true
-    }
-}
-
-private extension AppDelegate {
-    @objc func userInterfaceStyleDidChange(_ userInterfaceStyle: UserInterfaceStyle) {
-        updateColors(animated: true)
-    }
-
-    func updateColors(animated: Bool) {
-        UIView.animate(withDuration: animated ? 0.3 : 0) {
-            let separatorColor: UIColor
-            let barTintColor: UIColor
-            let tintColor: UIColor
-            let barStyle: UIBarStyle
-            switch State.currentUserInterfaceStyle {
-            case .light:
-                separatorColor = .sardine
-                barTintColor = .milk
-                tintColor = .primaryBlue
-                barStyle = .default
-            case .dark:
-                separatorColor = .midnightSectionSeparator
-                barTintColor = .midnightBackground
-                tintColor = .secondaryBlue
-                barStyle = .black
-            }
-
-            if let navigationController = self.window?.rootViewController as? UINavigationController {
-                self.setBottomBorderColor(navigationBar: navigationController.navigationBar, color: separatorColor, height: 0.5)
-                navigationController.navigationBar.barTintColor = barTintColor
-                navigationController.navigationBar.tintColor = tintColor
-                navigationController.navigationBar.barStyle = barStyle
-                navigationController.navigationBar.layoutIfNeeded()
-            }
-        }
-    }
-
-    func setBottomBorderColor(navigationBar: UINavigationBar, color: UIColor, height: CGFloat) {
-        if hairlineView == nil {
-            let bottomBorderRect = CGRect(x: 0, y: navigationBar.frame.height, width: navigationBar.frame.width, height: height)
-            let view = UIView(frame: bottomBorderRect)
-            navigationBar.addSubview(view)
-            hairlineView = view
-        }
-
-        hairlineView?.backgroundColor = color
     }
 }
 
