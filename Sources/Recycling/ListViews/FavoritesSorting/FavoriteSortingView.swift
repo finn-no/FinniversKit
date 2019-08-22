4d7dc791eb119834de720a7db720e5f86b620695
Vadym Markov <markov.vadym@gmail.com>
2019-08-22 13:28:08 +0200

    Implement favorite sorting view

---
+++ Sources/Recycling/ListViews/FavoritesSorting/FavoriteSortingView.swift
@@ -0,0 +1,104 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+public protocol FavoriteSortingViewDelegate: AnyObject {
+    func favoriteSortingView(_ view: FavoriteSortingView, didSelectOption option: FavoriteSortOption)
+}
+
+public final class FavoriteSortingView: UIView {
+    public static let rowHeight: CGFloat = 48.0
+    public static let totalHeight = rowHeight * CGFloat(FavoriteSortOption.allCases.count)
+
+    // MARK: - Public properties
+
+    public weak var delegate: FavoriteSortingViewDelegate?
+
+    // MARK: - Private properties
+
+    private let viewModel: FavoriteSortingViewModel
+    private let options = FavoriteSortOption.allCases
+    private var selectedSortOption: FavoriteSortOption
+
+    private lazy var tableView: UITableView = {
+        let tableView = TableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .milk
+        tableView.rowHeight = FavoriteSortingView.rowHeight
+        tableView.estimatedRowHeight = FavoriteSortingView.rowHeight
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.tableFooterView = UIView()
+        tableView.isScrollEnabled = false
+        tableView.separatorColor = .clear
+        tableView.register(FavoriteSortOptionCell.self)
+        return tableView
+    }()
+
+    // MARK: - Init
+
+    public init(viewModel: FavoriteSortingViewModel, selectedSortOption: FavoriteSortOption) {
+        self.viewModel = viewModel
+        self.selectedSortOption = selectedSortOption
+        super.init(frame: .zero)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup
+
+    private func setup() {
+        addSubview(tableView)
+        tableView.fillInSuperview()
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension FavoriteSortingView: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let option = options[indexPath.row]
+        let cell = tableView.dequeue(FavoriteSortOptionCell.self, for: indexPath)
+        cell.configure(withTitle: viewModel.title(for: option), icon: viewModel.icon(for: option))
+        cell.isCheckmarkHidden = option == selectedSortOption
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension FavoriteSortingView: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        selectedSortOption = options[indexPath.row]
+        tableView.reloadData()
+        delegate?.favoriteSortingView(self, didSelectOption: selectedSortOption)
+    }
+}
+
+// MARK: - Private types
+
+private final class TableView: UITableView {
+    override var intrinsicContentSize: CGSize {
+        return contentSize
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if bounds.size != intrinsicContentSize {
+            invalidateIntrinsicContentSize()
+        }
+    }
+}
---
+++ Sources/Recycling/ListViews/FavoritesSorting/ViewModel/FavoriteSortingViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct FavoriteSortingViewModel {
+    public let lastAddedText: String
+    public let statusText: String
+    public let lastUpdatedText: String
+    public let distanceText: String
+
+    // MARK: - Init
+
+    public init(
+        lastAddedText: String,
+        statusText: String,
+        lastUpdatedText: String,
+        distanceText: String
+    ) {
+        self.lastAddedText = lastAddedText
+        self.statusText = statusText
+        self.lastUpdatedText = lastUpdatedText
+        self.distanceText = distanceText
+    }
+
+    // MARK: - Content
+
+    func title(for option: FavoriteSortOption) -> String {
+        switch option {
+        case .lastAdded:
+            return lastAddedText
+        case .status:
+            return statusText
+        case .lastUpdated:
+            return lastUpdatedText
+        case .distance:
+            return distanceText
+        }
+    }
+
+    func icon(for option: FavoriteSortOption) -> UIImage? {
+        switch option {
+        case .lastAdded:
+            return UIImage(named: .favoritesSortLastAdded)
+        case .status:
+            return UIImage(named: .favoritesSortAdStatus)
+        case .lastUpdated:
+            return UIImage(named: .republish)
+        case .distance:
+            return UIImage(named: .pin)
+        }
+    }
+}
