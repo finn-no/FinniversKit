ef0cbfb48d1067fdce1c5ca76b48aa930a735165
Jonas Olsson <osanoj@users.noreply.github.com>
2019-08-29 15:16:27 +0200

    Feature: AddressView with polygon overlay support (#545)
    * Extended `AddressView` with possibility to show a polygon overlay instead of annotation or circle
    
    * Support multiple polygons per postal area
    
    * Make map overlay fill colors a bit more transparent so it is easier to see map underneath
    
    * Refactored to make demo code more readable. Also removed a bunch of public access modifiers

--- Demo/Fullscreen/AddressView/AddressViewDemoView.swift
+++ Demo/Fullscreen/AddressView/AddressViewDemoView.swift
@@ -5,7 +5,7 @@
 import FinniversKit
 import MapKit
 
-enum MapTypes: Int, CaseIterable {
+private enum MapTypes: Int, CaseIterable {
     case standard
     case satellite
     case hybrid
@@ -19,24 +19,24 @@ enum MapTypes: Int, CaseIterable {
     }
 }
 
-public struct AddressViewData: AddressViewModel {
-    public var mapTypes: [String] {
+private struct AddressViewData: AddressViewModel {
+    var mapTypes: [String] {
         return MapTypes.allCases.map { $0.value }
     }
-    public var title: String
-    public var subtitle: String
-    public var copyButtonTitle: String
-    public var selectedMapType = 0
-    public var getDirectionsButtonTitle = "Åpne veibeskrivelse"
+    var title: String
+    var subtitle: String
+    var copyButtonTitle: String
+    var selectedMapType = 0
+    var getDirectionsButtonTitle = "Åpne veibeskrivelse"
 
-    public init(title: String, subtitle: String, copyButtonTitle: String) {
+    init(title: String, subtitle: String, copyButtonTitle: String) {
         self.title = title
         self.subtitle = subtitle
         self.copyButtonTitle = copyButtonTitle
     }
 }
 
-public class AddressViewDemoView: UIView, Tweakable {
+class AddressViewDemoView: UIView, Tweakable {
     lazy var tweakingOptions: [TweakingOption] = {
         var options = [TweakingOption]()
 
@@ -54,6 +54,19 @@ public class AddressViewDemoView: UIView, Tweakable {
             self.addressView.centerMap(location: location, regionDistance: 1200, animated: false)
         })
 
+        let polygonDemoData = [
+            PolygonDemoData(title: "0010", subtitle: "OSLO", areas: [PolygonDemoArea.area0010]),
+            PolygonDemoData(title: "Multi polygons", subtitle: "OSLO", areas: [PolygonDemoArea.area0010, PolygonDemoArea(coordinates: [[10.74, 59.92], [10.75, 59.92], [10.74, 59.91]])])
+        ]
+        polygonDemoData.forEach({
+            let postalCode = $0.title
+            let postalAreaName = $0.subtitle
+            let areas = $0.areas
+            options.append(TweakingOption(title: "Postalcode shape (\(postalCode))") {
+                self.addressView.model = AddressViewData(title: postalCode, subtitle: postalAreaName, copyButtonTitle: "Kopier postnummer")
+                self.addressView.configurePolygons(areas.compactMap({ $0?.coordinates }))
+            })
+        })
         return options
     }()
 
@@ -69,7 +82,7 @@ public class AddressViewDemoView: UIView, Tweakable {
         setup()
     }
 
-    public required init?(coder aDecoder: NSCoder) { fatalError() }
+    required init?(coder aDecoder: NSCoder) { fatalError() }
 
     private func setup() {
         addSubview(addressView)
@@ -91,19 +104,20 @@ public class AddressViewDemoView: UIView, Tweakable {
 }
 
 extension AddressViewDemoView: AddressViewDelegate {
-    public func addressViewDidSelectCopyButton(_ addressView: AddressView) {
+    func addressViewDidSelectCopyButton(_ addressView: AddressView) {
         print("addressViewDidSelectCopyButton")
     }
 
-    public func addressViewDidSelectGetDirectionsButton(_ addressView: AddressView, sender: UIView) {
+    func addressViewDidSelectGetDirectionsButton(_ addressView: AddressView, sender: UIView) {
         print("addressViewDidSelectGetDirectionsButton")
     }
 
-    public func addressViewDidSelectCenterMapButton(_ addressView: AddressView) {
+    func addressViewDidSelectCenterMapButton(_ addressView: AddressView) {
         print("addressViewDidSelectCenterMapButton")
+        addressView.makePolygonOverlayVisible()
     }
 
-    public func addressView(_ addressView: AddressView, didSelectMapTypeAtIndex index: Int) {
+    func addressView(_ addressView: AddressView, didSelectMapTypeAtIndex index: Int) {
         guard let mapType = MapTypes(rawValue: index) else { return }
         switch mapType {
         case .standard:
@@ -114,4 +128,34 @@ extension AddressViewDemoView: AddressViewDelegate {
             addressView.changeMapType(.hybrid)
         }
     }
+}
+
+private struct PolygonDemoData {
+    let title: String
+    let subtitle: String
+    let areas: [PolygonDemoArea?]
+}
+
+private struct PolygonDemoArea {
+    private enum AddressViewDemoError: Error {
+        case decodingError
+    }
+
+    let coordinates: [CLLocationCoordinate2D]
+
+    init?(coordinates: [[CLLocationDegrees]]) {
+        guard let convertedCoordinates = try? coordinates.map({ (degreesArray) -> CLLocationCoordinate2D in
+            guard degreesArray.count == 2, let lat = degreesArray.last, let lon = degreesArray.first else {
+                throw AddressViewDemoError.decodingError
+            }
+            return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        }) else {
+            return nil
+        }
+        self.coordinates = convertedCoordinates
+    }
+
+    static let area0010 = PolygonDemoArea(coordinates: [
+        [10.7244, 59.9181], [10.7252, 59.9185], [10.7264, 59.9192], [10.727099999999998, 59.919799999999995], [10.7274, 59.92], [10.727899999999998, 59.9202], [10.728499999999999, 59.9199], [10.7294, 59.919599999999996], [10.730299999999998, 59.9192], [10.729899999999999, 59.9184], [10.731199999999998, 59.917399999999994], [10.732099999999999, 59.9173], [10.7326, 59.917199999999994], [10.734699999999998, 59.9165], [10.734299999999998, 59.9161], [10.733899999999998, 59.9159], [10.7334, 59.9156], [10.7327, 59.9154], [10.730899999999998, 59.9153], [10.730199999999998, 59.9153], [10.7294, 59.9154], [10.728399999999999, 59.9154], [10.7266, 59.9154], [10.7252, 59.9155], [10.723999999999998, 59.9154], [10.723299999999998, 59.9154], [10.7228, 59.9154], [10.7227, 59.9157], [10.7228, 59.9161], [10.7229, 59.9163], [10.722999999999999, 59.916799999999995], [10.722999999999999, 59.916999999999994], [10.7234, 59.9173], [10.7244, 59.9181]
+        ])
 }
