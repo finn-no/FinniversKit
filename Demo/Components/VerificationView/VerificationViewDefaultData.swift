b1f9fa618377bc5c7096cc78aa13dcacee6991e9
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2019-09-09 15:13:46 +0200

    Fix typo in defaultData model

--- Demo/Components/VerificationView/VerificationViewDefaultData.swift
+++ Demo/Components/VerificationView/VerificationViewDefaultData.swift
@@ -6,6 +6,6 @@ import FinniversKit
 
 public struct VerificationViewDefaultData: VerificationViewModel {
     public var title = "Krever identifisering med BankID"
-    public var description = "Ved å identifisere brukere med BankID er det kun mulig å legge ut annonser fra én konto.\n\nDet er mange som trenger hjelp i julen, og på denne måten sikrer vi at det blir rettferdig og flest mulig får hjelp."
+    public var description = "Ved å identifisere brukeren med BankID er det kun mulig å legge ut annonser fra én konto.\n\nDet er mange som trenger hjelp i julen, og på denne måten sikrer vi at det blir rettferdig og flest mulig får hjelp."
     public var buttonTitle = "Gå videre"
 }
