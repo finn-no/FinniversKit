61630a8f9b3d9d0f7df9e8603034625c8b4120d1
Vadym Markov <markov.vadym@gmail.com>
2019-09-09 15:03:19 +0200

    Simplify favorite sorting sheet

--- Sources/Fullscreen/FavoriteSortingSheet/FavoriteSortingSheet.swift
+++ Sources/Fullscreen/FavoriteSortingSheet/FavoriteSortingSheet.swift
@@ -10,14 +10,22 @@ public protocol FavoriteSortingSheetDelegate: AnyObject {
 
 public final class FavoriteSortingSheet: BottomSheet {
     public weak var sortingDelegate: FavoriteSortingSheetDelegate?
-    private weak var viewController: SortingViewController?
+
+    private let selectedSortOption: FavoriteSortOption
+    private let viewModel: FavoriteSortingViewModel
+    private lazy var sortingView: FavoriteSortingView = {
+        let view = FavoriteSortingView(viewModel: viewModel, selectedSortOption: selectedSortOption)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.delegate = self
+        return view
+    }()
 
     // MARK: - Init
 
     public required init(viewModel: FavoriteSortingViewModel, selectedSortOption: FavoriteSortOption) {
-        let viewController = SortingViewController(viewModel: viewModel, selectedSortOption: selectedSortOption)
-        super.init(rootViewController: viewController, height: .default)
-        self.viewController = viewController
+        self.selectedSortOption = selectedSortOption
+        self.viewModel = viewModel
+        super.init(rootViewController: UIViewController(), height: .default)
     }
 
     public required init?(coder aDecoder: NSCoder) {
@@ -28,7 +36,8 @@ public final class FavoriteSortingSheet: BottomSheet {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        viewController?.sortingView.delegate = self
+        rootViewController.view.addSubview(sortingView)
+        sortingView.fillInSuperview()
     }
 }
 
@@ -37,35 +46,6 @@ public final class FavoriteSortingSheet: BottomSheet {
 extension FavoriteSortingSheet: FavoriteSortingViewDelegate {
     public func favoriteSortingView(_ view: FavoriteSortingView, didSelectSortOption option: FavoriteSortOption) {
         sortingDelegate?.favoriteSortingSheet(self, didSelectSortOption: option)
-    }
-}
-
-// MARK: - Private types
-
-private final class SortingViewController: UIViewController {
-    private let viewModel: FavoriteSortingViewModel
-    private let selectedSortOption: FavoriteSortOption
-
-    private(set) lazy var sortingView: FavoriteSortingView = {
-        let view = FavoriteSortingView(viewModel: viewModel, selectedSortOption: selectedSortOption)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-    init(viewModel: FavoriteSortingViewModel, selectedSortOption: FavoriteSortOption) {
-        self.viewModel = viewModel
-        self.selectedSortOption = selectedSortOption
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(sortingView)
-        sortingView.fillInSuperview()
     }
 }
 
