312beb26b8c552e8fb5b3668145cbfc87a7c6727
Vadym Markov <markov.vadym@gmail.com>
2019-09-09 11:08:32 +0200

    Favorite actions: remove extra label (#561)
    * Favorite action view: remove accessory label
    
    * Update snapshot tests

--- Demo/Recycling/ListViews/FavoriteActions/FavoriteActionViewModel+Demo.swift
+++ Demo/Recycling/ListViews/FavoriteActions/FavoriteActionViewModel+Demo.swift
@@ -7,8 +7,7 @@ import FinniversKit
 extension FavoriteActionViewModel {
     static let `default` = FavoriteActionViewModel(
         headerImage: createImage(),
-        headerDetailText: "Ærverdig herskapelig villa, med praktfull beliggende strandtomt (6500 kvm), helt i vannkanten. Perle, finnes ikke maken!",
-        headerAccessoryText: "340m² • 15 000 000,-",
+        headerTitle: "Ærverdig herskapelig villa, med praktfull beliggende strandtomt (6500 kvm), helt i vannkanten. Perle, finnes ikke maken!",
         addNoteText: "Legg til notat",
         deleteText: "Slett favoritten fra listen"
     )
