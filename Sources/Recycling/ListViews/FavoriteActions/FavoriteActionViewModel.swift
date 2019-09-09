8278f966c66b6d53bbe5cbeb7fdbea4b36e27282
Vadym Markov <markov.vadym@gmail.com>
2019-09-09 10:34:03 +0200

    Favorite action view: remove accessory label

--- Sources/Recycling/ListViews/FavoriteActions/FavoriteActionViewModel.swift
+++ Sources/Recycling/ListViews/FavoriteActions/FavoriteActionViewModel.swift
@@ -4,21 +4,18 @@
 
 public struct FavoriteActionViewModel {
     public let headerImage: UIImage?
-    public let headerDetailText: String
-    public let headerAccessoryText: String
+    public let headerTitle: String
     public let addNoteText: String
     public let deleteText: String
 
     public init(
         headerImage: UIImage?,
-        headerDetailText: String,
-        headerAccessoryText: String,
+        headerTitle: String,
         addNoteText: String,
         deleteText: String
     ) {
         self.headerImage = headerImage
-        self.headerDetailText = headerDetailText
-        self.headerAccessoryText = headerAccessoryText
+        self.headerTitle = headerTitle
         self.addNoteText = addNoteText
         self.deleteText = deleteText
     }
--- Sources/Recycling/ListViews/FavoriteActions/Views/FavoriteActionHeaderView.swift
+++ Sources/Recycling/ListViews/FavoriteActions/Views/FavoriteActionHeaderView.swift
@@ -15,19 +15,9 @@ final class FavoriteActionHeaderView: UIView {
         return imageView
     }()
 
-    private lazy var detailTextLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         let label = UILabel(withAutoLayout: true)
-        label.font = FavoriteActionHeaderView.detailTextLabelFont
-        label.textColor = .licorice
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.setContentHuggingPriority(.required, for: .vertical)
-        return label
-    }()
-
-    private lazy var accessoryLabel: UILabel = {
-        let label = UILabel(withAutoLayout: true)
-        label.font = FavoriteActionHeaderView.accessoryLabelFont
+        label.font = FavoriteActionHeaderView.titleLabelFont
         label.textColor = .licorice
         label.textAlignment = .center
         label.numberOfLines = 0
@@ -55,17 +45,15 @@ final class FavoriteActionHeaderView: UIView {
 
     // MARK: - Setup
 
-    func configure(withImage image: UIImage?, detailText: String, accessoryText: String) {
+    func configure(withImage image: UIImage?, title: String) {
         imageView.image = image
-        detailTextLabel.text = detailText
-        accessoryLabel.text = accessoryText
+        titleLabel.text = title
     }
 
     private func setup() {
         addSubview(blurView)
         addSubview(imageView)
-        addSubview(detailTextLabel)
-        addSubview(accessoryLabel)
+        addSubview(titleLabel)
         addSubview(hairlineView)
 
         layoutMargins = FavoriteActionHeaderView.layoutMargins
@@ -78,15 +66,11 @@ final class FavoriteActionHeaderView: UIView {
             imageView.widthAnchor.constraint(equalToConstant: FavoriteActionHeaderView.imageViewSize),
             imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
 
-            detailTextLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: .detailTextLabelTopSpacing),
-            detailTextLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
-            detailTextLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: .titleLabelTopSpacing),
+            titleLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
 
-            accessoryLabel.topAnchor.constraint(equalTo: detailTextLabel.bottomAnchor, constant: .accessoryLabelTopSpacing),
-            accessoryLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
-            accessoryLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
-
-            hairlineView.topAnchor.constraint(equalTo: accessoryLabel.bottomAnchor, constant: .hairlineTopSpacing),
+            hairlineView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: .hairlineTopSpacing),
             hairlineView.leadingAnchor.constraint(equalTo: leadingAnchor),
             hairlineView.trailingAnchor.constraint(equalTo: trailingAnchor),
             hairlineView.heightAnchor.constraint(equalToConstant: .hairlineHeight),
@@ -106,16 +90,14 @@ extension FavoriteActionHeaderView {
     )
 
     private static let imageViewSize: CGFloat = 56
-    private static let detailTextLabelFont = UIFont.body
-    private static let accessoryLabelFont = UIFont.bodyStrong
+    private static let titleLabelFont = UIFont.body
 
-    static func height(forDetailText detailText: String, accessoryText: String, width: CGFloat) -> CGFloat {
+    static func height(forTitle title: String, width: CGFloat) -> CGFloat {
         let width = width - layoutMargins.left - layoutMargins.right
         var height = layoutMargins.top
 
         height += imageViewSize
-        height += .detailTextLabelTopSpacing + detailText.height(withConstrainedWidth: width, font: detailTextLabelFont)
-        height += .accessoryLabelTopSpacing + accessoryText.height(withConstrainedWidth: width, font: accessoryLabelFont)
+        height += .titleLabelTopSpacing + title.height(withConstrainedWidth: width, font: titleLabelFont)
         height += .hairlineTopSpacing + .hairlineHeight
         height += layoutMargins.bottom
 
@@ -126,8 +108,7 @@ extension FavoriteActionHeaderView {
 // MARK: - Private extensions
 
 private extension CGFloat {
-    static let detailTextLabelTopSpacing: CGFloat = 10
-    static let accessoryLabelTopSpacing: CGFloat = 14
+    static let titleLabelTopSpacing: CGFloat = 10
     static let hairlineTopSpacing: CGFloat = 18
     static let hairlineHeight = 1 / UIScreen.main.scale
 }
