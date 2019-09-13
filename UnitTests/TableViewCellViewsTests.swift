3f9bf25608e7e31da0228a01554c5beb04f93a4b
trularse <truls.benjamin.larsen@finn.no>
2019-09-13 10:30:58 +0200

    Added snapshot-test for RadioButtonCell

--- UnitTests/TableViewCellViewsTests.swift
+++ UnitTests/TableViewCellViewsTests.swift
@@ -43,6 +43,10 @@ class TableViewCellsViewTests: FBSnapshotTestCase {
         snapshot(.checkboxSubtitleCell)
     }
 
+    func testRadioButtonCell() {
+        snapshot(.radioButtonCell)
+    }
+
     func testHeartSubtitleCell() {
         snapshot(.heartSubtitleCell)
     }
