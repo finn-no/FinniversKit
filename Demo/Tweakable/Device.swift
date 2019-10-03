1d71946f06dff20a84b17085787fd96324ebbaca
Elvis Nuñez <elvisnunez@me.com>
2019-10-03 16:19:56 +0200

    Refactor

--- Demo/Tweakable/Device.swift
+++ Demo/Tweakable/Device.swift
@@ -22,78 +22,60 @@ enum Device: String {
 
     func dimensions(orientation: Orientation) -> (traits: UITraitCollection, frame: CGRect) {
         let size: CGSize
-        let traits: UITraitCollection
+        let horizontalSizeClass: UIUserInterfaceSizeClass
+        let verticalSizeClass: UIUserInterfaceSizeClass
+        let userInterfaceIdiom: UIUserInterfaceIdiom
         switch (self, orientation) {
         case (.phone4inch, .portrait):
             size = .init(width: 320, height: 568)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .compact),
-                .init(verticalSizeClass: .regular),
-                .init(userInterfaceIdiom: .phone)
-                ])
+            horizontalSizeClass = .compact
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .phone
         case (.phone4inch, .landscape):
             size = .init(width: 568, height: 320)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .compact),
-                .init(verticalSizeClass: .compact),
-                .init(userInterfaceIdiom: .phone)
-                ])
+            horizontalSizeClass = .compact
+            verticalSizeClass = .compact
+            userInterfaceIdiom = .phone
         case (.phone4_7inch, .portrait):
             size = .init(width: 375, height: 667)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .compact),
-                .init(verticalSizeClass: .regular),
-                .init(userInterfaceIdiom: .phone)
-                ])
+            horizontalSizeClass = .compact
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .phone
         case (.phone4_7inch, .landscape):
             size = .init(width: 667, height: 375)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .compact),
-                .init(verticalSizeClass: .compact),
-                .init(userInterfaceIdiom: .phone)
-                ])
+            horizontalSizeClass = .compact
+            verticalSizeClass = .compact
+            userInterfaceIdiom = .phone
         case (.phone5_5inch, .portrait):
             size = .init(width: 414, height: 736)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .compact),
-                .init(verticalSizeClass: .regular),
-                .init(userInterfaceIdiom: .phone)
-                ])
+            horizontalSizeClass = .compact
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .phone
         case (.phone5_5inch, .landscape):
             size = .init(width: 736, height: 414)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .regular),
-                .init(verticalSizeClass: .compact),
-                .init(userInterfaceIdiom: .phone)
-                ])
+            horizontalSizeClass = .regular
+            verticalSizeClass = .compact
+            userInterfaceIdiom = .phone
         case (.pad10_5, .portrait):
             size = .init(width: 768, height: 1_024)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .regular),
-                .init(verticalSizeClass: .regular),
-                .init(userInterfaceIdiom: .pad)
-                ])
+            horizontalSizeClass = .regular
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .pad
         case (.pad10_5, .landscape):
             size = .init(width: 1_024, height: 768)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .regular),
-                .init(verticalSizeClass: .regular),
-                .init(userInterfaceIdiom: .pad)
-                ])
+            horizontalSizeClass = .regular
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .pad
         case (.pad12_9inch, .portrait):
             size = .init(width: 1_024, height: 1_366)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .regular),
-                .init(verticalSizeClass: .regular),
-                .init(userInterfaceIdiom: .pad)
-                ])
+            horizontalSizeClass = .regular
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .pad
         case (.pad12_9inch, .landscape):
             size = .init(width: 1_366, height: 1_024)
-            traits = .init(traitsFrom: [
-                .init(horizontalSizeClass: .regular),
-                .init(verticalSizeClass: .regular),
-                .init(userInterfaceIdiom: .pad)
-                ])
+            horizontalSizeClass = .regular
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .pad
         }
 
         // swiftlint:disable:next identifier_name
@@ -101,6 +83,12 @@ enum Device: String {
 
         // swiftlint:disable:next identifier_name
         let y: CGFloat = (UIScreen.main.bounds.height - size.height) / 2
+
+        let traits: UITraitCollection = .init(traitsFrom: [
+            .init(horizontalSizeClass: horizontalSizeClass),
+            .init(verticalSizeClass: verticalSizeClass),
+            .init(userInterfaceIdiom: userInterfaceIdiom)
+            ])
 
         return (traits, CGRect(x: x, y: y, width: size.width, height: size.height))
     }
