da1bc0d5884ac53be4a09d87feefbf1a4a870841
Bastian Stien <bastian.stien@gmail.com>
2019-10-03 10:55:56 +0200

    New favorites: Section header detail for search (#640)
    * Add detailLabel to FavoriteAdsSectionHeaderView
    
    * Override prepareForReuse
    
    * Add delegate method and populate sectionHeader
    
    * Update demo code
    
    * Change detail color to .licorice
    
    * Arbitrary change to force rebuild in GitHub
    
    * Undo last commit

--- Sources/Fullscreen/FavoriteAdsList/Subviews/FavoriteAdsSectionHeaderView.swift
+++ Sources/Fullscreen/FavoriteAdsList/Subviews/FavoriteAdsSectionHeaderView.swift
@@ -15,6 +15,14 @@ class FavoriteAdsSectionHeaderView: UITableViewHeaderFooterView {
         return label
     }()
 
+    private lazy var detailLabel: UILabel = {
+        let label = UILabel(withAutoLayout: true)
+        label.font = .detailStrong
+        label.textColor = .licorice
+        label.textAlignment = .right
+        return label
+    }()
+
     // MARK: - Init
 
     override init(reuseIdentifier: String?) {
@@ -24,22 +32,37 @@ class FavoriteAdsSectionHeaderView: UITableViewHeaderFooterView {
 
     required init?(coder aDecoder: NSCoder) { fatalError() }
 
+    // MARK: - Overrides
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+        detailLabel.text = nil
+    }
+
     // MARK: - Setup
 
     private func setup() {
         contentView.backgroundColor = .bgTertiary
         contentView.addSubview(titleLabel)
+        contentView.addSubview(detailLabel)
 
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: .mediumSpacing),
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: .mediumLargeSpacing),
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -.mediumSpacing)
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -.mediumSpacing),
+            titleLabel.trailingAnchor.constraint(equalTo: detailLabel.leadingAnchor, constant: -.mediumSpacing),
+
+            detailLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: .mediumSpacing),
+            detailLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -.mediumLargeSpacing),
+            detailLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -.mediumSpacing)
         ])
     }
 
     // MARK: - Internal methods
 
-    func configure(title: String) {
+    func configure(title: String, detail: String? = nil) {
         titleLabel.text = title.uppercased()
+        detailLabel.text = detail?.uppercased()
     }
 }
