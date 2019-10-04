4b90bb556b83d3d74020137232a7c981804d2771
Elvis Nuñez <elvisnunez@me.com>
2019-10-04 15:08:54 +0200

    Better iPad support

--- Demo/Tweakable/Device.swift
+++ Demo/Tweakable/Device.swift
@@ -1,10 +1,5 @@
 import UIKit
 
-enum Orientation {
-    case portrait
-    case landscape
-}
-
 enum Device: String {
     case phone4inch = "iPhone 5S (4-inch)"
 
@@ -17,11 +12,19 @@ enum Device: String {
     // swiftlint:disable:next identifier_name
     case phone5_8inch = "iPhone X (5.8-inch)"
 
-    // swiftlint:disable:next identifier_name
-    case pad10_5 = "iPad (10.5-inch)"
+    case padPortraitOneThird = "iPad Portrait 1/3"
 
-    // swiftlint:disable:next identifier_name
-    case pad12_9inch = "iPad (12.9-inch)"
+    case padPortraitTwoThirds = "iPad Portrait 2/3"
+
+    case padPortraitFull = "iPad Portrait Full"
+
+    case padLandscapeOneThird = "iPad Landscape 1/3"
+
+    case padLandscapeOneHalf = "iPad Landscape 1/2"
+
+    case padLandscapeTwoThirds = "iPad Landscape 2/3"
+
+    case padLandscapeFull = "iPad Landscape Full"
 
     // swiftlint:disable:next large_tuple
     func dimensions(currentTraitCollection: UITraitCollection) -> (traits: UITraitCollection, frame: CGRect, autoresizingMask: UIView.AutoresizingMask) {
@@ -30,20 +33,6 @@ enum Device: String {
         let verticalSizeClass: UIUserInterfaceSizeClass
         let userInterfaceIdiom: UIUserInterfaceIdiom
         let autoresizingMask: UIView.AutoresizingMask
-        let isPortrait = UIDevice.current.orientation == .portrait || UIDevice.current.orientation == .portraitUpsideDown
-
-        switch self {
-        case .phone4inch, .phone4_7inch, .phone5_5inch, .phone5_8inch:
-            horizontalSizeClass = .compact
-            verticalSizeClass = .regular
-            userInterfaceIdiom = .phone
-            autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin]
-        case .pad10_5, .pad12_9inch:
-            horizontalSizeClass = .regular
-            verticalSizeClass = .regular
-            userInterfaceIdiom = .pad
-            autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        }
 
         switch self {
         case .phone4inch:
@@ -54,17 +43,46 @@ enum Device: String {
             size = .init(width: 414, height: 736)
         case .phone5_8inch:
             size = .init(width: 375, height: 812)
-        case .pad10_5:
-            size = isPortrait ? .init(width: 768, height: 1_024) : .init(width: 1_024, height: 768)
-        case .pad12_9inch:
-            size = isPortrait ? .init(width: 1_024, height: 1_366) : .init(width: 1_366, height: 1_024)
+        case .padPortraitOneThird:
+            size = .init(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height)
+        case .padPortraitTwoThirds:
+            size = .init(width: (UIScreen.main.bounds.width / 3) * 2, height: UIScreen.main.bounds.height)
+        case .padPortraitFull:
+            size = UIScreen.main.bounds.size
+        case .padLandscapeOneThird:
+            size = .init(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height)
+        case .padLandscapeOneHalf:
+            size = .init(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height)
+        case .padLandscapeTwoThirds:
+            size = .init(width: (UIScreen.main.bounds.width / 3) * 2, height: UIScreen.main.bounds.height)
+        case .padLandscapeFull:
+            size = UIScreen.main.bounds.size
         }
 
         // swiftlint:disable:next identifier_name
-        let x: CGFloat = (UIScreen.main.bounds.width - size.width) / 2
-
+        var x: CGFloat = (UIScreen.main.bounds.width - size.width) / 2
         // swiftlint:disable:next identifier_name
         let y: CGFloat = (UIScreen.main.bounds.height - size.height) / 2
+
+        switch self {
+        case .phone4inch, .phone4_7inch, .phone5_5inch, .phone5_8inch:
+            horizontalSizeClass = .compact
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .phone
+            autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin]
+        case .padLandscapeOneThird, .padLandscapeOneHalf, .padPortraitOneThird, .padPortraitTwoThirds:
+            horizontalSizeClass = .compact
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .pad
+            autoresizingMask = [.flexibleHeight]
+            x = 0
+        case .padPortraitFull, .padLandscapeFull, .padLandscapeTwoThirds:
+            horizontalSizeClass = .regular
+            verticalSizeClass = .regular
+            userInterfaceIdiom = .pad
+            autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            x = 0
+        }
 
         let traits: UITraitCollection = .init(traitsFrom: [
             .init(horizontalSizeClass: horizontalSizeClass),
@@ -76,11 +94,22 @@ enum Device: String {
     }
 
     static var all: [Device] {
-        return [.phone4inch,
+        var devices: [Device] = [.phone4inch,
                 .phone4_7inch,
                 .phone5_5inch,
-                .phone5_8inch,
-                .pad10_5,
-                .pad12_9inch]
+                .phone5_8inch]
+
+        let isPortrait = UIDevice.current.orientation == .portrait || UIDevice.current.orientation == .portraitUpsideDown
+        if isPortrait {
+            devices.append(contentsOf: [.padPortraitOneThird,
+                                        .padPortraitTwoThirds,
+                                        .padPortraitFull])
+        } else {
+            devices.append(contentsOf: [.padLandscapeOneThird,
+                                        .padLandscapeOneHalf,
+                                        .padLandscapeTwoThirds,
+                                        .padLandscapeFull])
+        }
+        return devices
     }
 }
