a2bada468ca6f5037e0a46ca0ef9bcd89da23a93
Felipe Espinoza <fespinozacast@gmail.com>
2019-10-22 11:18:57 +0200

    Add `ChristmasWishListView` component
    This is a view that will be rendered as a pop up, meaning the view
    itself will have constraints that limit its width and heith.
    
    The view implementation includes the subview:
    
    - ChristmasWishListView.ContentView: a nested subview to display the
    text content of the popup
    - ChristmasWishListView.ChrismasButton: a custom call-to-action like
    button with custom colors
    - MissionPopupView: a new generic popup-like container view. The reason
    for this is that pop up with this structure are not that infrequent,
    then I tried to make it a bit more generic, although for now I leave the
    subview within the Christmas folder until it's really needed as a
    generic component.
    
    The `ChristmasWishListView` implementation sets the contentView  of the
    `MissionPopupView` as a UIView that contains two instances of
    `ContentView` on top of each other. Then with a `CGAffineTransform` the
    second page content will be translated to the right to give the page
    behavior when clicking the button.
    
    I felt this was the easiest approach while being able to create a more
    reusable component in `MissionPopupView`.

---
+++ Demo/Components/ChristmasWishList/ChristmasWishListDemoView.swift
@@ -0,0 +1,64 @@
+//
+//  Copyright © 2019 FINN AS. All rights reserved.
+//
+
+import FinniversKit
+
+class ChristmasWishListDemoView: UIView {
+    private lazy var backgrounView = UIView()
+    private lazy var christmasWishListView = ChristmasWishListView(withAutoLayout: true)
+    let viewModel = ChristmasWishListViewModel()
+
+    // MARK: - Init
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup
+
+    private func setup() {
+        backgrounView.backgroundColor = .bgSecondary
+        christmasWishListView.configure(with: viewModel)
+
+        addSubview(backgrounView)
+        addSubview(christmasWishListView)
+
+        christmasWishListView.dropShadow(color: .black, opacity: 0.5, offset: CGSize(width: 5, height: 5), radius: 30)
+
+        backgrounView.fillInSuperview()
+
+        NSLayoutConstraint.activate([
+            christmasWishListView.widthAnchor.constraint(equalToConstant: 337),
+            christmasWishListView.heightAnchor.constraint(equalToConstant: 463),
+            christmasWishListView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            christmasWishListView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
+
+extension ChristmasWishListViewModel {
+    init() {
+        self.init(
+            firstPage: Page(
+                title: "Ønsk deg brukte gaver til jul!",
+                text: "Gjør det enkelt for dine nærmeste ved å dele ønskelisten din!",
+                accessoryButtonTitle: "Les mer om brukte julegaver",
+                actionButtonIcon: UIImage(named: FinniversImageAsset.gift),
+                actionButtonTitle: "Lag ønskeliste"
+            ),
+            secondPage: Page(
+                title: "Ønskelisten din er nå opprettet",
+                text: "Du vil nå kunne finne en helt nystrøken ønskeliste blant dine favorittlister.",
+                accessoryButtonTitle: nil,
+                actionButtonIcon: nil,
+                actionButtonTitle: "Den er god! God jul!"
+            )
+        )
+    }
+}
---
+++ Sources/Components/ChristmasWishListView/Views/MissionPopupView.swift
@@ -0,0 +1,112 @@
+//
+//  Copyright © 2019 FINN AS. All rights reserved.
+//
+
+public protocol MissionPopupViewDelegate: AnyObject {
+    func missionPopupViewDidSelectClose(_ view: MissionPopupView)
+}
+
+/// An attempt to create a generic pop-up like container to create views like the klimabrølet pop up
+public class MissionPopupView: UIView {
+    // MARK: - Public properties
+    weak var delegate: MissionPopupViewDelegate?
+
+    // MARK: - Private properties
+    private var headerView: UIView
+    private var contentView: UIView
+    private var actionView: UIView
+
+    private lazy var closeButton: UIButton = {
+        let button = UIButton(withAutoLayout: true)
+        button.tintColor = .bgPrimary
+        button.setImage(UIImage(named: .cross), for: .normal)
+        button.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        button.contentEdgeInsets = UIEdgeInsets(all: 6)
+        button.addTarget(self, action: #selector(handleTapOnCloseButton), for: .touchUpInside)
+
+        return button
+    }()
+
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView(withAutoLayout: true)
+        scrollView.bounces = true
+        scrollView.contentInset = UIEdgeInsets(bottom: .largeSpacing)
+        scrollView.delaysContentTouches = false
+        return scrollView
+    }()
+
+    private lazy var scrollableContentView: UIView = {
+        let view = UIView(withAutoLayout: true)
+        view.accessibilityLabel = "scrollableContent"
+        return view
+    }()
+
+    // MARK: - Initializers
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("Use the initializer init(withAutoLayout:headerView:contentView:actionView) instead")
+    }
+
+    public init(withAutoLayout: Bool, headerView: UIView, contentView: UIView, actionView: UIView) {
+        self.headerView = headerView
+        self.contentView = contentView
+        self.actionView = actionView
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = !withAutoLayout
+        setup()
+    }
+
+    // MARK: - Overrides
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        closeButton.layer.cornerRadius = closeButton.bounds.width / 2.0
+    }
+
+    // MARK: - Private methods
+    private func setup() {
+        backgroundColor = .bgPrimary
+        layer.cornerRadius = 20
+        clipsToBounds = true
+
+        scrollableContentView.addSubview(headerView)
+        scrollableContentView.addSubview(contentView)
+
+        scrollView.addSubview(scrollableContentView)
+
+        addSubview(scrollView)
+        addSubview(actionView)
+        addSubview(closeButton)
+
+        actionView.setContentHuggingPriority(.required, for: .vertical)
+        scrollableContentView.fillInSuperview()
+
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: scrollableContentView.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: scrollableContentView.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: scrollableContentView.trailingAnchor),
+            headerView.bottomAnchor.constraint(equalTo: contentView.topAnchor),
+
+            contentView.leadingAnchor.constraint(equalTo: scrollableContentView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollableContentView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollableContentView.bottomAnchor),
+
+            scrollableContentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            scrollableContentView.heightAnchor.constraint(greaterThanOrEqualTo: scrollView.heightAnchor),
+
+            closeButton.topAnchor.constraint(equalTo: topAnchor, constant: .mediumSpacing),
+            closeButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.mediumSpacing),
+
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: actionView.topAnchor),
+
+            actionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            actionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            actionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+
+    @objc private func handleTapOnCloseButton() {
+        delegate?.missionPopupViewDidSelectClose(self)
+    }
+}
