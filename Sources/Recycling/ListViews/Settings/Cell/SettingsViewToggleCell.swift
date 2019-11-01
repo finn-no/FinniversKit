d4f192641e36c1cd1922d12166090dee5a368b9e
Henrik Brustad <henrikbrustad@gmail.com>
2019-11-01 12:33:53 +0100

    Add settings tableview (#683)
    * delete previous settings files
    
    * adds settings view, cells and models
    
    * delete old settings demo files
    
    * adds demo settings view
    
    * updates snapshot reference image
    
    * change switch on tint color

---
+++ Sources/Recycling/ListViews/Settings/Cell/SettingsViewToggleCell.swift
@@ -0,0 +1,75 @@
+//
+//  Copyright © 2019 FINN AS. All rights reserved.
+//
+
+import UIKit
+
+protocol SettingsViewToggleCellDelegate: AnyObject {
+    func settingsViewToggleCell(_ cell: SettingsViewToggleCell, didToggleSettingFor model: SettingsViewToggleCellModel)
+}
+
+class SettingsViewToggleCell: SettingsViewCell {
+
+    // MARK: - Internal Properties
+
+    weak var delegate: SettingsViewToggleCellDelegate?
+
+    // MARK: - Private Properties
+
+    private var model: SettingsViewToggleCellModel?
+
+    private lazy var uiswitch: UISwitch = {
+        let toggle = UISwitch(withAutoLayout: true)
+        toggle.onTintColor = .btnPrimary
+        toggle.addTarget(self, action: #selector(handleValueChanged(toggle:)), for: .valueChanged)
+        return toggle
+    }()
+
+    // MARK: - Init
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Overrides
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        configure(with: nil, isLastItem: false)
+    }
+
+    override func setup() {
+        super.setup()
+
+        contentView.addSubview(uiswitch)
+
+        NSLayoutConstraint.activate([
+            uiswitch.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            uiswitch.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -.mediumLargeSpacing),
+        ])
+    }
+}
+
+extension SettingsViewToggleCell {
+    func configure(with model: SettingsViewToggleCellModel?, isLastItem: Bool) {
+        super.configure(with: model, isLastItem: isLastItem)
+        self.model = model
+        uiswitch.isOn = model?.isOn ?? false
+    }
+}
+
+private extension SettingsViewToggleCell {
+    @objc func handleValueChanged(toggle: UISwitch) {
+        guard var model = model else {
+            return
+        }
+
+        model.isOn = toggle.isOn
+        delegate?.settingsViewToggleCell(self, didToggleSettingFor: model)
+    }
+}
