1069a917619acdec9b8fa698fc38a045169b65ad
Elvis Nuñez <elvisnunez@me.com>
2019-11-12 17:27:59 +0100

    Add Sparkle framework

---
+++ Sparkle/Sparkle.h
@@ -0,0 +1,19 @@
+//
+//  Sparkle.h
+//  Sparkle
+//
+//  Created by Elvis Nuñez on 12/11/2019.
+//  Copyright © 2019 FINN AS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sparkle.
+FOUNDATION_EXPORT double SparkleVersionNumber;
+
+//! Project version string for Sparkle.
+FOUNDATION_EXPORT const unsigned char SparkleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sparkle/PublicHeader.h>
+
+
