e52387c9462b61a06ba767ccff4612212384cecd
Felipe Espinoza <fespinozacast@gmail.com>
2019-11-18 10:12:39 +0100

    Adapt `UserAdManagementStatisticsCell` for header data
    This means the cell uses a `configure(with:)` method to set the data into the cell.
    
    It also means the inclusion of a delegate method for when the full statistic button
    is tapped.

--- Sources/Recycling/ListViews/AdManagement/Cells/UserAdManagementStatisticsCell.swift
+++ Sources/Recycling/ListViews/AdManagement/Cells/UserAdManagementStatisticsCell.swift
@@ -2,15 +2,31 @@
 //  Copyright © FINN.no AS. All rights reserved.
 //
 
+public protocol UserAdManagementStatisticsCellDelegate: AnyObject {
+    func userAdManagementStatisticsCellDidSelectFullStatistics(_ cell: UserAdManagementStatisticsCell)
+}
+
 public class UserAdManagementStatisticsCell: UITableViewCell {
 
     // MARK: - Public
 
-    public var itemModels = [StatisticsItemModel]() {
-        didSet { updateStackViewContent() }
-    }
+    public weak var delegate: UserAdManagementStatisticsCellDelegate?
 
     // MARK: - Private
+
+    private lazy var headerStackView: UIStackView = {
+        let stackView = UIStackView(withAutoLayout: true)
+        stackView.axis = .horizontal
+        return stackView
+    }()
+
+    private lazy var titleLabel: Label = Label(style: .bodyStrong, withAutoLayout: true)
+
+    private lazy var fullStatisticsButton: Button = {
+        let button = Button(style: .link, withAutoLayout: true)
+        button.addTarget(self, action: #selector(fullStatisticsButtonTapped), for: .touchUpInside)
+        return button
+    }()
 
     private lazy var stackView: UIStackView = {
         let view = UIStackView(withAutoLayout: true)
@@ -21,7 +37,7 @@ public class UserAdManagementStatisticsCell: UITableViewCell {
 
     private lazy var separatorView: UIView = {
         let view = UIView(withAutoLayout: true)
-        view.backgroundColor = .textDisabled
+        view.backgroundColor = .tableViewSeparator
         return view
     }()
 
@@ -37,16 +53,29 @@ public class UserAdManagementStatisticsCell: UITableViewCell {
         setup()
     }
 
+    // MARK: - Public methods
+    public func configure(with model: StatisticsModel) {
+        if let header = model.header {
+            headerStackView.isHidden = false
+            titleLabel.text = header.title
+            fullStatisticsButton.setTitle(header.fullStatisticsTitle, for: .normal)
+        } else {
+            headerStackView.isHidden = true
+        }
+
+        updateStackViewContent(items: model.statisticItems)
+    }
+
     // MARK: - Private methods
 
-    private func updateStackViewContent() {
+    private func updateStackViewContent(items: [StatisticsItemModel]) {
         for oldSubview in stackView.arrangedSubviews {
             stackView.removeArrangedSubview(oldSubview)
             oldSubview.removeFromSuperview()
         }
 
-        let lastIndex = itemModels.count-1
-        for (index, model) in itemModels.enumerated() {
+        let lastIndex = items.count-1
+        for (index, model) in items.enumerated() {
             let itemView = StatisticsItemView(model: model)
             itemView.shouldShowLeftSeparator = index > 0 && index < lastIndex
             itemView.shouldShowRightSeparator = index > 0 && index < lastIndex
@@ -58,9 +87,19 @@ public class UserAdManagementStatisticsCell: UITableViewCell {
         }
     }
 
+    @objc func fullStatisticsButtonTapped() {
+        delegate?.userAdManagementStatisticsCellDidSelectFullStatistics(self)
+    }
+
     private func setup() {
         selectionStyle = .none
+        backgroundColor = .bgPrimary
+
+        headerStackView.addArrangedSubview(titleLabel)
+        headerStackView.addArrangedSubview(fullStatisticsButton)
+
         contentView.addSubview(separatorView)
+        contentView.addSubview(headerStackView)
         contentView.addSubview(stackView)
 
         let hairLineSize = 1.0/UIScreen.main.scale
@@ -71,7 +110,12 @@ public class UserAdManagementStatisticsCell: UITableViewCell {
             separatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             separatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
 
-            stackView.topAnchor.constraint(equalTo: separatorView.bottomAnchor),
+            headerStackView.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: .mediumSpacing),
+            headerStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: .mediumLargeSpacing),
+            headerStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -.mediumLargeSpacing),
+            headerStackView.bottomAnchor.constraint(equalTo: stackView.topAnchor),
+
+            stackView.topAnchor.constraint(equalTo: headerStackView.bottomAnchor),
             stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)])
