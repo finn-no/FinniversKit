a60c82d48818e10138424af9f79d4f80f8c2c435
Vadym Markov <markov.vadym@gmail.com>
2019-11-20 12:30:33 +0100

    Fix snapshot tests

--- UnitTests/ComponentViewTests.swift
+++ UnitTests/ComponentViewTests.swift
@@ -19,6 +19,10 @@ class ComponentViewTests: XCTestCase {
         }
     }
 
+    func testAddressCardView() {
+        snapshot(.addressCardView)
+    }
+
     func testButton() {
         snapshot(.button)
     }
