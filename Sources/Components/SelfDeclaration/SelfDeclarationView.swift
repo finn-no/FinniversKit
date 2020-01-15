cd7f222243dce0ac50edc5940643de2edc54657f
Bastian Stien <bastian.stien@gmail.com>
2020-01-15 14:28:33 +0100

    Add SelfDeclarationView

---
+++ Sources/Components/SelfDeclaration/SelfDeclarationView.swift
@@ -0,0 +1,60 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+public class SelfDeclarationView: UIView {
+
+    // MARK: - Private properties
+
+    private lazy var introductionLabel: Label = {
+        let label = Label(style: .body, withAutoLayout: true)
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(withAutoLayout: true)
+        stackView.axis = .vertical
+        stackView.spacing = .mediumLargeSpacing
+        return stackView
+    }()
+
+    // MARK: - Init
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder: NSCoder) { fatalError() }
+
+    // MARK: - Setup
+
+    private func setup() {
+        stackView.addArrangedSubview(introductionLabel)
+        addSubview(stackView)
+        stackView.fillInSuperview()
+    }
+
+    // MARK: - Public methods
+
+    public func configure(with viewModel: SelfDeclarationViewModel) {
+        introductionLabel.text = viewModel.introduction
+
+        stackView.removeDeclarationItemSubviews()
+
+        viewModel.items.forEach {
+            let view = SelfDeclarationItemView(withAutoLayout: true)
+            view.configure(with: $0)
+            stackView.addArrangedSubview(view)
+        }
+    }
+}
+
+// MARK: - Private extensions
+
+private extension UIStackView {
+    func removeDeclarationItemSubviews() {
+        arrangedSubviews.filter { $0 is SelfDeclarationView }.forEach { removeArrangedSubview($0) }
+    }
+}
