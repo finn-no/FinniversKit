148c0427540951d90a90e6fba79b39589823413c
Bastian Stien <bastian.stien@gmail.com>
2020-01-16 11:14:55 +0100

    🚗 MP: Seller self declaration (#743)
    * Add models
    
    * Add SelfDeclarationItemView
    
    * Add SelfDeclarationView
    
    * Add demo
    
    * Add snapshot test
    
    * Removed extra newline

--- Demo/Demo/DemoViews/ComponentDemoViews.swift
+++ Demo/Demo/DemoViews/ComponentDemoViews.swift
@@ -59,6 +59,7 @@ public enum ComponentDemoViews: String, CaseIterable {
     case collapseView
     case orderSummaryView
     case orderTotalSumView
+    case selfDeclarationView
 
     public static var items: [ComponentDemoViews] {
         return allCases.sorted { $0.rawValue < $1.rawValue }
@@ -183,6 +184,8 @@ public enum ComponentDemoViews: String, CaseIterable {
             return DemoViewController<OrderSummaryDemoView>()
         case .orderTotalSumView:
             return DemoViewController<OrderTotalSumDemoView>()
+        case .selfDeclarationView:
+            return DemoViewController<SelfDeclarationDemoView>()
         }
     }
 }
--- UnitTests/ComponentViewTests.swift
+++ UnitTests/ComponentViewTests.swift
@@ -238,4 +238,8 @@ class ComponentViewTests: XCTestCase {
     func testCollapseView() {
         snapshot(.collapseView)
     }
+
+    func testSelfDeclarationView() {
+        snapshot(.selfDeclarationView)
+    }
 }
