adae60c18879818c65931f2513ca05bfe63a3c55
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2020-02-18 12:41:27 +0100

    Create dummy data

--- Demo/Fullscreen/TransactionView/TransactionDemoViewDefaultData.swift
+++ Demo/Fullscreen/TransactionView/TransactionDemoViewDefaultData.swift
@@ -2,4 +2,50 @@
 //  Copyright © 2020 FINN AS. All rights reserved.
 //
 
-import Foundation
+import FinniversKit
+
+public struct TransactionStepModel: TransactionStepViewModel {
+    public var state: TransactionState
+    public var title: String
+    public var subtitle: String?
+    public var buttonText: String?
+    public var detail: String?
+}
+
+public struct TransactionStepsFactory {
+    public static var steps: [TransactionStepModel] = completedSteps + currentStep + notStartedSteps
+    public static var numberOfSteps: Int = completedSteps.count + currentStep.count + notStartedSteps.count
+
+    public static var completedSteps: [TransactionStepModel] = [
+        TransactionStepModel(
+            state: .completed,
+            title: "Annonsen er lagt ut",
+            buttonText: "Se annonsen"),
+
+        TransactionStepModel(
+            state: .completed,
+            title: "Kontrakt",
+            subtitle: "Du har opprettet kontrakt.",
+            buttonText: "Inviter kjøper"),
+    ]
+
+    public static var currentStep = [TransactionStepModel(
+        state: .inProgress,
+        title: "Betaling",
+        subtitle: "Før kjøper kan overføre pengene må du forberede betalingen.",
+        buttonText: "Forbered betaling")
+    ]
+
+    public static var notStartedSteps: [TransactionStepModel] = [
+        TransactionStepModel(
+            state: .notStarted,
+            title: "Overlevering",
+            subtitle: "Dere må møtes og bekrefte overleveringen innen 7 dager etter kjøper har betalt."),
+
+        TransactionStepModel(
+            state: .notStarted,
+            title: "Gratulerer med salget!",
+            subtitle: "Du kan finne igjen bilen i Mine kjøretøy under <<Eide før>>\n\nDet kan ta noen dager før pengene dukker opp på kontoen din",
+            detail: ""),
+    ]
+}
