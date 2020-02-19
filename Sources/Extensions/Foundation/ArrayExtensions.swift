4d2765839baaea3b706858f821d46ac09cb6d5aa
Felipe Espinoza <fespinozacast@gmail.com>
2020-02-19 10:39:11 +0100

    Fix: style for columns list component (#789)
    * Fix column list view style to not use bullet points
    
    as the new changes to the design of that component, then I replaced the internal
    text views for stack views with labels, since they were easy to control the spacing
    and line behavior of
    
    * Update snapshot for the columns list view

--- Sources/Extensions/Foundation/ArrayExtensions.swift
+++ Sources/Extensions/Foundation/ArrayExtensions.swift
@@ -24,6 +24,8 @@ public extension Array where Element == String {
 
 extension Array {
     func chunked(by size: Int) -> [[Element]] {
+        guard !isEmpty else { return [[]] }
+
         return stride(from: 0, to: count, by: size).map {
             Array(self[$0..<Swift.min($0 + size, count)])
         }
