06fad6b460f8c79d73800c45a7bac830c5c9553a
Felipe Espinoza <fespinozacast@gmail.com>
2020-03-06 11:41:49 +0100

    Sort implementation and set tweaking options for the demo

--- Sources/Fullscreen/ErrorView/ErrorView.swift
+++ Sources/Fullscreen/ErrorView/ErrorView.swift
@@ -5,37 +5,13 @@
 import UIKit
 
 public class ErrorView: UIView {
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-
-    public required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setup()
-    }
-
-    public func configure(title: String, description: String? = nil, icon: UIImage? = nil) {
-        titleLabel.text = title
-        descriptionLabel.text = description
-        iconImageView.image = icon
-    }
-
-    public func configure(title: NSAttributedString, description: String? = nil, icon: UIImage? = nil) {
-        titleLabel.attributedText = title
-        descriptionLabel.text = description
-        iconImageView.image = icon
-    }
-
-    private func show() {
-        isHidden = false
-    }
-
-    private func hide() {
-        isHidden = true
-    }
-
-    private lazy var titleLabel: Label = Label(style: .title3Strong, withAutoLayout: true)
+    // MARK: - Private variables
+    private lazy var titleLabel: Label = {
+        let label = Label(style: .title3Strong, withAutoLayout: true)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
 
     private lazy var descriptionLabel: Label = {
         let label = Label(style: .body, withAutoLayout: true)
@@ -57,6 +33,34 @@ public class ErrorView: UIView {
         return stackView
     }()
 
+    // MARK: - Initializers
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    // MARK: - Public methods
+
+    public func configure(
+        title: String,
+        titleColor: UIColor = .textPrimary,
+        description: String? = nil,
+        icon: UIImage? = nil
+    ) {
+        titleLabel.text = title
+        titleLabel.textColor = titleColor
+        descriptionLabel.text = description
+        iconImageView.image = icon
+        iconImageView.isHidden = icon == nil
+    }
+
+    // MARK: - Private methods
+
     private func setup() {
         backgroundColor = .bgTertiary
 
@@ -71,6 +75,6 @@ public class ErrorView: UIView {
             iconImageView.heightAnchor.constraint(equalToConstant: 40),
         ])
 
-        stackView.centerInSuperview()
+        stackView.centerAndConstraintInSuperview()
     }
 }
