b640409d5cf24cab84a7853aad6817a214855b59
Jonas Olsson <osanoj@users.noreply.github.com>
2020-03-26 15:20:16 +0100

    Create FinnUI project and restructure FinniversKit files (#846)
    * Added FinnUI project
    
    * Changed ordering
    
    * Updated circleci config
    
    * Moved FinniversKit project + files into a subfolder
    
    * Updated workspace reference to FinniversKit
    
    * Updated FinniversKit podspec

---
+++ FinnUI/Sources/FinnUI.h
@@ -0,0 +1,9 @@
+@import UIKit;
+
+FOUNDATION_EXPORT double FinnUIVersionNumber;
+FOUNDATION_EXPORT const unsigned char FinnUIVersionString[];
+
+//#import "FinniversImageAsset.h"
+#import "Spacing.h"
+
+
