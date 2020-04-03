f3793e97e5963b9c4e44d8a795ba4fe9688279d5
Bastian Stien <bastian.stien@gmail.com>
2020-04-03 12:07:20 +0200

    Add StatusView

--- FinnUI/Sources/Components/ChatAvailability/ChatAvailabilityView.swift
+++ FinnUI/Sources/Components/ChatAvailability/ChatAvailabilityView.swift
@@ -14,10 +14,12 @@ public class ChatAvailabilityView: UIView {
 
     // MARK: - Private properties
 
+    private lazy var statusView = StatusView(withAutoLayout: true)
+
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView(withAutoLayout: true)
         stackView.axis = .vertical
-        stackView.spacing = .spacingM
+        stackView.spacing = .spacingS
         return stackView
     }()
 
@@ -27,7 +29,6 @@ public class ChatAvailabilityView: UIView {
         button.setTitle("Chat med oss", for: .normal)
         button.titleEdgeInsets = UIEdgeInsets(leading: .spacingS)
         button.imageEdgeInsets = UIEdgeInsets(top: .spacingXS, leading: -.spacingS)
-        button.adjustsImageWhenHighlighted = false
         let image = UIImage(named: .videoChat)
         button.setImage(image.withRenderingMode(.alwaysTemplate), for: .normal)
         button.imageView?.tintColor = .milk
@@ -48,13 +49,21 @@ public class ChatAvailabilityView: UIView {
     private func setup() {
         addSubview(stackView)
         stackView.addArrangedSubview(chatNowButton)
+        stackView.addArrangedSubview(statusView)
         stackView.fillInSuperview()
     }
 
     // MARK: - Public methods
 
-    public func configure(status: Status) {
+    public func configure(status: Status, statusTitle: String? = nil) {
+        switch status {
+        case .online, .unknown:
+            chatNowButton.isEnabled = true
+        case .loading, .offline:
+            chatNowButton.isEnabled = false
+        }
 
+        statusView.configure(status: status, statusTitle: statusTitle)
     }
 
     // MARK: - Private methods
@@ -70,9 +79,12 @@ private class StatusView: UIView {
 
     // MARK: - Private properties
 
+    private lazy var statusLabel = Label(style: .detail, withAutoLayout: true)
+    private lazy var loadingIndicator = LoadingIndicatorView(withAutoLayout: true)
+
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView(withAutoLayout: true)
-        stackView.spacing = .spacingM
+        stackView.spacing = .spacingS
         return stackView
     }()
 
@@ -88,7 +100,44 @@ private class StatusView: UIView {
     // MARK: - Setup
 
     private func setup() {
+        stackView.addArrangedSubview(loadingIndicator)
+        stackView.addArrangedSubview(statusLabel)
+
         addSubview(stackView)
         stackView.fillInSuperview()
+
+        NSLayoutConstraint.activate([
+            loadingIndicator.widthAnchor.constraint(equalToConstant: .spacingM),
+            loadingIndicator.heightAnchor.constraint(equalToConstant: .spacingM)
+        ])
     }
+
+    // MARK: - Public methods
+
+    public func configure(status: ChatAvailabilityView.Status, statusTitle: String? = nil) {
+        statusLabel.text = statusTitle
+        statusLabel.isHidden = statusTitle?.isEmpty ?? true
+
+        switch status {
+        case .loading:
+            loadingIndicator.isHidden = false
+            loadingIndicator.startAnimating()
+        default:
+            loadingIndicator.isHidden = true
+            loadingIndicator.stopAnimating()
+        }
+
+        switch status {
+        case .online:
+            statusLabel.textColor = .online
+        case .offline, .unknown:
+            statusLabel.textColor = .textCritical
+        case .loading:
+            statusLabel.textColor = .textSecondary
+        }
+    }
+}
+
+private extension UIColor {
+    static var online = dynamicColorIfAvailable(defaultColor: UIColor(hex: "#32A24C"), darkModeColor: .pea)
 }
