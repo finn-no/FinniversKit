78b4c719751188f6de1702ac625513903bd901ac
T. Benjamin Larsen <benjamin.larsen@noreagle.no>
2020-04-29 07:24:44 +0200

    Adin 8096 external button in ad confirmation (#865)
    * Model and demo data for AdConfirmationLinkView
    * AdConfirmationLinkView

---
+++ FinniversKit/Sources/Fullscreen/AdConfirmationView/Models/AdConfirmationLinkViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2020 FINN AS. All rights reserved.
+//
+
+import Foundation
+
+public protocol AdConfirmationLinkViewModel {
+    var title: String? { get set }
+    var body: String? { get set }
+    var linkTitle: String { get set }
+}
