16e21bc24719079e4ed2adf29250c6ae9e922c2a
Felipe Espinoza <fespinozacast@gmail.com>
2020-05-04 15:03:30 +0200

    Add `Color` extensions for SwiftUI (#869)
    Using our current semantic colors.
    
    Notice I didn't add the non-semantic colors to promote usage of the semantic ones only in swiftUI

---
+++ FinniversKit/Sources/DNA/Color/Color+SwiftUI.swift
@@ -0,0 +1,51 @@
+//
+//  Copyright © 2020 FINN AS. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension Color {
+    // MARK: - Background
+
+    public static var bgPrimary: Color     { Color(.bgPrimary) }
+    public static var bgSecondary: Color   { Color(.bgSecondary) }
+    public static var bgTertiary: Color    { Color(.bgTertiary) }
+    public static var bgBottomSheet: Color { Color(.bgBottomSheet) }
+    public static var bgAlert: Color       { Color(.bgAlert) }
+    public static var bgSuccess: Color     { Color(.bgSuccess) }
+    public static var bgCritical: Color    { Color(.bgCritical) }
+
+    // MARK: - Button
+
+    public static var btnPrimary: Color  { Color(.btnPrimary) }
+    public static var btnDisabled: Color { Color(.btnDisabled) }
+    public static var btnCritical: Color { Color(.btnCritical) }
+    public static var btnAction: Color   { Color(.btnAction) }
+
+    // MARK: - Text
+
+    public static var textPrimary: Color     { Color(.textPrimary) }
+    public static var textSecondary: Color   { Color(.textSecondary) }
+    public static var textTertiary: Color    { Color(.textTertiary) }
+    public static var textAction: Color      { Color(.textAction) }
+    public static var textDisabled: Color    { Color(.textDisabled) }
+    public static var textCritical: Color    { Color(.textCritical) }
+    public static var textToast: Color       { Color(.textToast) }
+    public static var textCTADisabled: Color { Color(.textCTADisabled) }
+
+    // MARK: - Icon
+
+    public static var iconPrimary: Color   { Color(.iconPrimary) }
+    public static var iconSecondary: Color { Color(.iconSecondary) }
+    public static var iconTertiary: Color  { Color(.iconTertiary) }
+
+    // MARK: - Others
+
+    public static var accentSecondaryBlue: Color { Color(.accentSecondaryBlue) }
+    public static var accentPea: Color           { Color(.accentPea) }
+    public static var accentToothpaste: Color    { Color(.accentToothpaste) }
+    public static var tableViewSeparator: Color  { Color(.tableViewSeparator) }
+    public static var imageBorder: Color         { Color(.imageBorder) }
+    public static var decorationSubtle: Color    { Color(.decorationSubtle) }
+}
