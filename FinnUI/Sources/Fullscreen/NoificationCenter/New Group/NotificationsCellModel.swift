65a3b929b4193a764be207c72bf6cbd0acfae125
Henrik Brustad <henrikbrustad@gmail.com>
2020-05-11 09:18:57 +0200

    fix broken folder structure

--- FinnUI/Sources/Fullscreen/NoificationCenter/New Group/NotificationsCellModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NotificationsCellModel.swift
-//  FinnUI
-//
-//  Created by Henrik Granheim Brustad on 10/05/2020.
-//  Copyright © 2020 FINN.no AS. All rights reserved.
-//
-
-import Foundation
