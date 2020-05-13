d90385b1801cffdc5941ac64836f1fa77bee9c9a
Robin Saleh-Jan <8507719+robinsalehjan@users.noreply.github.com>
2020-05-13 13:45:16 +0200

    Update TransactionProcessSummaryView model (#883)
    * Turn externalTitle into a ViewModel
    
    * Remove redundant property
    
    * Reorder init arguments
    
    Co-authored-by: Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>

--- FinniversKit/Sources/Components/TransactionProcessSummaryView/TransactionProcessSummaryViewModel.swift
+++ FinniversKit/Sources/Components/TransactionProcessSummaryView/TransactionProcessSummaryViewModel.swift
@@ -6,14 +6,25 @@ public struct TransactionProcessSummaryViewModel {
     public let title: String
     public let detail: String
     public let description: String
-    public let externalTitle: String?
+    public let externalView: TransactionProcessSummaryExternalViewModel?
     public let style: String?
 
-    public init(title: String, detail: String, description: String, externalTitle: String?, style: String?) {
+    public init(title: String, detail: String, description: String, externalView: TransactionProcessSummaryExternalViewModel?, style: String?) {
         self.title = title
         self.detail = detail
         self.description = description
-        self.externalTitle = externalTitle
+        self.externalView = externalView
         self.style = style
+    }
+}
+
+// swiftlint:disable:next superfluous_disable_command type_name
+public struct TransactionProcessSummaryExternalViewModel {
+    public let text: String
+    public let url: String
+
+    public init(text: String, url: String) {
+        self.text = text
+        self.url = url
     }
 }
