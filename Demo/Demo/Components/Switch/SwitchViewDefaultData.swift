07822f3881c33de8334bbae74ad88cebefa91db7
Bastian Stien <bastian.stien@gmail.com>
2020-05-14 13:50:46 +0200

    Remove redundant inits

--- Demo/Demo/Components/Switch/SwitchViewDefaultData.swift
+++ Demo/Demo/Components/Switch/SwitchViewDefaultData.swift
@@ -5,19 +5,19 @@
 import FinniversKit
 
 extension SwitchViewDefaultModel {
-    static var demoViewModel1 = SwitchViewDefaultModel.init(
+    static var demoViewModel1 = SwitchViewDefaultModel(
         title: "Anbefalinger",
         detail: "Vi gir deg relevante tips på forsiden",
         initialSwitchValue: true
     )
 
-    static var demoViewModel2 = SwitchViewDefaultModel.init(
+    static var demoViewModel2 = SwitchViewDefaultModel(
         title: "Smart reklame",
         detail: "Vi leter for deg når du gjør andre ting",
         initialSwitchValue: false
     )
 
-    static var demoViewModel3 = SwitchViewDefaultModel.init(
+    static var demoViewModel3 = SwitchViewDefaultModel(
         title: "Annen styling",
         detail: "Denne her har en annen styling enn de to andre over",
         initialSwitchValue: true
