5ce45e3971cc90913c8f7ac785d1dd82a891f5d0
Bastian Stien <bastian.stien@gmail.com>
2020-05-14 14:00:16 +0200

    SwitchView styling (#885)

--- Demo/Demo/Components/Switch/SwitchViewDemoView.swift
+++ Demo/Demo/Components/Switch/SwitchViewDemoView.swift
@@ -5,18 +5,17 @@
 import FinniversKit
 
 public class SwitchViewDemoView: UIView {
-    private lazy var recommendationsSwitchView: SwitchView = {
-        let switchView = SwitchView()
-        switchView.translatesAutoresizingMaskIntoConstraints = false
-        return switchView
-    }()
+    private lazy var demoSwitch1 = SwitchView(withAutoLayout: true)
+    private lazy var demoSwitch2 = SwitchView(withAutoLayout: true)
+    private lazy var demoSwitch3 = SwitchView(style: .customStyle, withAutoLayout: true)
 
-    private lazy var commercialSwitchView: SwitchView = {
-        let switchView = SwitchView()
-        switchView.translatesAutoresizingMaskIntoConstraints = false
-        return switchView
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [demoSwitch1, demoSwitch2, demoSwitch3])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = .spacingS
+        return stackView
     }()
-
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -26,20 +25,25 @@ public class SwitchViewDemoView: UIView {
     public required init?(coder aDecoder: NSCoder) { fatalError() }
 
     private func setup() {
-        recommendationsSwitchView.configure(with: SwitchDefaultData1())
-        commercialSwitchView.configure(with: SwitchDefaultData2())
+        demoSwitch1.configure(with: SwitchViewDefaultModel.demoViewModel1)
+        demoSwitch2.configure(with: SwitchViewDefaultModel.demoViewModel2)
+        demoSwitch3.configure(with: SwitchViewDefaultModel.demoViewModel3)
 
-        addSubview(recommendationsSwitchView)
-        addSubview(commercialSwitchView)
+        addSubview(stackView)
 
         NSLayoutConstraint.activate([
-            recommendationsSwitchView.topAnchor.constraint(equalTo: topAnchor, constant: .spacingM),
-            recommendationsSwitchView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
-            recommendationsSwitchView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingM),
-
-            commercialSwitchView.topAnchor.constraint(equalTo: recommendationsSwitchView.bottomAnchor, constant: .spacingS),
-            commercialSwitchView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
-            commercialSwitchView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingM)
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: .spacingM),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingM),
         ])
     }
+}
+
+private extension SwitchViewStyle {
+    static var customStyle = SwitchViewStyle(
+        titleLabelStyle: .bodyStrong,
+        titleLabelTextColor: .textPrimary,
+        detailLabelStyle: .caption,
+        detailLabelTextColor: .textPrimary
+    )
 }
