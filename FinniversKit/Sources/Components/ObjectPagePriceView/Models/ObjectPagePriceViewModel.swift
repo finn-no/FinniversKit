8e3d905a018c80e1b20dddd7a5e5063fd3c0ad68
Bastian Stien <bastian.stien@gmail.com>
2020-05-27 08:46:19 +0200

    Add adType to price view (#895)

--- FinniversKit/Sources/Components/ObjectPagePriceView/Models/ObjectPagePriceViewModel.swift
+++ FinniversKit/Sources/Components/ObjectPagePriceView/Models/ObjectPagePriceViewModel.swift
@@ -3,19 +3,24 @@
 //
 
 public struct ObjectPagePriceViewModel {
-    let mainPriceModel: Price
+    let mainPriceModel: Price?
     let secondaryPriceModel: Price?
     let links: [LinkButtonViewModel]
+    let adTypeText: String?
 
-    public init(title: String? = nil, totalPrice: String, subtitle: String? = nil, accessibilityLabel: String? = nil, links: [LinkButtonViewModel] = []) {
-        let mainPriceModel = Price(title: title, totalPrice: totalPrice, subtitle: subtitle, accessibilityLabel: accessibilityLabel)
-        self.init(mainPriceModel: mainPriceModel, links: links)
+    public init(title: String? = nil, totalPrice: String? = nil, subtitle: String? = nil, accessibilityLabel: String? = nil, links: [LinkButtonViewModel] = [], adTypeText: String? = nil) {
+        var mainPriceModel: Price?
+        if let totalPrice = totalPrice {
+            mainPriceModel = Price(title: title, totalPrice: totalPrice, subtitle: subtitle, accessibilityLabel: accessibilityLabel)
+        }
+        self.init(mainPriceModel: mainPriceModel, links: links, adTypeText: adTypeText)
     }
 
-    public init(mainPriceModel: Price, secondaryPriceModel: Price? = nil, links: [LinkButtonViewModel]) {
+    public init(mainPriceModel: Price?, secondaryPriceModel: Price? = nil, links: [LinkButtonViewModel], adTypeText: String? = nil) {
         self.mainPriceModel = mainPriceModel
         self.secondaryPriceModel = secondaryPriceModel
         self.links = links
+        self.adTypeText = adTypeText
     }
 
     public struct Price {
