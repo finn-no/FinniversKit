375f5f01581acdccd2064ecf3b054cabc1341062
Bastian Stien <bastian.stien@gmail.com>
2020-06-10 15:07:10 +0200

    Update demo

--- Demo/Demo/Components/ImageLinkView/ImageLinkViewDemo.swift
+++ Demo/Demo/Components/ImageLinkView/ImageLinkViewDemo.swift
@@ -31,6 +31,7 @@ public class ImageLinkViewDemo: UIView {
         let views = viewModels.map { viewModel -> ImageLinkView in
             let view = ImageLinkView(withAutoLayout: true)
             view.remoteImageViewDataSource = DemoRemoteImageViewDataSource.shared
+            view.delegate = self
             view.configure(with: viewModel)
             return view
         }
@@ -46,9 +47,18 @@ public class ImageLinkViewDemo: UIView {
     }
 }
 
-extension ImageLinkViewModel {
+extension ImageLinkViewDemo: ImageLinkViewDelegate {
+    public func imageLinkViewWasSelected(_ view: ImageLinkView, url: String) {
+        print("🔥🔥🔥🔥 ImageLinkView tapped with url: \(url)")
+    }
+}
+
+// MARK: - Private extensions
+
+private extension ImageLinkViewModel {
     static var videoLink = ImageLinkViewModel(
         description: "Videovisning",
+        url: "https://www.finn.no",
         imageUrl: "https://i.pinimg.com/736x/73/de/32/73de32f9e5a0db66ec7805bb7cb3f807--navy-blue-houses-blue-and-white-houses-exterior.jpg",
         loadingColor: .salmon,
         overlayKind: .video
@@ -56,6 +66,7 @@ extension ImageLinkViewModel {
 
     static var virtualViewing = ImageLinkViewModel(
         description: "360° visning",
+        url: "https://www.finn.no",
         imageUrl: "http://i3.au.reastatic.net/home-ideas/raw/a96671bab306bcb39783bc703ac67f0278ffd7de0854d04b7449b2c3ae7f7659/facades.jpg",
         loadingColor: .mint,
         overlayKind: .virtualViewing
