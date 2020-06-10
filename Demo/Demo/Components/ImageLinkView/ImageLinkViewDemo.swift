7c8d6d0327716cd29c3b0713712fe0b1e721a1dc
Bastian Stien <bastian.stien@gmail.com>
2020-06-10 13:52:21 +0200

    Refactor demo

--- Demo/Demo/Components/ImageLinkView/ImageLinkViewDemo.swift
+++ Demo/Demo/Components/ImageLinkView/ImageLinkViewDemo.swift
@@ -30,6 +30,7 @@ public class ImageLinkViewDemo: UIView {
     private func setup() {
         let views = viewModels.map { viewModel -> ImageLinkView in
             let view = ImageLinkView(withAutoLayout: true)
+            view.remoteImageViewDataSource = self
             view.configure(with: viewModel)
             return view
         }
@@ -45,34 +46,49 @@ public class ImageLinkViewDemo: UIView {
     }
 }
 
+// MARK: - RemoteImageTableViewCellDataSource
+
+extension ImageLinkViewDemo: RemoteImageViewDataSource {
+    public func remoteImageView(_ view: RemoteImageView, loadImageWithPath imagePath: String, imageWidth: CGFloat, completion: @escaping (UIImage?) -> Void) {
+        guard let url = URL(string: imagePath) else {
+            completion(nil)
+            return
+        }
+
+        // Demo code only.
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            usleep(50_000)
+            DispatchQueue.main.async {
+                if let data = data, let image = UIImage(data: data) {
+                    completion(image)
+                } else {
+                    completion(nil)
+                }
+            }
+        }
+
+        task.resume()
+    }
+
+    public func remoteImageView(_ view: RemoteImageView, cancelLoadingImageWithPath imagePath: String, imageWidth: CGFloat) {}
+
+    public func remoteImageView(_ view: RemoteImageView, cachedImageWithPath imagePath: String, imageWidth: CGFloat) -> UIImage? {
+        nil
+    }
+}
+
 extension ImageLinkViewModel {
     static var videoLink = ImageLinkViewModel(
         description: "Videovisning",
-        image: .imageWithColor(color: .salmon),
+        imageUrl: "https://i.pinimg.com/736x/73/de/32/73de32f9e5a0db66ec7805bb7cb3f807--navy-blue-houses-blue-and-white-houses-exterior.jpg",
+        loadingColor: .salmon,
         overlayKind: .video
     )
 
     static var virtualViewing = ImageLinkViewModel(
         description: "360° visning",
-        image: .imageWithColor(color: .mint),
+        imageUrl: "http://i3.au.reastatic.net/home-ideas/raw/a96671bab306bcb39783bc703ac67f0278ffd7de0854d04b7449b2c3ae7f7659/facades.jpg",
+        loadingColor: .mint,
         overlayKind: .virtualViewing
     )
-
-    static var noOverlay = ImageLinkViewModel(
-        description: "Bare et vanlig bilde",
-        image: .imageWithColor(color: .primaryBlue)
-    )
-}
-
-private extension UIImage {
-    static func imageWithColor(color: UIColor) -> UIImage {
-        let size = CGSize(width: 1, height: 1)
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        color.setFill()
-        UIRectFill(rect)
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return image
-    }
 }
