ed8e444523b8d9efb98fa4ac274f49c5e70d0fd5
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2020-06-17 11:17:52 +0200

    Add Button struct to SummaryViewModel

--- FinniversKit/Sources/Components/TransactionProcessSummaryView/TransactionProcessSummaryViewModel.swift
+++ FinniversKit/Sources/Components/TransactionProcessSummaryView/TransactionProcessSummaryViewModel.swift
@@ -6,14 +6,30 @@ public struct TransactionProcessSummaryViewModel {
     public let title: String
     public let detail: String
     public let description: String
-    public let externalView: TransactionActionButtonViewModel?
+    public let externalView: TransactionProcessSummaryButtonViewModel?
     public let style: String?
 
-    public init(title: String, detail: String, description: String, externalView: TransactionActionButtonViewModel?, style: String?) {
+    public init(title: String, detail: String, description: String, externalView: TransactionProcessSummaryButtonViewModel?, style: String?) {
         self.title = title
         self.detail = detail
         self.description = description
         self.externalView = externalView
         self.style = style
+    }
+}
+
+public struct TransactionProcessSummaryButtonViewModel: TransactionActionButtonViewModel {
+    public var text: String
+    public var style: String?
+    public var action: String?
+    public var url: String?
+    public var fallbackUrl: String?
+
+    public init(text: String, style: String? = nil, action: String? = nil, url: String? = nil, fallbackUrl: String? = nil) {
+        self.text = text
+        self.style = style
+        self.action = action
+        self.url = url
+        self.fallbackUrl = fallbackUrl
     }
 }
