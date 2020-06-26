f194bc2f73042fa0bcca0017de8a1705d2040585
Bastian Stien <bastian.stien@gmail.com>
2020-06-26 10:27:45 +0200

    Add helper enum to demo

--- Demo/Demo/Components/VotingView/VotingDemoView.swift
+++ Demo/Demo/Components/VotingView/VotingDemoView.swift
@@ -37,13 +37,15 @@ public class VotingDemoView: UIView, Tweakable {
     }
 }
 
+// MARK: - Private extensions / types
+
 private extension VotingViewModel {
     static func viewModel(
         icon: FinniversImageAsset = .favoritesXmasFolder,
         title: String = "Her eksperimenterer vi",
         description: String = "Burde vi prioritere å lage denne funksjonen?",
-        leftVotingButton: VotingButtonViewModel = .votingButton(identifier: "left"),
-        rightVotingButton: VotingButtonViewModel = .votingButton(identifier: "right")
+        leftVotingButton: VotingButtonViewModel = .votingButton(votingButtonKind: .left),
+        rightVotingButton: VotingButtonViewModel = .votingButton(votingButtonKind: .right)
     ) -> VotingViewModel {
         VotingViewModel(
             icon: UIImage(named: icon),
@@ -57,20 +59,41 @@ private extension VotingViewModel {
 
 private extension VotingButtonViewModel {
     static func votingButton(
-        identifier: String = "button",
-        title: String = "Stem her plz",
+        votingButtonKind: VotingButtonKind,
         subtitle: String? = nil,
-        icon: FinniversImageAsset = .arrowUp,
         isEnabled: Bool = true,
         isSelected: Bool = false
     ) -> VotingButtonViewModel {
         VotingButtonViewModel(
-            identifier: identifier,
-            title: title,
+            identifier: votingButtonKind.identifier,
+            title: votingButtonKind.title,
             subtitle: subtitle,
-            icon: UIImage(named: icon),
+            icon: UIImage(named: votingButtonKind.icon),
             isEnabled: isEnabled,
             isSelected: isSelected
         )
+    }
+}
+
+private enum VotingButtonKind: String {
+    case left
+    case right
+
+    var identifier: String {
+        rawValue
+    }
+
+    var title: String {
+        switch self {
+        case .left: return "Stem opp"
+        case .right: return "Stem ned"
+        }
+    }
+
+    var icon: FinniversImageAsset {
+        switch self {
+        case .left: return .arrowUp
+        case .right: return .arrowDown
+        }
     }
 }
