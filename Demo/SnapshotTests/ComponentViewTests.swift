51751e577e24dc304d77c4232a308552978e10d2
ninarg <ninagraneggen@gmail.com>
2020-07-15 10:42:27 +0200

    Add snapshots

--- Demo/SnapshotTests/ComponentViewTests.swift
+++ Demo/SnapshotTests/ComponentViewTests.swift
@@ -282,4 +282,8 @@ class ComponentViewTests: XCTestCase {
     func testTransactionProcessSummaryObjectPageView() {
         snapshot(.transactionProcessSummaryObjectPageView)
     }
+
+    func testSearchFilterTags() {
+        snapshot(.searchFilterTags)
+    }
 }
