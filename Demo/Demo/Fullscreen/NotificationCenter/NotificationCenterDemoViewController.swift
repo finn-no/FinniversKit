e68cfb46ae4f2c61dfff52db57a22adbe18b8cff
Felipe Espinoza <fespinozacast@gmail.com>
2020-08-11 14:32:57 +0200

    Change demoView for view controller for the notification center

--- Demo/Demo/Fullscreen/NotificationCenter/NotificationCenterDemoViewController.swift
+++ Demo/Demo/Fullscreen/NotificationCenter/NotificationCenterDemoViewController.swift
@@ -5,53 +5,73 @@
 import FinniversKit
 import FinnUI
 
-final class NotificationCenterDemoView: UIView, Tweakable {
-
+final class NotificationCenterDemoViewController: BaseDemoViewController<UIView>, Tweakable {
     lazy var tweakingOptions: [TweakingOption] = [
         TweakingOption(title: "Empty states", action: {
             self.segments = [self.emptyPersonalNotificationsSegment, self.emptySavedSearchNotificationsSegment]
+            self.notificationsCenterView.savedSearchGroupTitle = "GRUPPER PER DAG"
             self.notificationsCenterView.reloadData()
         }),
-        TweakingOption(title: "Populated states", action: {
+        TweakingOption(title: "Populated states (grouped by search)", action: {
+            self.segments = [self.personalSegment, self.savedSearchSegmentGroupedPerSearch]
+            self.notificationsCenterView.savedSearchGroupTitle = "GRUPPER PER SØK"
+            self.notificationsCenterView.reloadData()
+        }),
+        TweakingOption(title: "Populated states (grouped by day)", action: {
             self.segments = [self.personalSegment, self.savedSearchSegment]
+            self.notificationsCenterView.savedSearchGroupTitle = "GRUPPER PER DAG"
+            self.notificationsCenterView.reloadData()
+        }),
+        TweakingOption(title: "Populated states (flat)", action: {
+            self.segments = [self.personalSegment, self.savedSearchSegmentFlat]
+            self.notificationsCenterView.savedSearchGroupTitle = "KRONOLOGISK"
             self.notificationsCenterView.reloadData()
         })
     ]
+    
+    var bottomSheet: BottomSheet?
 
     private lazy var segments = [
         personalSegment,
-        savedSearchSegment
+        savedSearchSegmentGroupedPerSearch
     ]
 
     private lazy var notificationsCenterView: NotificationCenterView = {
-        let notificationCenterView = NotificationCenterView(withAutoLayout: true)
+        let notificationCenterView = NotificationCenterView(
+            viewModel: .init(
+                groupSelectionTitle: "GRUPPER PER DAG",
+                markAllAsReadButtonTitle: "Marker alt som lest"
+            )
+        )
+        notificationCenterView.translatesAutoresizingMaskIntoConstraints = false
         notificationCenterView.selectedSegment = 1
         notificationCenterView.dataSource = self
         notificationCenterView.delegate = self
         notificationCenterView.remoteImageViewDataSource = self
         return notificationCenterView
     }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        addSubview(notificationsCenterView)
-        notificationsCenterView.fillInSuperview()
-        notificationsCenterView.reloadData()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        view.addSubview(notificationsCenterView)
+        notificationsCenterView.fillInSuperviewSafeArea()
+        
+        tweakingOptions[2].action?()
     }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
 }
 
-extension NotificationCenterDemoView: NotificationCenterViewDataSource {
+extension NotificationCenterDemoViewController: NotificationCenterViewDataSource {
     func numberOfSegments(in view: NotificationCenterView) -> Int {
         segments.count
     }
 
     func notificationCenterView(_ view: NotificationCenterView, titleInSegment segment: Int) -> String {
         segments[segment].title
+    }
+    
+    func notificationCenterView(_ view: NotificationCenterView, includeHeaderIn segment: Int) -> Bool {
+        segments[segment].title == "Lagrede søk"
     }
 
     func notificationCenterView(_ view: NotificationCenterView, numberOfSectionsInSegment segment: Int) -> Int {
@@ -84,9 +104,16 @@ extension NotificationCenterDemoView: NotificationCenterViewDataSource {
     }
 }
 
-extension NotificationCenterDemoView: NotificationCenterViewDelegate {
+extension NotificationCenterDemoViewController: NotificationCenterViewDelegate {
     func notificationCenterView(_ view: NotificationCenterView, didChangeToSegment segment: Int) {
         print("Did change to segment: \(segment)")
+    }
+    
+    func notificationCenterView(_ view: NotificationCenterView, didSelectMarkAllAsReadButtonIn segment: Int) {
+        
+    }
+    
+    func notificationCenterView(_ view: NotificationCenterView, didSelectShowGroupOptions segment: Int) {
     }
 
     func notificationCenterView(_ view: NotificationCenterView, segment: Int, didSelectModelAt indexPath: IndexPath) {
@@ -112,10 +139,6 @@ extension NotificationCenterDemoView: NotificationCenterViewDelegate {
 
     }
 
-    func notificationCenterView(_ view: NotificationCenterView, segment: Int, didSelectMarkAllAsReadButtonIn section: Int) {
-
-    }
-
     func notificationCenterView(_ view: NotificationCenterView, segment: Int, didPullToRefreshUsing refreshControl: UIRefreshControl) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             refreshControl.endRefreshing()
@@ -123,13 +146,13 @@ extension NotificationCenterDemoView: NotificationCenterViewDelegate {
     }
 }
 
-extension NotificationCenterDemoView: FeedbackViewDelegate {
+extension NotificationCenterDemoViewController: FeedbackViewDelegate {
     func feedbackView(_ feedbackView: FeedbackView, didSelectButtonOfType buttonType: FeedbackView.ButtonType, forState state: FeedbackView.State) {
         print("Did select button type: \(buttonType)")
     }
 }
 
-extension NotificationCenterDemoView: RemoteImageViewDataSource {
+extension NotificationCenterDemoViewController: RemoteImageViewDataSource {
     func remoteImageView(_ view: RemoteImageView, cachedImageWithPath imagePath: String, imageWidth: CGFloat) -> UIImage? {
         nil
     }
