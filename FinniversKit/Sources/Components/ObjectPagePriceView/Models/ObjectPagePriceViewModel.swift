a91771a6ad98f56db45696bbe4d0b1a36352d2ac
Bastian Stien <bastian.stien@gmail.com>
2020-09-01 11:11:15 +0200

    Add priceDetails to ObjectPagePriceViewModel

--- FinniversKit/Sources/Components/ObjectPagePriceView/Models/ObjectPagePriceViewModel.swift
+++ FinniversKit/Sources/Components/ObjectPagePriceView/Models/ObjectPagePriceViewModel.swift
@@ -6,20 +6,36 @@ public struct ObjectPagePriceViewModel {
     let mainPriceModel: Price?
     let secondaryPriceModel: Price?
     let links: [PriceLinkButtonViewModel]
+    let priceDetails: [KeyValuePair]
     let adTypeText: String?
 
-    public init(title: String? = nil, totalPrice: String? = nil, subtitle: String? = nil, accessibilityLabel: String? = nil, links: [PriceLinkButtonViewModel] = [], adTypeText: String? = nil) {
+    public init(
+        title: String? = nil,
+        totalPrice: String? = nil,
+        subtitle: String? = nil,
+        accessibilityLabel: String? = nil,
+        links: [PriceLinkButtonViewModel] = [],
+        priceDetails: [KeyValuePair] = [],
+        adTypeText: String? = nil
+    ) {
         var mainPriceModel: Price?
         if let totalPrice = totalPrice {
             mainPriceModel = Price(title: title, totalPrice: totalPrice, subtitle: subtitle, accessibilityLabel: accessibilityLabel)
         }
-        self.init(mainPriceModel: mainPriceModel, links: links, adTypeText: adTypeText)
+        self.init(mainPriceModel: mainPriceModel, links: links, priceDetails: priceDetails, adTypeText: adTypeText)
     }
 
-    public init(mainPriceModel: Price?, secondaryPriceModel: Price? = nil, links: [PriceLinkButtonViewModel], adTypeText: String? = nil) {
+    public init(
+        mainPriceModel: Price?,
+        secondaryPriceModel: Price? = nil,
+        links: [PriceLinkButtonViewModel],
+        priceDetails: [KeyValuePair] = [],
+        adTypeText: String? = nil
+    ) {
         self.mainPriceModel = mainPriceModel
         self.secondaryPriceModel = secondaryPriceModel
         self.links = links
+        self.priceDetails = priceDetails
         self.adTypeText = adTypeText
     }
 
