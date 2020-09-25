e4f78bdc460c3fc240b0b7055763660a44d637a3
Bastian Stien <bastian.stien@gmail.com>
2020-09-25 08:38:51 +0200

    Move viewmodel to separate file

--- FinnUI/Sources/Components/Blink/ObjectPageBlinkView.swift
+++ FinnUI/Sources/Components/Blink/ObjectPageBlinkView.swift
@@ -4,28 +4,6 @@ public protocol ObjectPageBlinkViewDelegate: AnyObject {
     func objectPageBlinkViewDidSelectReadMoreButton(view: ObjectPageBlinkView)
 }
 
-public struct ObjectPageBlinkViewModel {
-    public let icon: UIImage
-    public let title: String
-    public let increasedClickPercentage: Int?
-    public let increasedClickDescription: String
-    public let readMoreButtonTitle: String
-
-    public init(
-        icon: UIImage,
-        title: String,
-        increasedClickPercentage: Int?,
-        increasedClickDescription: String,
-        readMoreButtonTitle: String
-    ) {
-        self.icon = icon
-        self.title = title
-        self.increasedClickPercentage = increasedClickPercentage
-        self.increasedClickDescription = increasedClickDescription
-        self.readMoreButtonTitle = readMoreButtonTitle
-    }
-}
-
 public class ObjectPageBlinkView: UIView {
 
     // MARK: - Public properties
