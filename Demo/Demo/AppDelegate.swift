27c1e19cab66dac0fae0246100d02d015140db95
Vadym Markov <markov.vadym@gmail.com>
2020-10-07 12:36:33 +0200

    Fix demo

--- Demo/Demo/AppDelegate.swift
+++ Demo/Demo/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let userInterfaceStyle = UserInterfaceStyle(rawValue: UserDefaults.standard.integer(forKey: State.currentUserInterfaceStyleKey))
         if let userInterfaceStyle = userInterfaceStyle {
-            FinniversKit.userInterfaceStyleSupport = userInterfaceStyle == .dark ? .forceDark : .forceLight
+            Config.userInterfaceStyleSupport = userInterfaceStyle == .dark ? .forceDark : .forceLight
         } else {
-            FinniversKit.userInterfaceStyleSupport = State.defaultUserInterfaceStyleSupport
+            Config.userInterfaceStyleSupport = State.defaultUserInterfaceStyleSupport
         }
         window = UIWindow(frame: UIScreen.main.bounds)
         if #available(iOS 13.0, *) {
--- FinniversKit/Sources/Config.swift
+++ FinniversKit/Sources/Config.swift
@@ -4,14 +4,14 @@
 
 import Foundation
 
-public struct Config {
-    public enum UserInterfaceStyleSupport {
-        @available(iOS 13.0, *)
-        case dynamic
-        case forceLight
-        case forceDark
-    }
+public enum UserInterfaceStyleSupport {
+    @available(iOS 13.0, *)
+    case dynamic
+    case forceLight
+    case forceDark
+}
 
+public struct Config {
     public static var bundle: Bundle { Bundle.finniversKit }
     public static var fontProvider: FontProvider = DefaultFontProvider()
     public static var isDynamicTypeEnabled: Bool = true
