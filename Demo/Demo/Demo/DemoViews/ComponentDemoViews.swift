9121292a125b412f3490644c6a5bd8360bfe4161
ninarg <ninagraneggen@gmail.com>
2020-10-09 12:54:55 +0200

    Delete corona help popup

--- Demo/Demo/Demo/DemoViews/ComponentDemoViews.swift
+++ Demo/Demo/Demo/DemoViews/ComponentDemoViews.swift
@@ -67,7 +67,6 @@ public enum ComponentDemoViews: String, CaseIterable {
     case favoriteButton
     case motorTransactionEntryAdManagementView
     case chatAvailabilityView
-    case coronaHelpView
     case iconLinkListView
     case motorTransactionEntryObjectPageView
     case extendedProfileView
@@ -215,8 +214,6 @@ public enum ComponentDemoViews: String, CaseIterable {
             return DemoViewController<MotorTransactionEntryAdManagementDemoView>()
         case .chatAvailabilityView:
             return DemoViewController<ChatAvailabilityDemoView>(dismissType: .dismissButton)
-        case .coronaHelpView:
-            return DemoViewController<CoronaHelpDemoView>()
         case .iconLinkListView:
             return DemoViewController<IconLinkListViewDemo>()
         case .motorTransactionEntryObjectPageView:
