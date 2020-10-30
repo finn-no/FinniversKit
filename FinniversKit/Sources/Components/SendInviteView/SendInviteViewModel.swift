bdeaabcf89415fe3f9f2233762f5fa2d10872232
Robin Saleh-Jan <8507719+robinsalehjan@users.noreply.github.com>
2020-10-30 11:42:46 +0100

    Remove profileImage from SendInviteViewModel (#985)
    * Remove profileImage from SendInviteViewModel
    
    * Update snapshot image
    
    Co-authored-by: Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>

--- FinniversKit/Sources/Components/SendInviteView/SendInviteViewModel.swift
+++ FinniversKit/Sources/Components/SendInviteView/SendInviteViewModel.swift
@@ -4,20 +4,17 @@
 
 public struct SendInviteViewModel {
     public let title: String
-    public let profileImage: URL
     public let profileName: String
     public let sendInviteButtonText: String
     public let sendInviteLaterButtonText: String
 
     public init(
         title: String,
-        profileImage: URL,
         profileName: String,
         sendInviteButtonText: String,
         sendInviteLaterButtonText: String
     ) {
         self.title = title
-        self.profileImage = profileImage
         self.profileName = profileName
         self.sendInviteButtonText = sendInviteButtonText
         self.sendInviteLaterButtonText = sendInviteLaterButtonText
