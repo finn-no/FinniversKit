57e93b2ff361f9afa68fabd36237f3c73a40f325
Felipe Espinoza <fespinozacast@gmail.com>
2020-11-09 14:58:14 +0100

    Add SPM package definition

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "FinniversKit",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "FinniversKit",
+            targets: ["FinniversKit"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "FinniversKit",
+            path: "FinniversKit/Sources",
+            resources: [
+                .copy("Assets/Fonts"),
+                .copy("Assets/Sounds"),
+            ]
+        ),
+    ]
+)
