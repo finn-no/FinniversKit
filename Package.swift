fc53a2a17c751127726df5c11c58587617aebc5e
Jonas Olsson <jonas.olsson@finn.no>
2020-12-07 21:41:48 +0100

    Change supported iOS version to 12 and up

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "FinniversKit",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "FinniversKit",
