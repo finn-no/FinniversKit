ae9998a443dda25e60595e80b18329f8a307a223
Håkon Ødegård Løvdal <hakon@loevdal.no>
2021-01-06 12:10:14 +0100

    Add a model and some TextFields to EasyApply

--- FinnUI/Sources/Fullscreen/EasyApply/EasyApplyView.swift
+++ FinnUI/Sources/Fullscreen/EasyApply/EasyApplyView.swift
@@ -1,33 +1,58 @@
 //
-//  EasyApplyView.swift
-//  FinnUI
-//
-//  Created by Håkon Ødegård Løvdal on 21/12/2020.
 //  Copyright © 2020 FINN.no AS. All rights reserved.
 //
 
 import SwiftUI
+import FinniversKit
+
+@available(iOS 13.0, *)
+class EasyApplyViewModel: ObservableObject {
+
+    @Published var name: String
+    @Published var email: String = ""
+    @Published var phone: String = ""
+    @Published var birthYear: String = ""
+    @Published var postalCode: String = ""
+    @Published var currentPosition = ""
+
+    init(name: String = "") {
+        self.name = name
+    }
+}
 
 @available(iOS 14.0, *)
 struct EasyApplyView: View {
+
+    @ObservedObject var model: EasyApplyViewModel
+
     var body: some View {
-        VStack(alignment: .leading) {
-
-            VStack(alignment: .leading) {
-                Text("Søk stilling som:").finnFont(.detail)
-                Text("Systemutvikler / softwareutvikler i Krabbe AS").finnFont(.bodyStrong)
-            }
-            .padding(.bottom, .spacingS)
-
-            VStack(alignment: .leading) {
-                Text("Du er logget inn som:").finnFont(.caption)
-                Text("Test Testesen").finnFont(.bodyRegular)
-            }
-
+        ScrollView {
             VStack(alignment: .leading) {
 
-            }
+                VStack(alignment: .leading) {
+                    Text("Søk stilling som:").finnFont(.detail)
+                    Text("Systemutvikler / softwareutvikler i Krabbe AS").finnFont(.bodyStrong)
+                }
+                .padding(.bottom, .spacingS)
 
+                VStack(alignment: .leading) {
+                    Text("Du er logget inn som:").finnFont(.caption)
+                    Text(model.name).finnFont(.bodyRegular)
+                }
+
+                VStack(alignment: .leading, spacing: .spacingM) {
+                    FinnTextField(placeholder: "Navn", text: $model.name)
+                    FinnTextField(input: .email, placeholder: "Epost", helpText: "Må være en gyldig epost-adresse", text: $model.email)
+                    FinnTextField(input: .phone, placeholder: "Telefon", text: $model.phone)
+                    FinnTextField(input: .number, placeholder: "Fødselsår", text: $model.birthYear)
+                    FinnTextField(input: .number, placeholder: "Postnummer", text: $model.postalCode)
+                    FinnTextField(placeholder: "Nåværende eller siste stilling", text: $model.currentPosition)
+                }
+                .padding(.top, .spacingS)
+
+                Spacer()
+            }
+            .padding(.horizontal, .spacingM)
         }
     }
 }
@@ -35,7 +60,7 @@ struct EasyApplyView: View {
 @available(iOS 14.0, *)
 struct EasyApplyView_Previews: PreviewProvider {
     static var previews: some View {
-        EasyApplyView()
+        EasyApplyView(model: EasyApplyViewModel(name: "Test Testesen"))
             .colorScheme(.light)
             .previewLayout(.device)
     }
