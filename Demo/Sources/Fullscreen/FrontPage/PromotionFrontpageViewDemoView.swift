9f367796b56c136345d32eea83b728f70ad68c2f
Bastian Stien <bastian.stien@gmail.com>
2021-03-24 13:14:00 +0100

    Add demo

---
+++ Demo/Sources/Fullscreen/FrontPage/PromotionFrontpageViewDemoView.swift
@@ -0,0 +1,163 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import FinniversKit
+
+public class PromotionFrontpageViewDemoView: UIView {
+    private let markets = Market.allMarkets
+    private var didSetupView = false
+    private var visibleItems = 20
+
+    private let ads: [Ad] = {
+        var ads = AdFactory.create(numberOfModels: 120)
+        ads.insert(AdFactory.googleDemoAd, at: 4)
+        return ads
+    }()
+
+    private lazy var frontPageView: PromotionFrontPageView = {
+        let view = PromotionFrontPageView(delegate: self, adRecommendationsGridViewDataSource: self)
+        view.model = FrontpageViewDefaultData()
+        view.isRefreshEnabled = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    // MARK: - Setup
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if didSetupView == false {
+            setup()
+            didSetupView = true
+        }
+    }
+
+    private func setup() {
+        addSubview(frontPageView)
+        frontPageView.fillInSuperview()
+        frontPageView.reloadData()
+    }
+}
+
+// MARK: - AdRecommendationsGridViewDelegate
+
+extension PromotionFrontpageViewDemoView: PromotionFrontPageViewDelegate {
+    public func promotionFrontPageViewDidSelectRetryButton(_ frontPageView: PromotionFrontPageView) {
+        frontPageView.reloadData()
+    }
+}
+
+extension PromotionFrontpageViewDemoView: AdRecommendationsGridViewDelegate {
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, willDisplayItemAtIndex index: Int) {
+        if index >= visibleItems - 10 {
+            visibleItems += 10
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { [weak self] in
+                self?.frontPageView.reloadAds()
+            })
+        }
+    }
+
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, didScrollInScrollView scrollView: UIScrollView) {}
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, didSelectItemAtIndex index: Int) {}
+
+    public func adRecommendationsGridViewDidStartRefreshing(_ adRecommendationsGridView: AdRecommendationsGridView) {
+        frontPageView.reloadData()
+    }
+
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, didSelectFavoriteButton button: UIButton, on cell: AdRecommendationCell, at index: Int) {
+        adRecommendationsGridView.updateItem(at: index, isFavorite: !cell.isFavorite)
+    }
+}
+
+// MARK: - AdRecommendationsGridViewDataSource
+
+extension PromotionFrontpageViewDemoView: AdRecommendationsGridViewDataSource {
+    public func numberOfColumns(inAdRecommendationsGridView adRecommendationsGridView: AdRecommendationsGridView) -> AdRecommendationsGridView.ColumnConfiguration? {
+        nil
+    }
+
+    public func numberOfItems(inAdRecommendationsGridView adRecommendationsGridView: AdRecommendationsGridView) -> Int {
+        min(ads.count, visibleItems)
+    }
+
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, cellClassesIn collectionView: UICollectionView) -> [UICollectionViewCell.Type] {
+        [
+            StandardAdRecommendationCell.self,
+            BannerAdDemoCell.self
+        ]
+    }
+
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, heightForItemWithWidth width: CGFloat, at indexPath: IndexPath) -> CGFloat {
+        let model = ads[indexPath.item]
+
+        switch model.adType {
+        case .google:
+            return 300
+        default:
+            return StandardAdRecommendationCell.height(
+                for: model,
+                width: width
+            )
+        }
+    }
+
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let model = ads[indexPath.item]
+
+        switch model.adType {
+        case .google:
+            return collectionView.dequeue(BannerAdDemoCell.self, for: indexPath)
+
+        default:
+            let cell = collectionView.dequeue(StandardAdRecommendationCell.self, for: indexPath)
+            cell.imageDataSource = adRecommendationsGridView
+            cell.delegate = adRecommendationsGridView
+            cell.configure(with: model, atIndex: indexPath.item)
+            return cell
+        }
+    }
+
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, loadImageWithPath imagePath: String, imageWidth: CGFloat, completion: @escaping ((UIImage?) -> Void)) {
+        guard let url = URL(string: imagePath) else {
+            completion(nil)
+            return
+        }
+
+        // Demo code only.
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            usleep(50_000)
+            DispatchQueue.main.async {
+                if let data = data, let image = UIImage(data: data) {
+                    completion(image)
+                } else {
+                    completion(nil)
+                }
+            }
+        }
+
+        task.resume()
+    }
+
+    public func adRecommendationsGridView(_ adRecommendationsGridView: AdRecommendationsGridView, cancelLoadingImageWithPath imagePath: String, imageWidth: CGFloat) {}
+}
+
+// MARK: - MarketsGridViewDelegate
+
+extension PromotionFrontpageViewDemoView: MarketsViewDelegate {
+    public func marketsView(_ marketsGridView: MarketsView, didSelectItemAtIndex index: Int) {}
+}
+
+// MARK: - MarketsGridViewDataSource
+
+extension PromotionFrontpageViewDemoView: MarketsViewDataSource {
+    public func numberOfItems(inMarketsView marketsView: MarketsView) -> Int {
+        markets.count
+    }
+
+    public func marketsView(_ marketsView: MarketsView, modelAtIndex index: Int) -> MarketsViewModel {
+        markets[index]
+    }
+}
