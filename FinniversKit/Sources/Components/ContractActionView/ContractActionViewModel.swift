cfa7c795abfc65977d0a0cd0225dbe6a16834065
Bastian Stien <bastian.stien@gmail.com>
2021-05-04 14:11:25 +0200

    Update model to include optional video link data and title

--- FinniversKit/Sources/Components/ContractActionView/ContractActionViewModel.swift
+++ FinniversKit/Sources/Components/ContractActionView/ContractActionViewModel.swift
@@ -4,14 +4,30 @@
 
 public struct ContractActionViewModel {
     public let identifier: String?
+    public let title: String?
     public let strings: [String]
     public let buttonTitle: String
     public let buttonUrl: URL
+    public let videoLink: VideoLink?
 
-    public init(identifier: String?, strings: [String], buttonTitle: String, buttonUrl: URL) {
+    public init(identifier: String?, title: String? = nil, strings: [String], buttonTitle: String, buttonUrl: URL, videoLink: VideoLink? = nil) {
         self.identifier = identifier
+        self.title = title
         self.strings = strings
         self.buttonTitle = buttonTitle
         self.buttonUrl = buttonUrl
+        self.videoLink = videoLink
+    }
+
+    public struct VideoLink {
+        public let title: String
+        public let videoUrl: URL
+        public let thumbnailUrl: URL
+
+        public init(title: String, videoUrl: URL, thumbnailUrl: URL) {
+            self.title = title
+            self.videoUrl = videoUrl
+            self.thumbnailUrl = thumbnailUrl
+        }
     }
 }
