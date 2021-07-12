35c760dbe8d96dbe7c43275ae21ab5e184f9c34e
ninarg <ninagraneggen@gmail.com>
2021-07-12 12:51:37 +0200

    Add optional action button in ResultView (#1045)

--- FinniversKit/Sources/Fullscreen/ResultView/ResultView.swift
+++ FinniversKit/Sources/Fullscreen/ResultView/ResultView.swift
@@ -4,6 +4,10 @@
 
 import UIKit
 
+public protocol ResultViewDelegate: AnyObject {
+    func resultViewDidTapActionButton(_ resultView: ResultView)
+}
+
 public class ResultView: UIView {
     // MARK: - Private variables
     private lazy var titleLabel: Label = {
@@ -29,10 +33,20 @@ public class ResultView: UIView {
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView(withAutoLayout: true)
         stackView.axis = .vertical
+        stackView.alignment = .center
         return stackView
     }()
 
+    private lazy var actionButton: Button = {
+        let button = Button(style: .callToAction, size: .small)
+        button.addTarget(self, action: #selector(handleTapOnActionButton), for: .touchUpInside)
+        return button
+    }()
+
     private lazy var iconHeightConstraint: NSLayoutConstraint = iconImageView.heightAnchor.constraint(equalToConstant: 40)
+
+    // MARK: Public variables
+    public weak var delegate: ResultViewDelegate?
 
     // MARK: - Initializers
     public override init(frame: CGRect) {
@@ -51,6 +65,7 @@ public class ResultView: UIView {
         title: String,
         titleColor: UIColor = .textPrimary,
         description: String? = nil,
+        actionButtonTitle: String? = nil,
         icon: UIImage? = nil,
         backgroundColor: UIColor = .bgTertiary,
         iconTintColor: UIColor? = .iconSecondary,
@@ -64,6 +79,11 @@ public class ResultView: UIView {
         descriptionLabel.isHidden = description == nil
         iconImageView.image = icon
         iconImageView.isHidden = icon == nil
+
+        actionButton.isHidden = actionButtonTitle == nil
+        if let actionButtonTitle = actionButtonTitle {
+            actionButton.setTitle(actionButtonTitle, for: .normal)
+        }
 
         self.backgroundColor = backgroundColor
         iconImageView.tintColor = iconTintColor
@@ -79,6 +99,7 @@ public class ResultView: UIView {
         stackView.addArrangedSubview(iconImageView)
         stackView.addArrangedSubview(titleLabel)
         stackView.addArrangedSubview(descriptionLabel)
+        stackView.addArrangedSubview(actionButton)
 
         addSubview(stackView)
 
@@ -86,6 +107,13 @@ public class ResultView: UIView {
             iconHeightConstraint,
         ])
 
+        stackView.setCustomSpacing(.spacingM, after: descriptionLabel)
         stackView.centerAndConstraintInSuperview()
+    }
+
+    // MARK: - Actions
+
+    @objc private func handleTapOnActionButton() {
+        delegate?.resultViewDidTapActionButton(self)
     }
 }
