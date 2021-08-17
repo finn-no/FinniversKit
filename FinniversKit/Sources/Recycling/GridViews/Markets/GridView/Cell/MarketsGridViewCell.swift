a9637d412e03ce2cc5cc780ae309d9be005ab1d8
Magnus Tviberg <magnus.arne.tviberg@schibsted.com>
2021-08-17 14:43:18 +0200

    Updated UI on MarketsGridViewCell

--- FinniversKit/Sources/Recycling/GridViews/Markets/GridView/Cell/MarketsGridViewCell.swift
+++ FinniversKit/Sources/Recycling/GridViews/Markets/GridView/Cell/MarketsGridViewCell.swift
@@ -6,7 +6,44 @@ import UIKit
 
 class MarketsGridViewCell: UICollectionViewCell {
     // MARK: - Internal properties
-
+    
+    private let cornerRadius: CGFloat = 16
+    private let shadowColor = Config.colorProvider.tileShadow.cgColor
+    
+    private lazy var smallShadowView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = cornerRadius
+        view.clipsToBounds = true
+        view.layer.applyShadow(ofType: .small)
+        return view
+    }()
+    
+    private lazy var bigShadowView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = cornerRadius
+        view.clipsToBounds = true
+        view.layer.applyShadow(ofType: .big)
+        return view
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = Config.colorProvider.tileBackgroundColor
+        view.layer.cornerRadius = cornerRadius
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private lazy var contentStackView: UIStackView = {
+        let sv = UIStackView(axis: .vertical, spacing: .spacingS, withAutoLayout: true)
+        return sv
+    }()
+    
     private lazy var iconImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -15,17 +52,7 @@ class MarketsGridViewCell: UICollectionViewCell {
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
-    private lazy var externalLinkImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: .webview).withRenderingMode(.alwaysTemplate)
-        imageView.tintColor = .externalLinkColor
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.layer.masksToBounds = true
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
-
+    
     private lazy var titleLabel: Label = {
         let label: Label
         if isHorizontalSizeClassRegular {
@@ -52,27 +79,34 @@ class MarketsGridViewCell: UICollectionViewCell {
 
     private func setup() {
         isAccessibilityElement = true
-
-        addSubview(iconImageView)
-        addSubview(titleLabel)
-        addSubview(externalLinkImageView)
         backgroundColor = .clear
 
+        addSubview(smallShadowView)
+        addSubview(bigShadowView)
+        addSubview(containerView)
+        containerView.addSubview(contentStackView)
+        contentStackView.addArrangedSubviews([iconImageView, titleLabel])
+        
+        bigShadowView.fillInSuperview()
+        smallShadowView.fillInSuperview()
+        containerView.fillInSuperview()
+
         NSLayoutConstraint.activate([
-            iconImageView.topAnchor.constraint(equalTo: topAnchor),
-            iconImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-
-            titleLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: .spacingS),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
-
-            externalLinkImageView.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor),
-            externalLinkImageView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor)
+            iconImageView.heightAnchor.constraint(equalToConstant: 28),
+            iconImageView.widthAnchor.constraint(equalToConstant: 42),
+            
+            contentStackView.widthAnchor.constraint(equalTo: widthAnchor),
+            contentStackView.heightAnchor.constraint(lessThanOrEqualTo: heightAnchor),
+            contentStackView.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
     }
 
     // MARK: - Superclass Overrides
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        styleShadowAfterLayout()
+    }
 
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -88,15 +122,48 @@ class MarketsGridViewCell: UICollectionViewCell {
             iconImageView.image = model?.iconImage
             titleLabel.text = model?.title
             accessibilityLabel = model?.accessibilityLabel
-
-            let showExternalLinkIcon = model?.showExternalLinkIcon ?? false
-            externalLinkImageView.isHidden = !showExternalLinkIcon
         }
     }
 }
 
-private extension UIColor {
-    class var externalLinkColor: UIColor {
-        dynamicColorIfAvailable(defaultColor: .sardine, darkModeColor: .darkSardine)
+// MARK: - Private functions
+private extension MarketsGridViewCell {
+    func styleShadowAfterLayout() {
+        self.bigShadowView.layer.shadowPath = CGPath(
+            roundedRect: self.bigShadowView.bounds,
+            cornerWidth: self.bigShadowView.layer.cornerRadius,
+            cornerHeight: self.bigShadowView.layer.cornerRadius,
+            transform: nil
+        )
+        self.smallShadowView.layer.shadowPath = CGPath(
+            roundedRect: self.smallShadowView.bounds,
+            cornerWidth: self.smallShadowView.layer.cornerRadius,
+            cornerHeight: self.smallShadowView.layer.cornerRadius,
+            transform: nil
+        )
+    }
+}
+
+private extension CALayer {
+    
+    enum ShadowType {
+        case small
+        case big
+    }
+    
+    func applyShadow(ofType type: ShadowType) {
+        self.masksToBounds = false
+        self.shadowColor = shadowColor
+        
+        switch type {
+        case .small:
+            self.shadowOpacity = 0.05
+            self.shadowOffset = CGSize(width: 0, height: -1)
+            self.shadowRadius = 1
+        case .big:
+            self.shadowOpacity = 0.14
+            self.shadowOffset = CGSize(width: 0, height: 4)
+            self.shadowRadius = 14
+        }
     }
 }
