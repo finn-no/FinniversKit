aeabf12f4e4ecf4fce71cea35b018223421a2cc6
Magnus Tviberg <magnustviberg@gmail.com>
2021-08-30 16:49:45 +0200

    Redesign of CompactMarketsView (#1052)
    * Updated the CompactMarketsViewCell
    
    * Updated the CompactMarketsView to use new cell and replaced bottom border with two shadows
    
    * Updated CompactMarketsDemoView to use new height
    
    * Updated Reference images for snapshot testing

--- FinniversKit/Sources/Recycling/GridViews/Markets/CompactView/CompactMarketsViewCell.swift
+++ FinniversKit/Sources/Recycling/GridViews/Markets/CompactView/CompactMarketsViewCell.swift
@@ -6,7 +6,6 @@ class CompactMarketsViewCell: UICollectionViewCell {
 
     var model: MarketsViewModel? {
         didSet {
-            iconImageView.image = model?.iconImage
             titleLabel.text = model?.title
             accessibilityLabel = model?.accessibilityLabel
 
@@ -18,16 +17,10 @@ class CompactMarketsViewCell: UICollectionViewCell {
     // MARK: - Private properties
 
     private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(axis: .horizontal, spacing: .spacingS, withAutoLayout: true)
-        stackView.addArrangedSubviews([iconImageView, titleLabel, externalLinkImageView])
+        let stackView = UIStackView(axis: .horizontal, spacing: Self.itemSpacing, withAutoLayout: true)
+        stackView.addArrangedSubviews([titleLabel, externalLinkImageView])
         stackView.alignment = .center
         return stackView
-    }()
-
-    private lazy var iconImageView: UIImageView = {
-        let imageView = UIImageView(withAutoLayout: true)
-        imageView.contentMode = .scaleAspectFit
-        return imageView
     }()
 
     private lazy var externalLinkImageView: UIImageView = {
@@ -61,14 +54,12 @@ class CompactMarketsViewCell: UICollectionViewCell {
 
     private func setup() {
         isAccessibilityElement = true
-        backgroundColor = .clear
+        backgroundColor = .cellBackgroundColor
 
         contentView.addSubview(stackView)
         stackView.fillInSuperview(insets: Self.contentInsets.forLayoutConstraints)
 
         NSLayoutConstraint.activate([
-            iconImageView.widthAnchor.constraint(equalToConstant: Self.iconImageSize.width),
-            iconImageView.heightAnchor.constraint(equalToConstant: Self.iconImageSize.height),
             externalLinkImageView.widthAnchor.constraint(equalToConstant: Self.externalLinkImageSize.width),
             externalLinkImageView.heightAnchor.constraint(equalToConstant: Self.externalLinkImageSize.height),
         ])
@@ -78,35 +69,28 @@ class CompactMarketsViewCell: UICollectionViewCell {
 
     override func prepareForReuse() {
         super.prepareForReuse()
-        iconImageView.image = nil
         titleLabel.text = ""
         accessibilityLabel = ""
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
-
         layer.cornerRadius = frame.height / 2
-        layer.borderWidth = 1
-        layer.borderColor = UIColor.externalLinkColor.cgColor
     }
 }
 
 // MARK: - Size calculations
 
 extension CompactMarketsViewCell {
-    static let borderWidth: CGFloat = 1
-    static let contentInsets = UIEdgeInsets(vertical: 10, horizontal: .spacingM)
-    static let titleLabelStyle = Label.Style.caption
-    static let cellHeight: CGFloat = 44
+    static let contentInsets = UIEdgeInsets(vertical: .spacingS, horizontal: 12)
+    static let titleLabelStyle = Label.Style.captionStrong
+    static let cellHeight: CGFloat = 34
     static let itemSpacing = CGFloat.spacingS
-    static let iconImageSize = CGSize(width: 24, height: 24)
-    static let externalLinkImageSize = CGSize(width: 16, height: 16)
+    static let externalLinkImageSize = CGSize(width: 12, height: 12)
 
     static func size(for model: MarketsViewModel) -> CGSize {
         var widths: [CGFloat] = [
             contentInsets.leading,
-            iconImageSize.width,
             itemSpacing,
             width(for: model.title)
         ]
@@ -120,15 +104,7 @@ extension CompactMarketsViewCell {
 
         widths.append(contentInsets.trailing)
 
-        let heights: [CGFloat] = [
-            borderWidth,
-            contentInsets.top,
-            iconImageSize.height,
-            contentInsets.bottom,
-            borderWidth
-        ]
-
-        return CGSize(width: widths.reduce(0, +), height: heights.reduce(0, +))
+        return CGSize(width: widths.reduce(0, +), height: cellHeight)
     }
 
     private static func width(for title: String) -> CGFloat {
@@ -147,10 +123,14 @@ extension CompactMarketsViewCell {
 // MARK: - Private extensions
 
 private extension UIColor {
-    class var externalLinkColor: UIColor {
-        dynamicColorIfAvailable(defaultColor: .sardine, darkModeColor: .darkSardine)
-    }
-}
+     class var externalLinkColor: UIColor {
+         return .blueGray600
+     }
+
+     class var cellBackgroundColor: UIColor {
+         return .dynamicColorIfAvailable(defaultColor: .coolGray100, darkModeColor: .blueGray700)
+     }
+ }
 
 private extension UIEdgeInsets {
     var forLayoutConstraints: UIEdgeInsets {
