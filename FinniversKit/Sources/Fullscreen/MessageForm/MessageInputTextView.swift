79dcf0aa9eab4a3fa1576c05175446fb70fef60c
jorgen.aase <jorgen.aase@finn.no>
2021-09-30 09:17:16 +0200

    Added telephone field to contact form

--- FinniversKit/Sources/Fullscreen/MessageForm/MessageInputTextView.swift
+++ FinniversKit/Sources/Fullscreen/MessageForm/MessageInputTextView.swift
@@ -12,6 +12,13 @@ class MessageInputTextView: UIView {
 
     // MARK: - UI properties
 
+    private lazy var textViewLabel: Label = {
+        let label = Label(style: .captionStrong)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return label
+    }()
+
     private lazy var textView: TextView = {
         let textView = TextView(withAutoLayout: true)
         textView.delegate = self
@@ -19,6 +26,27 @@ class MessageInputTextView: UIView {
         return textView
     }()
 
+    private lazy var disclaimerLabel: Label = {
+        let label = Label(style: .detail)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return label
+    }()
+
+    private lazy var phoneViewLabel: Label = {
+        let label = Label(style: .captionStrong)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return label
+    }()
+    
+    private lazy var phoneView: TextView = {
+        let textView = TextView(withAutoLayout: true)
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
     private lazy var additionalInfoLabel: Label = {
         let label = Label(style: .detail)
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -41,6 +69,15 @@ class MessageInputTextView: UIView {
         }
     }
 
+    var telephone: String {
+        get {
+            return phoneView.text ?? ""
+        }
+        set {
+            textView.text = newValue
+        }
+    }
+    
     var inputEnabled: Bool = true {
         didSet {
             textView.isUserInteractionEnabled = inputEnabled
@@ -58,24 +95,49 @@ class MessageInputTextView: UIView {
         fatalError()
     }
 
-    required init(additionalInfoText: String) {
+    required init(disclaimerText: String, additionalInfoText: String, messageLabel: String, messageHint: String, telephoneLabel: String, telephoneHint: String) {
         super.init(frame: .zero)
 
         setup()
+        disclaimerLabel.text = disclaimerText
         additionalInfoLabel.text = additionalInfoText
+        textViewLabel.text = messageLabel
+        textView.placeholderText = messageHint
+        phoneViewLabel.text = telephoneLabel
+        phoneView.placeholderText = telephoneHint
     }
 
     private func setup() {
+        addSubview(textViewLabel)
         addSubview(textView)
+        addSubview(disclaimerLabel)
+        addSubview(phoneViewLabel)
+        addSubview(phoneView)
         addSubview(additionalInfoLabel)
 
         NSLayoutConstraint.activate([
-            textView.topAnchor.constraint(equalTo: topAnchor, constant: .spacingS),
-            textView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingS),
+            textViewLabel.topAnchor.constraint(equalTo: topAnchor, constant: .spacingM),
+            textViewLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
+            textViewLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingS),
+            textViewLabel.bottomAnchor.constraint(equalTo: textView.topAnchor, constant: -.spacingS),
+            
+            textView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
             textView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingS),
-            textView.bottomAnchor.constraint(equalTo: additionalInfoLabel.topAnchor, constant: -.spacingS),
+            textView.bottomAnchor.constraint(equalTo: disclaimerLabel.topAnchor, constant: -.spacingS),
 
-            additionalInfoLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingS),
+            disclaimerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
+            disclaimerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingS),
+            disclaimerLabel.bottomAnchor.constraint(equalTo: phoneViewLabel.topAnchor, constant: -.spacingM),
+            
+            phoneViewLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
+            phoneViewLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingS),
+            phoneViewLabel.bottomAnchor.constraint(equalTo: phoneView.topAnchor, constant: -.spacingS),
+            
+            phoneView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
+            phoneView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingS),
+            phoneView.bottomAnchor.constraint(equalTo: additionalInfoLabel.topAnchor, constant: -.spacingS),
+            
+            additionalInfoLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
             additionalInfoLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.spacingS),
             additionalInfoLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -.spacingS)
         ])
