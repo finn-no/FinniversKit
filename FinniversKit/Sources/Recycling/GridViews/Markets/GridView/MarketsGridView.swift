81b4df7e6013853d3a6c13325f67bebc488f598a
Jonas Olsson <jonas.olsson@finn.no>
2021-11-04 15:23:00 +0100

    WIP

--- FinniversKit/Sources/Recycling/GridViews/Markets/GridView/MarketsGridView.swift
+++ FinniversKit/Sources/Recycling/GridViews/Markets/GridView/MarketsGridView.swift
@@ -23,6 +23,11 @@ public class MarketsGridView: UIView, MarketsView {
     private weak var delegate: MarketsViewDelegate?
     private weak var dataSource: MarketsViewDataSource?
 
+    private let itemSize = CGSize(width: 92, height: 72)
+    private let itemSpacing: CGFloat = .spacingS
+    private let sideMargin: CGFloat = .spacingM
+    private let rowSpacing: CGFloat = .spacingS
+
     // MARK: - Setup
 
     public init(frame: CGRect = .zero, delegate: MarketsViewDelegate, dataSource: MarketsViewDataSource) {
@@ -44,12 +49,19 @@ public class MarketsGridView: UIView, MarketsView {
         setup()
     }
 
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        //if collectionView.bounds.width < collectionView.contentSize.width {
+            addGradientMask(to: self)
+        //}
+    }
+
     private func setup() {
         clipsToBounds = false
         backgroundColor = .clear
         collectionView.register(MarketsGridViewCell.self)
         addSubview(collectionView)
-        
+
         collectionView.fillInSuperview()
     }
 
@@ -60,44 +72,57 @@ public class MarketsGridView: UIView, MarketsView {
     }
 
     public func calculateSize(constrainedTo width: CGFloat) -> CGSize {
-        let size = itemSize(for: width)
-        let line = lineSpacing(for: width)
-        let inst = insets(for: width)
+        let gridInsets = insets(for: width)
         let rows = numberOfRows(for: width)
 
-        let height = (size.height * CGFloat(rows)) + (line * CGFloat(rows - 1)) + inst.top + inst.bottom
+        let height = (itemSize.height * CGFloat(rows)) + (rowSpacing * CGFloat(rows - 1)) + gridInsets.top + gridInsets.bottom
 
         return CGSize(width: width, height: height)
     }
 
     // MARK: - Private
-    
-    private func numberOfRows(for viewWidth: CGFloat) -> CGFloat {
-        return MarketsGridViewLayoutConfiguration(width: viewWidth).itemsPerRow
-    }
 
-    private func itemSize(for viewWidth: CGFloat) -> CGSize {
-        let screenWidth = MarketsGridViewLayoutConfiguration(width: viewWidth)
-        let numberOfSpacesBetweenOnScreenItems = screenWidth.columns - 1
-        let spaceBetweenItems = screenWidth.interimSpacing * numberOfSpacesBetweenOnScreenItems
-        let margins = screenWidth.sideMargins * 2
-        let itemSize = CGSize(width: (viewWidth - spaceBetweenItems - margins) / screenWidth.columns, height: screenWidth.itemHeight)
-        return itemSize
+    private func numberOfRows(for viewWidth: CGFloat) -> CGFloat {
+        guard
+            let numberOfItems = dataSource?.numberOfItems(inMarketsView: self),
+            numberOfItems > 0
+        else {
+            return 1
+        }
+        let items = CGFloat(numberOfItems)
+        let numberOfFittingItems = viewWidth / (itemSize.width + itemSpacing)
+        let fitFactor = numberOfFittingItems / items
+        if fitFactor > 0.75 {
+            return 1
+        }
+        return 2
     }
 
     private func insets(for viewWidth: CGFloat) -> UIEdgeInsets {
-        let screenWidth = MarketsGridViewLayoutConfiguration(width: viewWidth)
-        return screenWidth.edgeInsets
+        UIEdgeInsets(top: 0,
+                     left: sideMargin,
+                     bottom: 0,
+                     right: sideMargin)
     }
 
-    private func lineSpacing(for viewWidth: CGFloat) -> CGFloat {
-        let screenWidth = MarketsGridViewLayoutConfiguration(width: viewWidth)
-        return screenWidth.lineSpacing
+    private func lineSpacing() -> CGFloat {
+        return rowSpacing
     }
 
     private func interimSpacing(for viewWidth: CGFloat) -> CGFloat {
-        let screenWidth = MarketsGridViewLayoutConfiguration(width: viewWidth)
-        return screenWidth.interimSpacing
+        return itemSpacing
+    }
+
+    private func addGradientMask(to view: UIView) {
+        let transparent = UIColor.white.withAlphaComponent(0.2).cgColor
+        let opaque = UIColor.white.cgColor
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: 0, y: -5, width: view.bounds.width, height: view.bounds.height + 10)
+        gradientLayer.colors = [opaque, transparent]
+        gradientLayer.locations = [0, 1]
+        gradientLayer.startPoint = CGPoint(x: 0.8, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        view.layer.mask = gradientLayer
     }
 }
 
@@ -105,15 +130,35 @@ public class MarketsGridView: UIView, MarketsView {
 
 extension MarketsGridView: UICollectionViewDelegateFlowLayout {
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return itemSize(for: bounds.width)
+        return itemSize
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return insets(for: bounds.width)
+        //return insets(for: bounds.width)
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
+            return insets(for: bounds.width)
+        }
+        let cellWidth: CGFloat = flowLayout.itemSize.width
+        let cellSpacing: CGFloat = flowLayout.minimumInteritemSpacing
+        var cellCount = CGFloat(collectionView.numberOfItems(inSection: section))
+        var collectionWidth = collectionView.frame.size.width
+        var totalWidth: CGFloat
+        collectionWidth -= collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right
+        repeat {
+            totalWidth = cellWidth * cellCount + cellSpacing * (cellCount - 1)
+            cellCount -= 1
+        } while totalWidth >= collectionWidth
+
+        if totalWidth > 0 {
+            let edgeInset = (collectionWidth - totalWidth) / 2
+            return UIEdgeInsets.init(top: flowLayout.sectionInset.top, left: edgeInset, bottom: flowLayout.sectionInset.bottom, right: edgeInset)
+        } else {
+            return flowLayout.sectionInset
+        }
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return lineSpacing(for: bounds.width)
+        return lineSpacing()
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
