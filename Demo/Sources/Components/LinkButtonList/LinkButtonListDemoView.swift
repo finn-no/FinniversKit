a623658532ff421b99ef8aa235a349c6cfb51f9c
Bastian Stien <bastian.stien@gmail.com>
2021-11-16 10:18:19 +0100

    Fix: LinkButtonView customize button style (#1074)

--- Demo/Sources/Components/LinkButtonList/LinkButtonListDemoView.swift
+++ Demo/Sources/Components/LinkButtonList/LinkButtonListDemoView.swift
@@ -4,7 +4,16 @@
 
 import FinniversKit
 
-class LinkButtonListDemoView: UIView {
+class LinkButtonListDemoView: UIView, Tweakable {
+    lazy var tweakingOptions: [TweakingOption] = [
+        .init(title: "Misc. buttons", action: { [weak self] in
+            self?.configure(viewModels: .defaults)
+        }),
+        .init(title: "Flat buttons", action: { [weak self] in
+            self?.configure(viewModels: .alternateStyle)
+        })
+    ]
+
     private lazy var linkListView: LinkButtonListView = {
         let view = LinkButtonListView(withAutoLayout: true)
         view.delegate = self
@@ -16,6 +25,7 @@ class LinkButtonListDemoView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
+        tweakingOptions.first?.action?()
     }
 
     public required init?(coder aDecoder: NSCoder) { fatalError() }
@@ -23,8 +33,6 @@ class LinkButtonListDemoView: UIView {
     // MARK: - Setup
 
     private func setup() {
-        linkListView.configure(with: .defaults)
-
         addSubview(linkListView)
         NSLayoutConstraint.activate([
             linkListView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .spacingM),
@@ -32,13 +40,23 @@ class LinkButtonListDemoView: UIView {
             linkListView.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
     }
+
+    // MARK: - Private methods
+
+    func configure(viewModels: [LinkButtonViewModel]) {
+        linkListView.configure(with: viewModels)
+    }
 }
+
+// MARK: - LinkButtonListViewDelegate
 
 extension LinkButtonListDemoView: LinkButtonListViewDelegate {
     func linksListView(_ view: LinkButtonListView, didTapButtonWithIdentifier identifier: String?, url: URL) {
-        print("🔥🔥🔥🔥 \(#function) - buttonIdentifier: \(identifier ?? "") - url: \(url)")
+        print("✅ \(#function) - buttonIdentifier: \(identifier ?? "") - url: \(url)")
     }
 }
+
+// MARK: - Private extensions
 
 private extension Array where Element == LinkButtonViewModel {
     static var `defaults`: [LinkButtonViewModel] = {
@@ -68,7 +86,44 @@ private extension Array where Element == LinkButtonViewModel {
                 subtitle: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
                 linkUrl: URL(string: "https://www.finn.no/")!,
                 isExternal: false
+            )
+        ]
+    }()
+
+    static var alternateStyle: [LinkButtonViewModel] = {
+        [
+            LinkButtonViewModel(
+                buttonIdentifier: "b1",
+                buttonTitle: "Button 1",
+                linkUrl: URL(string: "https://www.finn.no/")!,
+                isExternal: false,
+                buttonStyle: .flat,
+                buttonSize: .normal
             ),
+            LinkButtonViewModel(
+                buttonIdentifier: "b2",
+                buttonTitle: "Button 2",
+                linkUrl: URL(string: "https://www.finn.no/")!,
+                isExternal: false,
+                buttonStyle: .flat,
+                buttonSize: .normal
+            ),
+            LinkButtonViewModel(
+                buttonIdentifier: "b3",
+                buttonTitle: "Button 3",
+                linkUrl: URL(string: "https://www.finn.no/")!,
+                isExternal: false,
+                buttonStyle: .flat,
+                buttonSize: .normal
+            ),
+            LinkButtonViewModel(
+                buttonIdentifier: "b4",
+                buttonTitle: "Button 4",
+                linkUrl: URL(string: "https://www.finn.no/")!,
+                isExternal: false,
+                buttonStyle: .flat,
+                buttonSize: .normal
+            )
         ]
     }()
 }
