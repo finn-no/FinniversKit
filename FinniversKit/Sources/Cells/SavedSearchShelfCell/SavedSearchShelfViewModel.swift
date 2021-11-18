34c30810b5422dd1cc286cb465b473fa687fb6e8
sutha <suthananth.arulanantham@schibsted.com>
2021-11-18 15:02:13 +0100

    Added new parameter to SavedSearchViewModel

--- FinniversKit/Sources/Cells/SavedSearchShelfCell/SavedSearchShelfViewModel.swift
+++ FinniversKit/Sources/Cells/SavedSearchShelfCell/SavedSearchShelfViewModel.swift
@@ -5,12 +5,14 @@ public struct SavedSearchShelfViewModel {
     public let imageUrlString: String?
     public let id: Int
     public let adId: Int
+    public let created: Date
     
-    public init(id: Int = Int.random(in: 0...100_000), title: String, imageUrlString: String?, adId: Int = Int.random(in: 0...100_000)) {
+    public init(id: Int = Int.random(in: 0...100_000), title: String, imageUrlString: String?, adId: Int = Int.random(in: 0...100_000), created: Date = Date()) {
         self.title = title
         self.imageUrlString = imageUrlString
         self.id = id
         self.adId = adId
+        self.created = created
     }
 }
 
