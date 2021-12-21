f6cbc20488f8ecf27ee73ad231a293b9d1a72681
sutha <suthananth.arulanantham@schibsted.com>
2021-12-21 18:31:03 +0100

    fixed section header title

--- FinniversKit/Sources/Fullscreen/FrontPage/ViewModels/FrontPageShelfViewModel.swift
+++ FinniversKit/Sources/Fullscreen/FrontPage/ViewModels/FrontPageShelfViewModel.swift
@@ -33,8 +33,8 @@ public class FrontPageShelfViewModel {
         recentlyFavoritedItems.remove(at: index)
     }
     
-    public func titleForSection(at index: IndexPath) -> String {
-        guard index.section < sectionTitles.count else { return "" }
-        return sectionTitles[index.section]
+    public func titleForSection(at index: Int) -> String {
+        guard index < sectionTitles.count else { return "" }
+        return sectionTitles[index]
     }
 }
