31fd92dd87ac360d06f7eff6925cd5f01c50cb01
Bastian Stien <bastian.stien@gmail.com>
2022-01-20 14:42:09 +0100

    Remove stuff from NavigationController

--- Demo/Sources/Demo/NavigationController.swift
+++ Demo/Sources/Demo/NavigationController.swift
@@ -13,45 +13,19 @@ class NavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
 
+    private func setup() {
         navigationBar.isTranslucent = false
-        updateColors(animated: false)
-        NotificationCenter.default.addObserver(self, selector: #selector(userInterfaceStyleDidChange), name: .didChangeUserInterfaceStyle, object: nil)
-    }
+        let separatorColor: UIColor = .textDisabled //DARK
+        let barTintColor: UIColor = .bgPrimary
+        let tintColor: UIColor = .textAction
 
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-
-        #if swift(>=5.1)
-        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
-            updateColors(animated: true)
-        }
-        #endif
-    }
-
-    @objc private func userInterfaceStyleDidChange() {
-        updateColors(animated: true)
-    }
-
-    private func updateColors(animated: Bool) {
-        func setupColors() {
-            let separatorColor: UIColor = .textDisabled //DARK
-            let barTintColor: UIColor = .bgPrimary
-            let tintColor: UIColor = .textAction
-
-            setBottomBorderColor(navigationBar: navigationBar, color: separatorColor, height: 0.5)
-            navigationBar.barTintColor = barTintColor
-            navigationBar.tintColor = tintColor
-            navigationBar.layoutIfNeeded()
-        }
-
-        if animated {
-            UIView.animate(withDuration: 0.3) {
-                setupColors()
-            }
-        } else {
-            setupColors()
-        }
+        setBottomBorderColor(navigationBar: navigationBar, color: separatorColor, height: 0.5)
+        navigationBar.barTintColor = barTintColor
+        navigationBar.tintColor = tintColor
+        navigationBar.layoutIfNeeded()
     }
 
     private func setBottomBorderColor(navigationBar: UINavigationBar, color: UIColor, height: CGFloat) {
