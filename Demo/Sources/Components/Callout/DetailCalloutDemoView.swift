ab65c2c3a080ab041547ee0428922ed4ecac19e3
Nina Graneggen <ninagraneggen@gmail.com>
2022-02-11 13:25:24 +0100

    Add new DetailCalloutView with demos

---
+++ Demo/Sources/Components/Callout/DetailCalloutDemoView.swift
@@ -0,0 +1,57 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import FinniversKit
+
+public class DetailCalloutDemoView: UIView {
+
+    private lazy var calloutViews: [DetailCalloutView] = {
+        DetailCalloutView.Direction.allCases.map { (direction) -> DetailCalloutView in
+            let view = DetailCalloutView(direction: direction)
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: calloutViews)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.distribution = .fillEqually
+        view.alignment = .center
+        view.spacing = .spacingXL
+        return view
+    }()
+
+    // MARK: - Init
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup
+
+    private func setup() {
+        addSubview(stackView)
+
+        let text = "Nyhet!"
+
+        stackView.arrangedSubviews.forEach {
+            guard let calloutView = $0 as? DetailCalloutView else { return }
+
+            calloutView.hide()
+            calloutView.show(withText: text, duration: 0)
+        }
+
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
