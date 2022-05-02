8fb9502def9cf6800e1b3b34229e811173b472c9
Håkon Ødegård Løvdal <hakon@loevdal.no>
2022-05-02 14:41:05 +0200

    Add snapshot test for MonthAndYearPickerView

--- Demo/SnapshotTests/ComponentViewTests.swift
+++ Demo/SnapshotTests/ComponentViewTests.swift
@@ -294,4 +294,8 @@ class ComponentViewTests: XCTestCase {
     func testDetailCallout() {
         snapshot(.detailCallout)
     }
+
+    func testMonthAndYearPickerView() {
+        snapshot(.monthAndYearPickerView)
+    }
 }
