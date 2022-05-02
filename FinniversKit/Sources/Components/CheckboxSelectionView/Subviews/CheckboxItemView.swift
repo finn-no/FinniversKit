08ffac392f7a1273b2b41f35efac7ccefb9463b9
Bastian Stien <bastian.stien@gmail.com>
2022-05-02 21:06:39 +0200

    Add CheckboxItemView

---
+++ FinniversKit/Sources/Components/CheckboxSelectionView/Subviews/CheckboxItemView.swift
@@ -0,0 +1,171 @@
+import UIKit
+
+class CheckboxItemView: UIView {
+
+    // MARK: - Internal properties
+
+    var isSelected: Bool = false {
+        didSet {
+            updateSelection()
+        }
+    }
+
+    // MARK: - Private properties
+
+    private let model: CheckboxItemModel
+    private let configuration: Configuration
+    private lazy var contentView = UIView(withAutoLayout: true)
+    private lazy var checkboxView = AnimatedCheckboxView(frame: .zero)
+    private lazy var textStackView = UIStackView(axis: .vertical, spacing: .spacingXS, withAutoLayout: true)
+
+    private lazy var titleLabel: Label = {
+        let label = Label(style: .bodyStrong, withAutoLayout: true)
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private lazy var descriptionLabel: Label = {
+        let label = Label(style: .body, withAutoLayout: true)
+        label.textColor = .textSecondary
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView(withAutoLayout: true)
+        imageView.tintColor = .textPrimary
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    // MARK: - Init
+
+    init(model: CheckboxItemModel, configuration: Configuration, withAutoLayout: Bool) {
+        self.model = model
+        self.configuration = configuration
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = !withAutoLayout
+        setup()
+    }
+
+    required init?(coder: NSCoder) { fatalError() }
+
+    // MARK: - Setup
+
+    private func setup() {
+        backgroundColor = .clear
+        contentView.backgroundColor = .bgPrimary
+
+        addCornerRadius(
+            maskedCorners:configuration.maskedCorners,
+            cornerRadius: configuration.cornerRadius
+        )
+
+        /// Because of the margins outside contentView we need to adjust the corner radius to
+        /// not make it look weird.
+        contentView.addCornerRadius(
+            maskedCorners: configuration.maskedCorners,
+            cornerRadius: configuration.cornerRadius - configuration.spacing
+        )
+
+        titleLabel.text = model.title
+        descriptionLabel.text = model.description
+        iconImageView.image = model.icon
+        checkboxView.animateSelection(selected: model.isInitiallySelected)
+
+        textStackView.addArrangedSubviews([titleLabel, descriptionLabel])
+
+        addSubview(contentView)
+        contentView.addSubview(checkboxView)
+        contentView.addSubview(textStackView)
+        contentView.addSubview(iconImageView)
+
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: topAnchor, constant: configuration.spacing),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: configuration.spacing),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -configuration.spacing),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -configuration.spacing),
+
+            checkboxView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: .spacingL),
+            checkboxView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: .spacingM),
+
+            textStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: .spacingM),
+            textStackView.leadingAnchor.constraint(equalTo: checkboxView.trailingAnchor, constant: .spacingM),
+            textStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -.spacingM),
+
+            iconImageView.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor, constant: .spacingS),
+            iconImageView.leadingAnchor.constraint(equalTo: textStackView.trailingAnchor, constant: .spacingM),
+            iconImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -.spacingM),
+            iconImageView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -.spacingM),
+            iconImageView.centerYAnchor.constraint(equalTo: checkboxView.centerYAnchor),
+
+            iconImageView.widthAnchor.constraint(equalToConstant: 24),
+            iconImageView.heightAnchor.constraint(equalToConstant: 24),
+        ])
+    }
+
+    // MARK: - Private methods
+
+    private func updateSelection() {
+        checkboxView.animateSelection(selected: isSelected)
+        backgroundColor = isSelected ? .primaryBlue : .clear
+    }
+}
+
+// MARK: - Inner types
+
+extension CheckboxItemView {
+    enum Position {
+        case first
+        case last
+        case middle
+        case theOnlyOne
+    }
+
+    struct Configuration {
+        let spacing: CGFloat
+        let cornerRadius: CGFloat
+        let position: Position
+
+        fileprivate var contentSpacingTop: CGFloat {
+            switch position {
+            case .first, .theOnlyOne:
+                return spacing
+            case .last, .middle:
+                return spacing
+            }
+        }
+
+        fileprivate var contentSpacingBottom: CGFloat {
+            switch position {
+            case .first, .middle:
+                return spacing
+            case .last, .theOnlyOne:
+                return spacing
+            }
+        }
+
+        fileprivate var maskedCorners: CACornerMask {
+            switch position {
+            case .first:
+                return [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            case .last:
+                return [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            case .theOnlyOne:
+                return [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            default:
+                return []
+            }
+        }
+    }
+}
+
+// MARK: - Private extensions
+
+private extension UIView {
+    func addCornerRadius(maskedCorners: CACornerMask, cornerRadius: CGFloat) {
+        guard !maskedCorners.isEmpty else { return }
+        layer.maskedCorners = maskedCorners
+        layer.cornerRadius = cornerRadius
+    }
+}
