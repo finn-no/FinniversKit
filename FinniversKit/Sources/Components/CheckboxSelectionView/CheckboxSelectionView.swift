d7224206005e1113a6fb8467181780a4858bba78
Bastian Stien <bastian.stien@gmail.com>
2022-05-03 10:48:33 +0200

    Pull out the model into a separate file

--- FinniversKit/Sources/Components/CheckboxSelectionView/CheckboxSelectionView.swift
+++ FinniversKit/Sources/Components/CheckboxSelectionView/CheckboxSelectionView.swift
@@ -1,24 +1,5 @@
 import UIKit
 
-public struct CheckboxItemModel {
-    public let title: String
-    public let description: Description
-    public let icon: UIImage
-    public let isInitiallySelected: Bool
-
-    public init(title: String, description: Description, icon: UIImage, isInitiallySelected: Bool) {
-        self.title = title
-        self.description = description
-        self.icon = icon
-        self.isInitiallySelected = isInitiallySelected
-    }
-
-    public enum Description {
-        case plain(String)
-        case attributed(NSAttributedString)
-    }
-}
-
 public class CheckboxSelectionView: UIView {
 
     // MARK: - Public properties
