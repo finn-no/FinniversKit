3d09a2b6459e4226347ce968e2c16f5b33d4b1c7
Bastian Stien <bastian.stien@gmail.com>
2022-05-10 09:30:32 +0200

    SelectionItemModel: Add property identifier

--- FinniversKit/Sources/Components/SelectionListView/Models/SelectionItemModel.swift
+++ FinniversKit/Sources/Components/SelectionListView/Models/SelectionItemModel.swift
@@ -1,12 +1,14 @@
 import Foundation
 
 public struct SelectionItemModel {
+    public let identifier: String?
     public let title: String
     public let description: Description
     public let icon: UIImage
     public let isInitiallySelected: Bool
 
-    public init(title: String, description: Description, icon: UIImage, isInitiallySelected: Bool) {
+    public init(identifier: String?, title: String, description: Description, icon: UIImage, isInitiallySelected: Bool) {
+        self.identifier = identifier
         self.title = title
         self.description = description
         self.icon = icon
