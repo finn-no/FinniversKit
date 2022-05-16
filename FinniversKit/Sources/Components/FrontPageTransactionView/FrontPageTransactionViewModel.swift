03d70a591d3991c5cb262fefe1c7cfbb6e08d4be
Suthananth Arulanantham <vikram.sh86@gmail.com>
2022-05-16 14:21:46 +0200

    Feature/frontpage tjt sold (#1111)
    * implemented FrontPageTransactionView
    
    * Added FrontPageTransacionView to Frontpage and updated SnapshotTests
    
    * updated access level for TransactionFeedViewModel
    
    * PR Comment update
    
    Co-authored-by: sutha <suthananth.arulanantham@schibsted.com>

---
+++ FinniversKit/Sources/Components/FrontPageTransactionView/FrontPageTransactionViewModel.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+public struct FrontPageTransactionViewModel {
+    public let headerTitle: String
+    public let title: String
+    public let subtitle: String
+    public let imageUrl: String?
+    public let adId: Int?
+    public let transactionId: String?
+
+    public init(
+        headerTitle: String,
+        title: String,
+        subtitle: String,
+        imageUrl: String? = nil,
+        adId: Int? = nil,
+        transactionId: String? = nil
+    ) {
+        self.headerTitle = headerTitle
+        self.title = title
+        self.subtitle = subtitle
+        self.imageUrl = imageUrl
+        self.adId = adId
+        self.transactionId = transactionId
+    }
+}
