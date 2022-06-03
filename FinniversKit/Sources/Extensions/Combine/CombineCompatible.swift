79029b6e3bf2dfded4c4448e825c9337e6aef442
Attila Szilagyi <attila.szilagyi@schibsted.com>
2022-06-03 13:26:42 +0200

    Add more spaces.

--- FinniversKit/Sources/Extensions/Combine/CombineCompatible.swift
+++ FinniversKit/Sources/Extensions/Combine/CombineCompatible.swift
@@ -3,7 +3,9 @@ import Combine
 
 /// Extending the `UIControl` types to be able to produce a `UIControl.Event` publisher.
 public protocol CombineCompatible { }
+
 extension UIControl: CombineCompatible { }
+
 public extension CombineCompatible where Self: UIControl {
     func publisher(for events: UIControl.Event) -> UIControlPublisher<Self> {
         return UIControlPublisher(control: self, events: events)
