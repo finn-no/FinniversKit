27cc7038b29fb6fb6b0377bdb08056bf11ccd17b
Bastian Stien <bastian.stien@gmail.com>
2022-07-07 12:11:54 +0200

    Update demo

--- Demo/Sources/Components/AddressCardView/AddressCardDemoView.swift
+++ Demo/Sources/Components/AddressCardView/AddressCardDemoView.swift
@@ -4,12 +4,17 @@
 
 import FinniversKit
 
-final class AddressCardDemoView: UIView {
-    private lazy var view: AddressCardView = {
+final class AddressCardDemoView: UIView, Tweakable {
+
+    lazy var tweakingOptions: [TweakingOption] = [
+        TweakingOption(title: "Default") { [weak self] in self?.addressCardView.configure(with: .default) },
+        TweakingOption(title: "Wihout directions button") { [weak self] in self?.addressCardView.configure(with: .withoutDirectionsButton) },
+    ]
+
+    private lazy var addressCardView: AddressCardView = {
         let view = AddressCardView(withAutoLayout: true)
         view.dropShadow(color: .black, opacity: 0.3, radius: 3)
         view.layer.cornerRadius = 16
-        view.configure(with: AddressCardViewModel())
         view.delegate = self
         return view
     }()
@@ -26,13 +31,20 @@ final class AddressCardDemoView: UIView {
     // MARK: - Setup
 
     private func setup() {
-        addSubview(view)
+        tweakingOptions.first?.action?()
+        addSubview(addressCardView)
 
         NSLayoutConstraint.activate([
-            view.leadingAnchor.constraint(equalTo: leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: trailingAnchor),
-            view.centerYAnchor.constraint(equalTo: centerYAnchor)
+            addressCardView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            addressCardView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            addressCardView.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
+    }
+
+    // MARK: - Private methods
+
+    private func configure() {
+
     }
 }
 
@@ -51,12 +63,20 @@ extension AddressCardDemoView: AddressCardViewDelegate {
 // MARK: - Demo data
 
 private extension AddressCardViewModel {
-    init() {
-        self.init(
+    static var `default`: Self {
+        Self.init(
             title: "Møllerøya 32",
             subtitle: "7982 Bindalseidet",
             copyButtonTitle: "Kopier adresse",
             getDirectionsButtonTitle: "Åpne veibeskrivelse"
+        )
+    }
+
+    static var withoutDirectionsButton: Self {
+        Self.init(
+            title: "Møllerøya 32",
+            subtitle: "7982 Bindalseidet",
+            copyButtonTitle: "Kopier adresse"
         )
     }
 }
