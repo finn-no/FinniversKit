1b5ecff753b3a71ee30a603f0058cd5537b05f03
Bastian Stien <bastian.stien@gmail.com>
2022-11-01 15:23:45 +0100

    Rewrite ScrollableTabView to use collectionView instead of stackView

--- FinniversKit/Sources/Components/ScrollableTabView/ScrollableTabView.swift
+++ FinniversKit/Sources/Components/ScrollableTabView/ScrollableTabView.swift
@@ -14,45 +14,22 @@ public class ScrollableTabView: BottomShadowView {
     public override var intrinsicContentSize: CGSize {
         CGSize(
             width: UIView.noIntrinsicMetric,
-            height: labelHeight + indicatorHeight + (verticalPadding * 2)
+            height: ItemCollectionViewCell.cellHeight
         )
     }
 
     // MARK: - Private properties
 
-    private let labelStyle = Label.Style.captionStrong
-    private let itemSpacing: CGFloat = 32
-    private let verticalPadding = CGFloat.spacingS
-    private let indicatorHeight: CGFloat = 4
-    private var currentViewModel: ScrollableTabViewModel?
-    private var indicatorViewLeadingConstraint: NSLayoutConstraint?
-    private var indicatorViewTrailingConstraint: NSLayoutConstraint?
-    private lazy var contentView = UIStackView(axis: .horizontal, spacing: itemSpacing, withAutoLayout: true)
+    private var itemModels = [ScrollableTabViewModel.Item]()
+    private lazy var dataSource = createDataSource()
 
-    private var labelHeight: CGFloat {
-        "I".height(withConstrainedWidth: .greatestFiniteMagnitude, font: labelStyle.font)
-    }
-
-    private var itemViews: [ItemView] {
-        contentView.arrangedSubviews.compactMap { $0 as? ItemView }
-    }
-
-    private var selectedItemView: ItemView? {
-        itemViews.first { $0.isSelected }
-    }
-
-    private lazy var scrollView: UIScrollView = {
-        let scrollView = UIScrollView(withAutoLayout: true)
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.contentInset = .init(vertical: 0, horizontal: .spacingM)
-        scrollView.alwaysBounceHorizontal = true
-        return scrollView
-    }()
-
-    private lazy var indicatorView: UIView = {
-        let indicatorView = UIView(withAutoLayout: true)
-        indicatorView.backgroundColor = .primaryBlue
-        return indicatorView
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.delegate = self
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.alwaysBounceVertical = false
+        collectionView.register(ItemCollectionViewCell.self)
+        return collectionView
     }()
 
     // MARK: - Init
@@ -62,149 +39,88 @@ public class ScrollableTabView: BottomShadowView {
         setup()
     }
 
-    public required init?(coder aDecoder: NSCoder) { fatalError() }
+    public required init?(coder: NSCoder) { fatalError() }
 
     // MARK: - Setup
 
     private func setup() {
-        backgroundColor = .bgPrimary
-        addSubview(scrollView)
-        scrollView.addSubview(contentView)
-        scrollView.addSubview(indicatorView)
-
-        scrollView.fillInSuperview()
-
-        NSLayoutConstraint.activate([
-            // Bind the stackView to the scroll view's content layout guide to
-            // set the content size.
-            contentView.topAnchor.constraint(equalTo: scrollView.frameLayoutGuide.topAnchor, constant: verticalPadding),
-            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
-            contentView.bottomAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor, constant: -verticalPadding),
-
-            indicatorView.heightAnchor.constraint(equalToConstant: indicatorHeight),
-            indicatorView.bottomAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor),
-        ])
+        addSubview(collectionView)
+        collectionView.fillInSuperview()
     }
 
     // MARK: - Public methods
 
     public func configure(with viewModel: ScrollableTabViewModel) {
-        guard viewModel != currentViewModel else { return }
-        currentViewModel = viewModel
-        contentView.removeArrangedSubviews()
+        itemModels = viewModel.items
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(viewModel.items, toSection: .main)
 
-        viewModel.items.forEach { item in
-            let itemView = ItemView(item: item, labelStyle: labelStyle, withAutoLayout: true)
-            contentView.addArrangedSubview(itemView)
-            itemView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleItemTap)))
-        }
-
-        contentView.layoutIfNeeded()
-
-        if let selectedIdentifier = viewModel.selectedIdentifier, let selectedView = itemViews.first(where: { $0.item.identifier == selectedIdentifier }) {
-            toggleSelection(newSelection: selectedView, notifyDelegate: false)
-        } else if let firstItemView = itemViews.first {
-            toggleSelection(newSelection: firstItemView, notifyDelegate: false)
-        }
-
-        invalidateIntrinsicContentSize()
-        layoutIfNeeded()
-    }
-
-    // MARK: - Overrides
-
-    public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if previousTraitCollection?.preferredContentSizeCategory != traitCollection.preferredContentSizeCategory {
-            invalidateIntrinsicContentSize()
-            redrawIndicator(animate: false)
-        }
+        dataSource.apply(snapshot)
+        let selectedIndex = viewModel.items.firstIndex(where: { $0.identifier == viewModel.selectedIdentifier })
+        collectionView.selectItem(at: indexPath(for: selectedIndex), animated: false, scrollPosition: .centeredHorizontally)
     }
 
     // MARK: - Private methods
 
-    private func toggleSelection(newSelection: ItemView, notifyDelegate: Bool) {
-        if selectedItemView == newSelection { return }
-        itemViews.forEach { $0.isSelected = $0 == newSelection }
-        scrollView.scrollRectToVisible(newSelection.frame, animated: true)
-        redrawIndicator(animate: true)
-
-        if notifyDelegate {
-            delegate?.scrollableTabViewDidTapItem(self, item: newSelection.item)
-        }
-    }
-
-    private func redrawIndicator(animate: Bool) {
-        guard let selectedItemView = selectedItemView else { return }
-
-        indicatorViewLeadingConstraint?.isActive = false
-        indicatorViewTrailingConstraint?.isActive = false
-
-        indicatorViewLeadingConstraint = indicatorView.leadingAnchor.constraint(equalTo: selectedItemView.leadingAnchor)
-        indicatorViewTrailingConstraint = indicatorView.trailingAnchor.constraint(equalTo: selectedItemView.trailingAnchor)
-
-        indicatorViewLeadingConstraint?.isActive = true
-        indicatorViewTrailingConstraint?.isActive = true
-
-        if animate {
-            UIView.animate(
-                withDuration: 0.2,
-                delay: 0,
-                options: .curveEaseOut,
-                animations: { [weak self] in
-                    self?.layoutIfNeeded()
-                }
-            )
-        } else {
-            layoutIfNeeded()
-        }
-    }
-
-    // MARK: - Actions
-
-    @objc private func handleItemTap(sender: UITapGestureRecognizer) {
-        guard let itemView = sender.view as? ItemView else { return }
-        toggleSelection(newSelection: itemView, notifyDelegate: true)
+    private func indexPath(for itemIndex: Int?) -> IndexPath? {
+        guard let itemIndex = itemIndex else { return nil }
+        return IndexPath(item: itemIndex, section: Section.main.rawValue)
     }
 }
 
-// MARK: - Private types / extensions
+// MARK: - CollectionView Datasource and layout
 
-private class ItemView: UIView {
+private extension ScrollableTabView {
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, ScrollableTabViewModel.Item>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, ScrollableTabViewModel.Item>
 
-    // MARK: - Internal properties
+    private enum Section: Int, Hashable {
+        case main
+    }
 
-    let item: ScrollableTabViewModel.Item
+    private func createLayout() -> UICollectionViewLayout {
+        UICollectionViewCompositionalLayout { section, _ in
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .estimated(50),
+                heightDimension: .fractionalHeight(1)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
-    var isSelected: Bool = false {
-        didSet {
-            titleLabel.textColor = isSelected ? .textPrimary : .stone
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .estimated(50),
+                heightDimension: .estimated(ItemCollectionViewCell.cellHeight)
+            )
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.interGroupSpacing = 32
+            section.contentInsets = .init(vertical: 0, horizontal: .spacingM)
+            return section
         }
     }
 
-    // MARK: - Private properties
-
-    private let labelStyle: Label.Style
-    private lazy var titleLabel = Label(style: labelStyle, withAutoLayout: true)
-
-    // MARK: - Init
-
-    init(item: ScrollableTabViewModel.Item, labelStyle: Label.Style, withAutoLayout: Bool) {
-        self.item = item
-        self.labelStyle = labelStyle
-        super.init(frame: .zero)
-        translatesAutoresizingMaskIntoConstraints = !withAutoLayout
-        setup()
+    private func createDataSource() -> DataSource {
+        DataSource(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, item in
+                let cell = collectionView.dequeue(ItemCollectionViewCell.self, for: indexPath)
+                cell.configure(item: item)
+                return cell
+            }
+        )
     }
+}
 
-    required init?(coder: NSCoder) { fatalError() }
+// MARK: - UICollectionViewDelegate
 
-    // MARK: - Setup
+extension ScrollableTabView: UICollectionViewDelegate {
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
 
-    private func setup() {
-        titleLabel.text = item.title
-        addSubview(titleLabel)
-        titleLabel.fillInSuperview()
+        if let itemModel = itemModels[safe: indexPath.item] {
+            delegate?.scrollableTabViewDidTapItem(self, item: itemModel)
+        }
     }
 }
