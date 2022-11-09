18a0b5eaa48479a23b3aa3da02cb7032fc0c297a
Bjørn Olav Ruud <bjorn.olav.ruud@schibsted.com>
2022-11-09 12:59:27 +0100

    CircleCI hates async apparently.

--- FinniversKit/Sources/Util/HTMLStringParser/HTMLStringParser.swift
+++ FinniversKit/Sources/Util/HTMLStringParser/HTMLStringParser.swift
@@ -18,30 +18,10 @@ public struct HTMLStringParser {
     }
 
     public func parse<Translator>(
-        html: String,
-        translator: Translator
-    ) async throws -> Translator.Output where Translator: HTMLStringParserTranslator {
-        let tokens = await tokenize(html: html)
-        return try await parse(tokens: tokens, translator: translator)
-    }
-
-    public func parse<Translator>(
         tokens: [HTMLLexer.Token],
         translator: Translator
     ) throws -> Translator.Output where Translator: HTMLStringParserTranslator {
         return try translator.translate(tokens: tokens)
-    }
-
-    public func parse<Translator>(
-        tokens: [HTMLLexer.Token],
-        translator: Translator
-    ) async throws -> Translator.Output where Translator: HTMLStringParserTranslator {
-        return try await withCheckedThrowingContinuation { continuation in
-            Task {
-                let output = try translator.translate(tokens: tokens)
-                continuation.resume(returning: output)
-            }
-        }
     }
 
     public func tokenize(html: String) -> [HTMLLexer.Token] {
@@ -50,15 +30,6 @@ public struct HTMLStringParser {
         lexer.delegate = delegate
         lexer.read(html: html)
         return delegate.tokens
-    }
-
-    public func tokenize(html: String) async -> [HTMLLexer.Token] {
-        return await withCheckedContinuation { continuation in
-            Task {
-                let tokens = tokenize(html: html)
-                continuation.resume(returning: tokens)
-            }
-        }
     }
 }
 
