81a6c3582aa301b0352fe820e81640e985eccf5e
Maren Zon Rafdal <49304658+MarenZR@users.noreply.github.com>
2022-11-24 12:41:21 +0100

    Added cardStyle to view model

--- FinniversKit/Sources/Components/BrazePromotionView/BrazePromotionViewModel.swift
+++ FinniversKit/Sources/Components/BrazePromotionView/BrazePromotionViewModel.swift
@@ -6,18 +6,21 @@ public struct BrazePromotionViewModel {
     let image: String?
     let primaryButtonTitle: String?
     let dismissible: Bool?
+    let cardStyle: String?
 
     public init(
         title: String,
         text: String? = nil,
         image: String? = nil,
         primaryButtonTitle: String? = nil,
-        dismissible: Bool? = true
+        dismissible: Bool? = true,
+        cardStyle: String? = "default"
     ) {
         self.title = title
         self.text = text
         self.image = image
         self.primaryButtonTitle = primaryButtonTitle
         self.dismissible = dismissible
+        self.cardStyle = cardStyle
     }
 }
