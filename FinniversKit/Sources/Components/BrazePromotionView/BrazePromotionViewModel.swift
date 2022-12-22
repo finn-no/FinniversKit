f9e4472c410876bda0bd19fb752b70f23aae2e4d
Maren Zon Rafdal <49304658+MarenZR@users.noreply.github.com>
2022-12-22 09:31:35 +0100

    Changed my mind and removed id again

--- FinniversKit/Sources/Components/BrazePromotionView/BrazePromotionViewModel.swift
+++ FinniversKit/Sources/Components/BrazePromotionView/BrazePromotionViewModel.swift
@@ -6,21 +6,18 @@ public struct BrazePromotionViewModel {
     let image: String?
     let primaryButtonTitle: String?
     let dismissible: Bool?
-    public let id: String?
 
     public init(
         title: String,
         text: String? = nil,
         image: String? = nil,
         primaryButtonTitle: String? = nil,
-        dismissible: Bool? = true,
-        id: String?
+        dismissible: Bool? = true
     ) {
         self.title = title
         self.text = text
         self.image = image
         self.primaryButtonTitle = primaryButtonTitle
         self.dismissible = dismissible
-        self.id = id
     }
 }
