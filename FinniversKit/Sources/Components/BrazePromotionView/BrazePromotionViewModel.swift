0a110e532224461135844eec0ac22372ecd8a34c
Maren Zon Rafdal <49304658+MarenZR@users.noreply.github.com>
2022-12-22 09:15:23 +0100

    Added id for tracking

--- FinniversKit/Sources/Components/BrazePromotionView/BrazePromotionViewModel.swift
+++ FinniversKit/Sources/Components/BrazePromotionView/BrazePromotionViewModel.swift
@@ -6,18 +6,21 @@ public struct BrazePromotionViewModel {
     let image: String?
     let primaryButtonTitle: String?
     let dismissible: Bool?
+    public let id: String?
 
     public init(
         title: String,
         text: String? = nil,
         image: String? = nil,
         primaryButtonTitle: String? = nil,
-        dismissible: Bool? = true
+        dismissible: Bool? = true,
+        id: String?
     ) {
         self.title = title
         self.text = text
         self.image = image
         self.primaryButtonTitle = primaryButtonTitle
         self.dismissible = dismissible
+        self.id = id
     }
 }
