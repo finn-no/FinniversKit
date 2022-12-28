39fc777033efeda3bed0ae32d1dd492a37cef1ed
thorbjørn <tbonvik@gmail.com>
2022-12-28 14:05:23 +0100

    refactoring

--- FinniversKit/Sources/Recycling/GridViews/AdRecommendations/Cell/StandardAdRecommendationCell.swift
+++ FinniversKit/Sources/Recycling/GridViews/AdRecommendations/Cell/StandardAdRecommendationCell.swift
@@ -213,24 +213,6 @@ public class StandardAdRecommendationCell: UICollectionViewCell, AdRecommendatio
         imageHeightMinimumConstraint.priority = .defaultHigh
 
         containerView.fillInSuperview()
-        let traitCollectionContentSize = {
-            switch self.traitCollection.preferredContentSizeCategory {
-            case UIContentSizeCategory.accessibilityExtraExtraExtraLarge:
-                return 2.5
-            case UIContentSizeCategory.accessibilityExtraExtraLarge:
-                return 2.25
-            case UIContentSizeCategory.accessibilityExtraLarge:
-                return 2.0
-            case UIContentSizeCategory.accessibilityLarge:
-                return 1.75
-            case UIContentSizeCategory.accessibilityMedium:
-                return 1.5
-            default:
-                return 1.0
-            }
-        }()
-
-        let accessibilityMultiplier: CGFloat = traitCollectionContentSize
 
         NSLayoutConstraint.activate([
             imageContentView.topAnchor.constraint(equalTo: contentView.topAnchor),
