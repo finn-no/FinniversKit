225bc77f2f3b7ef1f0c289841eb33e35d2eec28d
Bjørn Olav Ruud <bjorn.olav.ruud@schibsted.com>
2023-01-03 17:12:18 +0100

    Convert to sRGB colorspace for hex conversion to support other colorspaces and colors with extended range.

--- FinniversKit/Sources/DNA/Color/UIColor+FinniversKit.swift
+++ FinniversKit/Sources/DNA/Color/UIColor+FinniversKit.swift
@@ -460,11 +460,18 @@ public extension UIColor {
 
     /// Returns hexadecimal represetation of a color.
     var hexString: String {
-        let components = self.cgColor.components
-        let red = components?[safe: 0] ?? 0.0
-        let green = components?[safe: 1] ?? 0.0
-        let blue = components?[safe: 2] ?? 0.0
-        return  String(format: "#%02x%02x%02x", (Int)(red * 255), (Int)(green * 255), (Int)(blue * 255))
+        guard
+            let targetColorSpace = CGColorSpace(name: CGColorSpace.sRGB),
+            let cgColor = self.cgColor.converted(to: targetColorSpace, intent: .relativeColorimetric, options: nil)
+        else {
+            // Not possible to convert source color space to RGB
+            return "#000000"
+        }
+        let components = cgColor.components
+        let red = components?[0] ?? 0.0
+        let green = components?[1] ?? 0.0
+        let blue = components?[2] ?? 0.0
+        return String(format: "#%02x%02x%02x", (Int)(red * 255), (Int)(green * 255), (Int)(blue * 255))
     }
 
     @available(*, deprecated, message: "Use dynamicColor(defaultColor:darkModeColor:) instead.")
