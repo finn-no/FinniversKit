db88ae16e031cb24d1b2db35019c2e7cbfa4c5ea
Nina Røsdal Graneggen <ninagraneggen@gmail.com>
2023-01-10 12:21:22 +0100

    Move setup function up to class

--- Demo/Sources/Components/FrontPageSavedSearchesDemoView/FrontPageSavedSearchesDemoView.swift
+++ Demo/Sources/Components/FrontPageSavedSearchesDemoView/FrontPageSavedSearchesDemoView.swift
@@ -19,11 +19,9 @@ class FrontPageSavedSearchesDemoView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
 
-// MARK: - Setup
+    // MARK: - Setup
 
-private extension FrontPageSavedSearchesDemoView {
     private func setup() {
         addSubview(frontPageSavedSearchesView)
         frontPageSavedSearchesView.fillInSuperview()
