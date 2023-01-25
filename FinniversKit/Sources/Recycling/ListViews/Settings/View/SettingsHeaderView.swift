0c7fcf5d1d34eb3f578faa35cee6d00f7a5aa3e1
Andrii Momot <andrii.momot@schibsted.com>
2023-01-25 15:45:08 +0100

    bg fix

--- FinniversKit/Sources/Recycling/ListViews/Settings/View/SettingsHeaderView.swift
+++ FinniversKit/Sources/Recycling/ListViews/Settings/View/SettingsHeaderView.swift
@@ -32,7 +32,7 @@ class SettingsHeaderView: UIView {
     // MARK: - Private methods
 
     private func setup() {
-        backgroundColor = .bgTertiary
+        backgroundColor = .bgPrimary
         addSubview(headerLabel)
         headerLabel.fillInSuperview(insets: UIEdgeInsets(top: .spacingM, left: .spacingM, bottom: -.spacingM, right: -.spacingM))
     }
