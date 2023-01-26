ffe718bc7a10632b68a6bba19f841054f4fbb862
ninarg <ninagraneggen@gmail.com>
2023-01-26 15:03:04 +0100

    Bugfix: Make recommendations accessible with VoiceOver (#1192)

--- FinniversKit/Sources/Recycling/GridViews/AdRecommendations/Cell/StandardAdRecommendationCell.swift
+++ FinniversKit/Sources/Recycling/GridViews/AdRecommendations/Cell/StandardAdRecommendationCell.swift
@@ -170,7 +170,6 @@ public class StandardAdRecommendationCell: UICollectionViewCell, AdRecommendatio
     }
 
     private func setup() {
-        isAccessibilityElement = true
         var accessibilityMultiplier: CGFloat = 1.0
         if Config.isDynamicTypeEnabled {
             accessibilityMultiplier = {
