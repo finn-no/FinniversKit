e07a7c28d48462e40d67a14c1b3c1cde666fee0c
Nina Røsdal Graneggen <ninagraneggen@gmail.com>
2023-01-31 13:03:41 +0100

    Copy preview data to ToastSwiftUIView as well

--- FinniversKit/Sources/SwiftUI Components/Toast/ToastSwiftUIView.swift
+++ FinniversKit/Sources/SwiftUI Components/Toast/ToastSwiftUIView.swift
@@ -34,6 +34,14 @@ public struct ToastSwiftUIView: View {
 
 struct ToastSwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        ToastSwiftUIView(text: "Success", style: .success)
+        VStack(spacing: .spacingM) {
+            ToastSwiftUIView(text: "Success", style: .success)
+            ToastSwiftUIView(text: "Action success, with a pretty long text which should wrap nicely", style: .success, action: .init(title: "Undo", action: {}))
+            ToastSwiftUIView(text: "Action success", style: .success, action: .init(title: "Action", buttonStyle: .promoted, action: {}))
+
+            ToastSwiftUIView(text: "Error", style: .error)
+            ToastSwiftUIView(text: "Action error", style: .error, action: .init(title: "Undo", action: {}))
+            ToastSwiftUIView(text: "Action error", style: .error, action: .init(title: "Undo", buttonStyle: .promoted, action: {}))
+        }
     }
 }
