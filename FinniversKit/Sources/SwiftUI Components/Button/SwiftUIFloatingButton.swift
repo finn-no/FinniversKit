e4097802b83df5a9bf7e2a4fb394c2503121b08b
Benjamin Tegelaár-Breiby <benjamin.breiby@schibsted.com>
2023-03-21 15:59:45 +0100

    Refactor to a button style

--- FinniversKit/Sources/SwiftUI Components/Button/SwiftUIFloatingButton.swift
+++ FinniversKit/Sources/SwiftUI Components/Button/SwiftUIFloatingButton.swift
@@ -1,92 +1,43 @@
 import SwiftUI
 
-public struct SwiftUIFloatingButton: View {
-    @ObservedObject var viewModel: SwiftUIFloatingButtonModel
-    @State private var isPressed: Bool = false
-    private let style: SwiftUIFloatingButtonStyle
-    private let action: () -> Void
+struct FloatingButtonStyle: ButtonStyle {
+    let backgroundColor: Color
+    let backgroundColorPressed: Color
+    let shadowColor: Color
+    let shadowOffset: CGSize
+    let shadowRadius: CGFloat
 
-    public init(
-        style: SwiftUIFloatingButtonStyle = .default,
-        viewModel: SwiftUIFloatingButtonModel,
-        action: @escaping () -> Void
-    ) {
-        self.style = style
-        self.viewModel = viewModel
-        self.action = action
-    }
-
-    public var body: some View {
-        SwiftUI.Button(action: {
-            action()
-            isPressed = false
-        }, label: {
-            HStack {
-                viewModel.image
-                    .foregroundColor(style.tintColor)
-                if let title = viewModel.title {
-                    Text(title)
-                        .finnFont(.detail)
-                        .foregroundColor(style.titleColor)
-                }
-            }
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
             .padding(.spacingM)
-            .background(isPressed ? style.highlightedBackgroundColor : style.primaryBackgroundColor)
+            .background(configuration.isPressed ? backgroundColorPressed : backgroundColor)
             .clipShape(Capsule())
             .shadow(
-                color: style.shadowColor,
-                radius: style.shadowRadius,
-                x: style.shadowOffset.width,
-                y: style.shadowOffset.height
+                color: shadowColor,
+                radius: shadowRadius,
+                x: shadowOffset.width,
+                y: shadowOffset.height
             )
-        })
-        .buttonStyle(PlainButtonStyle())
-        .simultaneousGesture(LongPressGesture().onChanged { _ in
-            isPressed = true
-        }.onEnded { _ in
-            isPressed = false
-        })
-    }
-}
-
-extension SwiftUIFloatingButton {
-    public class SwiftUIFloatingButtonModel: ObservableObject {
-        public var title: String?
-        public var image: Image?
-
-        public init(title: String, image: Image? = nil) {
-            self.title = title
-            self.image = image
-        }
-    }
-
-    public struct SwiftUIFloatingButtonStyle {
-        let tintColor: Color
-        let titleColor: Color
-        let primaryBackgroundColor: Color
-        let highlightedBackgroundColor: Color
-        let borderWidth: CGFloat
-        let borderColor: Color?
-        let shadowColor: Color
-        let shadowOffset: CGSize
-        let shadowRadius: CGFloat
-
-        public static let `default` = SwiftUIFloatingButtonStyle(
-            tintColor: Color.btnPrimary,
-            titleColor: Color.btnPrimary,
-            primaryBackgroundColor: Color.bgPrimary,
-            highlightedBackgroundColor: Color.bgSecondary,
-            borderWidth: 0,
-            borderColor: nil,
-            shadowColor: Color.black.opacity(0.6),
-            shadowOffset: CGSize(width: 0, height: 5),
-            shadowRadius: 8
-        )
     }
 }
 
 struct SwiftUIFloatingButton_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIFloatingButton(viewModel: .init(title: "Legg til element", image: .init(systemName: "plus"))) {}
+        SwiftUI.Button(action: {}, label: {
+            HStack {
+                Image(systemName: "plus")
+                    .foregroundColor(.btnPrimary)
+                Text("Legg til element")
+                    .finnFont(.detail)
+                    .foregroundColor(.btnPrimary)
+            }
+        })
+        .buttonStyle(FloatingButtonStyle(
+            backgroundColor: .bgPrimary,
+            backgroundColorPressed: .bgSecondary,
+            shadowColor: .black.opacity(0.6),
+            shadowOffset: .init(width: 0, height: 5),
+            shadowRadius: .spacingS
+        ))
     }
 }
