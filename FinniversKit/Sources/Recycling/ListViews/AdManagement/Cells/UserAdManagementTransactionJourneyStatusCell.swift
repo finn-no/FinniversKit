8e60124016fe54f30c2273188a9c7f52abb039a1
thorbjørn <tbonvik@gmail.com>
2023-05-04 16:59:06 +0200

    add TJT status cell

---
+++ FinniversKit/Sources/Recycling/ListViews/AdManagement/Cells/UserAdManagementTransactionJourneyStatusCell.swift
@@ -0,0 +1,83 @@
+//
+//  Copyright © FINN.no AS. All rights reserved.
+//
+
+public protocol UserAdManagementTransactionJourneyStatusCellDelegate: AnyObject {
+    func transactionJourneyStatusCellWasTapped(_ sender: UserAdManagementTransactionJourneyStatusCell)
+}
+
+public class UserAdManagementTransactionJourneyStatusCell: UITableViewCell {
+
+    weak public var delegate: UserAdManagementTransactionJourneyStatusCellDelegate?
+
+    public var labelText: String? {
+        didSet {
+            transactionStatusLabel.attributedText = NSAttributedString(string: labelText ?? "")
+        }
+    }
+
+    private lazy var contentStackViewTrailingConstraint = contentStackView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor)
+
+    private lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView(axis: .horizontal, spacing: .spacingM, withAutoLayout: true)
+        stackView.addArrangedSubviews([iconImageView, transactionStatusLabel])
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        return stackView
+    }()
+
+    private lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView(withAutoLayout: true)
+        imageView.tintColor = .iconPrimary
+        imageView.contentMode = .scaleAspectFit
+
+        if let image = UIImage(named: "torgetShipping") {
+            imageView.image = image.withRenderingMode(.alwaysTemplate)
+        }
+
+        return imageView
+    }()
+
+    private lazy var transactionStatusLabel: UILabel = {
+        let label = UILabel(withAutoLayout: true)
+        label.numberOfLines = 0
+        label.font = .bodyStrong
+        label.textColor = .textPrimary
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+
+    // MARK: - init
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup
+
+    private func setup() {
+        backgroundColor = .bgPrimary
+
+        contentStackView.addArrangedSubviews([iconImageView, transactionStatusLabel])
+
+        contentView.addSubview(contentStackView)
+
+        let iconImageWidthAndHeight: CGFloat = 24
+
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: .spacingM),
+            contentStackView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            contentStackViewTrailingConstraint,
+            contentStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -.spacingM),
+
+            iconImageView.widthAnchor.constraint(equalToConstant: iconImageWidthAndHeight),
+            iconImageView.heightAnchor.constraint(equalToConstant: iconImageWidthAndHeight),
+        ])
+    }
+}
