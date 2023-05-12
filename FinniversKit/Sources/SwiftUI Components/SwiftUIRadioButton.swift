5463c4ed91f3d77432baa2af4c19a1d4c590aac0
Bjørn Olav Ruud <bjorn.olav.ruud@schibsted.com>
2023-05-12 15:02:15 +0200

    Customizable size for SwiftUI radio button. Updated size to Fabric reference.

--- FinniversKit/Sources/SwiftUI Components/SwiftUIRadioButton.swift
+++ FinniversKit/Sources/SwiftUI Components/SwiftUIRadioButton.swift
@@ -1,35 +1,37 @@
 import SwiftUI
 
 public struct SwiftUIRadioButton: View {
-    @Environment(\.colorScheme) var colorScheme
-
     @Binding public var isSelected: Bool
 
-    private static let size: CGFloat = 16
+    private let size: CGFloat
+    private let borderWidth: CGFloat
+    private let selectionScale: CGFloat
 
     private var backgroundColor: Color {
         isSelected ? .textAction : .textSecondary
     }
 
-    private var borderWidth: CGFloat {
-        // The selection border looks too wide in dark mode
-        let selectedBorderWidth: CGFloat = colorScheme == .dark ? 4 : 5
-        return isSelected ? selectedBorderWidth : 1
-    }
-
     private var innerSize: CGFloat {
-        SwiftUIRadioButton.size - borderWidth * 2
+        isSelected ? size * selectionScale : size - borderWidth * 2
     }
 
-    public init(isSelected: Binding<Bool>) {
+    public init(
+        isSelected: Binding<Bool>,
+        size: CGFloat = 16,
+        borderWidth: CGFloat = 1,
+        selectionScale: CGFloat = 7/16
+    ) {
         self._isSelected = isSelected
+        self.size = size
+        self.borderWidth = borderWidth
+        self.selectionScale = selectionScale
     }
 
     public var body: some View {
         ZStack {
             Circle()
                 .foregroundColor(backgroundColor)
-                .frame(width: SwiftUIRadioButton.size, height: SwiftUIRadioButton.size)
+                .frame(width: size, height: size)
                 .animation(.spring(response: 0.2, dampingFraction: 0.5), value: isSelected)
 
             Circle()
@@ -43,9 +45,19 @@ public struct SwiftUIRadioButton: View {
 struct SwiftUIRadioButton_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            SwiftUIRadioButton(isSelected: .constant(true))
+            StatefulPreviewWrapper(true) { binding in
+                SwiftUIRadioButton(isSelected: binding)
+                    .onTapGesture {
+                        binding.wrappedValue.toggle()
+                    }
+            }
 
-            SwiftUIRadioButton(isSelected: .constant(false))
+            StatefulPreviewWrapper(false) { binding in
+                SwiftUIRadioButton(isSelected: binding)
+                    .onTapGesture {
+                        binding.wrappedValue.toggle()
+                    }
+            }
         }
     }
 }
