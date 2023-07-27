6fa9e51e89d75d5cb971331a13042a7456661aee
Bastian Stien <bastian.stien@gmail.com>
2023-07-27 13:31:42 +0200

    Delete DemoViewsTableViewController

--- Demo/Sources/AppDelegate.swift
+++ Demo/Sources/AppDelegate.swift
@@ -10,8 +10,6 @@ import DemoKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    lazy var navigationController = NavigationController(rootViewController: DemoViewsTableViewController())
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
 
