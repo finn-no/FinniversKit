1d1d82bed4d9c1e483b60b32c7f0b320a27e90d4
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-09-19 11:29:25 +0200

    Prepend `ImageAsset` to magnifyingGlass image keys.

--- FinniversKit/Sources/SwiftUI Components/ResultSwiftUIView.swift
+++ FinniversKit/Sources/SwiftUI Components/ResultSwiftUIView.swift
@@ -74,7 +74,7 @@ struct ResultSwiftUIView_Previews: PreviewProvider {
             ).previewDisplayName("Error retry")
 
             ResultSwiftUIView(
-                image: Image(.magnifyingGlass),
+                image: Image(ImageAsset.magnifyingGlass),
                 title: "Klarte ikke finne annonsen",
                 text: "Det kan se ut som annonsen du kikker etter har blitt slettet"
             ).previewDisplayName("Not found")
