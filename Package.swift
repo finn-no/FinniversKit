62264b2ecafae943817ba557b1c5f2859b0df2cc
Bjørn Olav Ruud <bjorn.olav.ruud@schibsted.com>
2023-09-25 14:01:06 +0200

    Disable asset name generation. iOS 15 in Package.swift.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "FinniversKit",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "FinniversKit",
