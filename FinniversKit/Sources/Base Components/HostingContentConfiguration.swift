ccace090f543a4c8a3d07218ac480dc092bd2213
Bjørn Olav Ruud <BjornRuud@users.noreply.github.com>
2023-10-12 11:00:25 +0200

    HostingContentConfiguration fix (#1273)

--- FinniversKit/Sources/Base Components/HostingContentConfiguration.swift
+++ FinniversKit/Sources/Base Components/HostingContentConfiguration.swift
@@ -2,12 +2,22 @@ import SwiftUI
 import UIKit
 
 /// A substitute for UIHostingConfiguration for use on iOS < 16.
+///
 /// https://medium.com/justeattakeaway-tech/using-swiftui-inside-an-ancient-uitableview-or-uicollectionview-bb8defb6304
+///
+/// https://github.com/woxtu/UIHostingConfigurationBackport
+///
 public struct HostingContentConfiguration<Content>: UIContentConfiguration where Content: View {
-    fileprivate let hostingController: UIHostingController<Content>
+    public let hostingController: UIHostingController<Content>
 
     public init(@ViewBuilder content: () -> Content) {
-        hostingController = UIHostingController(rootView: content())
+        let hostingController = UIHostingController(rootView: content())
+        hostingController.view.backgroundColor = .clear
+        self.hostingController = hostingController
+    }
+
+    public init(hostingController: UIHostingController<Content>) {
+        self.hostingController = hostingController
     }
 
     public func makeContentView() -> UIView & UIContentView {
@@ -20,68 +30,45 @@ public struct HostingContentConfiguration<Content>: UIContentConfiguration where
 }
 
 private final class ContentView<Content>: UIView, UIContentView where Content: View {
-    var configuration: UIContentConfiguration {
-        didSet {
-            configure(configuration)
-        }
+    var configuration: UIContentConfiguration
+
+    private var hostingController: UIHostingController<Content>? {
+        (configuration as? HostingContentConfiguration<Content>)?.hostingController
     }
 
-    private weak var hostingController: UIHostingController<Content>?
-
-    init(_ configuration: UIContentConfiguration) {
+    init(_ configuration: HostingContentConfiguration<Content>) {
         self.configuration = configuration
         super.init(frame: .zero)
+
+        guard let hostingView = hostingController?.view else { return }
+        hostingView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(hostingView)
+        NSLayoutConstraint.activate([
+            hostingView.topAnchor.constraint(equalTo: topAnchor),
+            hostingView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            hostingView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            hostingView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    deinit {
-        hostingController?.removeFromParent()
-    }
-
-    func configure(_ configuration: UIContentConfiguration) {
-        guard
-            let configuration = configuration as? HostingContentConfiguration<Content>,
-            let parent = findViewController()
-        else { return }
-
-        let hostingController = configuration.hostingController
-
-        guard
-            let swiftUICellView = hostingController.view,
-            subviews.isEmpty
-        else {
-            hostingController.view.invalidateIntrinsicContentSize()
-            return
+    override func didMoveToSuperview() {
+        guard let hostingController else { return }
+        if superview == nil {
+            hostingController.willMove(toParent: nil)
+            hostingController.removeFromParent()
+        } else if let parentVC = parentViewController {
+            parentVC.addChild(hostingController)
+            hostingController.didMove(toParent: parentVC)
         }
-
-        hostingController.view.backgroundColor = .clear
-
-        parent.addChild(hostingController)
-        addSubview(hostingController.view)
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            leadingAnchor.constraint(equalTo: swiftUICellView.leadingAnchor),
-            trailingAnchor.constraint(equalTo: swiftUICellView.trailingAnchor),
-            topAnchor.constraint(equalTo: swiftUICellView.topAnchor),
-            bottomAnchor.constraint(equalTo: swiftUICellView.bottomAnchor)
-        ])
-        hostingController.didMove(toParent: parent)
-        self.hostingController = hostingController
     }
 }
 
-private extension UIView {
-    func findViewController() -> UIViewController? {
-        if let nextResponder = next as? UIViewController {
-            return nextResponder
-        } else if let nextResponder = next as? UIView {
-            return nextResponder.findViewController()
-        }
-
-        return nil
+private extension UIResponder {
+    var parentViewController: UIViewController? {
+        return next as? UIViewController ?? next?.parentViewController
     }
 }
