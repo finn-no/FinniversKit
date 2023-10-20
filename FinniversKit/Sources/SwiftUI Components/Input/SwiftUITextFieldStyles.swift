8b7b85a01e7fced0bae08690e54334656b45d4f0
Benjamin Tegelaár-Breiby <benjamin.breiby@schibsted.com>
2023-10-20 12:00:16 +0200

    Add footer to jobs text field style

--- FinniversKit/Sources/SwiftUI Components/Input/SwiftUITextFieldStyles.swift
+++ FinniversKit/Sources/SwiftUI Components/Input/SwiftUITextFieldStyles.swift
@@ -3,9 +3,13 @@ import SwiftUI
 public struct JobsTextFieldStyle: SwiftUI.TextFieldStyle {
     @FocusState var focused: Bool
     let title: String?
+    let footer: String?
 
-    public init(title: String? = nil) {
+    /// - Parameter title: Text to be displayed above the text field
+    /// - Parameter footer: Text to be displayed below the text field
+    public init(title: String? = nil, footer: String? = nil) {
         self.title = title
+        self.footer = footer
     }
 
     public func _body(configuration: SwiftUI.TextField<Self._Label>) -> some View {
@@ -24,6 +28,12 @@ public struct JobsTextFieldStyle: SwiftUI.TextFieldStyle {
                     RoundedRectangle(cornerRadius: .spacingXS, style: .continuous)
                         .stroke(focused ? Color.btnPrimary : Color.borderDefault, lineWidth: 1)
                 )
+
+            if let footer {
+                Text(footer)
+                    .finnFont(.caption)
+                    .foregroundColor(.textPrimary)
+            }
         }
         .focused($focused)
         .animation(.snappy, value: focused)
@@ -34,7 +44,7 @@ struct SwiftUITextFieldStyles_Previews: PreviewProvider {
     static var previews: some View {
         SwiftUI.TextField("Batman", text: .constant(""))
             .textFieldStyle(
-                JobsTextFieldStyle(title: "Name")
+                JobsTextFieldStyle(title: "Name", footer: "Your real name or a nickname. We use this to personalise your experience in the app.")
             )
             .padding()
     }
