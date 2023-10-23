7f1fc50d14521d371096896d0896836c71f05fe7
joalux2 <joakim.lundberg@schibsted.com>
2023-10-23 14:01:43 +0200

    started with the new view
    added toggle te set if you want to see tori or finn grid

--- Demo/Sources/Recycling/GridViews/Markets/MarketsGridViewDemoView.swift
+++ Demo/Sources/Recycling/GridViews/Markets/MarketsGridViewDemoView.swift
@@ -4,6 +4,7 @@
 
 import FinniversKit
 import DemoKit
+import UIKit
 
 class MarketDataSource: NSObject {
     var models = Market.newMarkets
@@ -13,6 +14,9 @@ class MarketsGridViewDemoView: UIView, Demoable {
     lazy var dataSource: MarketDataSource = {
         return MarketDataSource()
     }()
+    var demoLabel = UILabel(frame:CGRect(x: 150, y: 140, width: 200, height: 21))
+    var showingTori = false
+    
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,12 +30,41 @@ class MarketsGridViewDemoView: UIView, Demoable {
         let collectionView = MarketsGridView(accessibilityHeader: "Markeder", delegate: self, dataSource: self)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(collectionView)
+
+        var stackView = UIStackView()
+        
+        demoLabel.textAlignment = .center
+        demoLabel.text = "Tori"
+        addSubview(demoLabel)
+        
+        let demoToggle = UISwitch(frame:CGRect(x: 150, y: 150, width: 0, height: 0))
+        demoToggle.addTarget(self, action: #selector(self.changedDemoView(_:)), for: .valueChanged)
+        addSubview(demoToggle)
+        
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: topAnchor),
             collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            collectionView.heightAnchor.constraint(equalToConstant: collectionView.calculateSize(constrainedTo: frame.width).height)
+            collectionView.heightAnchor.constraint(equalToConstant: collectionView.calculateSize(constrainedTo: frame.width).height),
+            
+            
+            
+            demoToggle.centerXAnchor.constraint(equalTo: collectionView.centerXAnchor),
+            demoToggle.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 10),
+            
+            demoLabel.bottomAnchor.constraint(equalTo: demoToggle.topAnchor, constant: 10),
+            demoLabel.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0)
         ])
+    }
+    
+    @objc func changedDemoView(_ sender: UISwitch){
+        print("TOGGLE TORI:", showingTori)
+        if showingTori {
+            demoLabel.text = "Tori"
+        } else {
+            demoLabel.text = "Finn"
+        }
+        showingTori.toggle()
     }
 }
 
