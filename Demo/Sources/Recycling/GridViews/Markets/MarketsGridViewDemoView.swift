779f030c58d13532cb50fdad004347bc0f1aed0a
joalux2 <joakim.lundberg@schibsted.com>
2023-11-20 14:28:08 +0100

    Duplicate removal, markets fix and new snaps
    oremoved duplicate images, made compactdeni use the finn markets and reorded new snapshots

--- Demo/Sources/Recycling/GridViews/Markets/MarketsGridViewDemoView.swift
+++ Demo/Sources/Recycling/GridViews/Markets/MarketsGridViewDemoView.swift
@@ -5,55 +5,6 @@
 import FinniversKit
 import DemoKit
 
-/*
-class MarketDataSource: NSObject {
-    var models = CompactMarket.newMarkets
-}
-
-class MarketsGridViewDemoView: UIView, Demoable {
-    lazy var dataSource: MarketDataSource = {
-        return MarketDataSource()
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        setup()
-    }
-
-    required init?(coder aDecoder: NSCoder) { fatalError() }
-
-    private func setup() {
-        let collectionView = MarketsGridView(accessibilityHeader: "Markeder", delegate: self, dataSource: self)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(collectionView)
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            collectionView.heightAnchor.constraint(equalToConstant: collectionView.calculateSize(constrainedTo: frame.width).height)
-        ])
-    }
-}
-*/
-
-/*
-extension MarketsGridViewDemoView: MarketsViewDataSource {
-    func numberOfItems(inMarketsView marketsView: MarketsView) -> Int {
-        return dataSource.models.count
-    }
-
-    func marketsView(_ marketsView: MarketsView, modelAtIndex index: Int) -> MarketsViewModel {
-        return dataSource.models[index]
-    }
-}
-
-extension MarketsGridViewDemoView: MarketsViewDelegate {
-    func marketsView(_ marketsGridView: MarketsView, didSelectItemAtIndex index: Int) {}
-}
-*/
-
-
 class MarketsDemoView: UIView, Demoable {
     var demoStack = UIStackView()
     var finnDemo = FinnMarketsDemoView()
