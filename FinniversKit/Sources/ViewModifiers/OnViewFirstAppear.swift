832e9d4caee4fbd299cbc58ac3f88a592a7cfe2a
Matvii Sakhnenko <39955656+sakhnenkoff@users.noreply.github.com>
2023-12-07 14:49:57 +0100

    Origin/On View First Appear modifier (#1298)
    * added onViewFirstAppear modifier
    
    * made the extension public
    
    ---------
    
    Co-authored-by: Matvii Sakhnenko <matvii.sakhnenko@schibsted.com>

---
+++ FinniversKit/Sources/ViewModifiers/OnViewFirstAppear.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+public extension View {
+    func onViewFirstAppear(_ action: @escaping () -> Void) -> some View {
+        modifier(FirstAppear(action: action))
+    }
+}
+
+private struct FirstAppear: ViewModifier {
+    let action: () -> Void
+
+    // Use this to only fire your block one time
+    @State private var hasAppeared = false
+
+    func body(content: Content) -> some View {
+        content.onAppear {
+            guard !hasAppeared else { return }
+            hasAppeared = true
+            action()
+        }
+    }
+}
