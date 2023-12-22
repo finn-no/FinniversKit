cf86abc7208c811caed2a2927a9e33005b711094
Bastian Stien <bastian.stien@gmail.com>
2023-12-22 09:20:59 +0100

    Demo: Place within stack- and scrollview

--- Demo/Sources/Components/Infobox/InfoboxDemoView.swift
+++ Demo/Sources/Components/Infobox/InfoboxDemoView.swift
@@ -6,35 +6,14 @@ import FinniversKit
 import DemoKit
 
 public class InfoboxDemoView: UIView, Demoable {
-    private lazy var smallInfoboxLabel: Label = {
-        let label = Label(style: .caption)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
-        label.text = "Style: .small(backgroundColor: .bgSecondary):"
-        label.textColor = .textSecondary
-        return label
-    }()
-    private lazy var smallInfoboxView = InfoboxView(style: .small(backgroundColor: .bgSecondary))
-    private lazy var normalInfoboxLabel: Label = {
-        let label = Label(style: .caption)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
-        label.text = "Style: .normal(backgroundColor: .bgPrimary, primaryButtonIcon: UIImage(named: .webview):"
-        label.numberOfLines = 2
-        label.textColor = .textSecondary
-        return label
-    }()
-    private lazy var normalInfoboxView = InfoboxView(style: .normal(backgroundColor: .bgPrimary, primaryButtonIcon: UIImage(named: .webview)))
-    private lazy var warningInfoboxLabel: Label = {
-        let label = Label(style: .caption)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
-        label.text = "Style: .warning"
-        label.numberOfLines = 1
-        label.textColor = .textSecondary
-        return label
-    }()
-    private lazy var warningInfoboxView = InfoboxView(style: .warning)
+    private lazy var stackView = UIStackView(axis: .vertical, spacing: .spacingL, distribution: .equalSpacing, withAutoLayout: true)
+    private lazy var scrollView = UIScrollView(withAutoLayout: true)
+
+    private var infoboxConfigurations: [(style: InfoboxView.Style, model: InfoboxViewModel)] = [
+        (style: .small(backgroundColor: .bgSecondary), model: InfoboxDefaultData()),
+        (style: .normal(backgroundColor: .bgPrimary, primaryButtonIcon: UIImage(named: .webview)), model: InfoboxOpenBrowserData()),
+        (style: .warning, model: InfoboxWarningData()),
+    ]
 
     // MARK: - Init
 
@@ -48,46 +27,23 @@ public class InfoboxDemoView: UIView, Demoable {
     // MARK: - Setup
 
     private func setup() {
-        smallInfoboxView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(smallInfoboxView)
-        smallInfoboxView.model = InfoboxDefaultData()
+        let infoboxes = infoboxConfigurations.map(createInfoBox(style:model:))
+        stackView.addArrangedSubviews(infoboxes)
 
-        normalInfoboxView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(normalInfoboxView)
-        normalInfoboxView.model = InfoboxOpenBrowserData()
+        scrollView.addSubview(stackView)
+        stackView.fillInSuperview(margin: .spacingM)
 
-        warningInfoboxView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(warningInfoboxView)
-        warningInfoboxView.model = InfoboxWarningData()
-
-        addSubview(smallInfoboxLabel)
-        addSubview(normalInfoboxLabel)
-        addSubview(warningInfoboxLabel)
+        addSubview(scrollView)
+        scrollView.fillInSuperview()
 
         NSLayoutConstraint.activate([
-            smallInfoboxLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            smallInfoboxLabel.widthAnchor.constraint(equalTo: widthAnchor),
-            smallInfoboxLabel.topAnchor.constraint(equalTo: topAnchor, constant: .spacingS),
-
-            smallInfoboxView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
-            smallInfoboxView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            smallInfoboxView.topAnchor.constraint(equalTo: smallInfoboxLabel.bottomAnchor, constant: .spacingXS),
-
-            normalInfoboxLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            normalInfoboxLabel.widthAnchor.constraint(equalTo: widthAnchor),
-            normalInfoboxLabel.topAnchor.constraint(equalTo: smallInfoboxView.bottomAnchor, constant: .spacingXL),
-
-            normalInfoboxView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
-            normalInfoboxView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            normalInfoboxView.topAnchor.constraint(equalTo: normalInfoboxLabel.bottomAnchor, constant: .spacingXS),
-
-            warningInfoboxLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            warningInfoboxLabel.widthAnchor.constraint(equalTo: widthAnchor),
-            warningInfoboxLabel.topAnchor.constraint(equalTo: normalInfoboxView.bottomAnchor, constant: .spacingXL),
-
-            warningInfoboxView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
-            warningInfoboxView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            warningInfoboxView.topAnchor.constraint(equalTo: warningInfoboxLabel.bottomAnchor, constant: .spacingXS),
+            stackView.widthAnchor.constraint(equalTo: widthAnchor, constant: -.spacingM * 2)
         ])
+    }
+
+    private func createInfoBox(style: InfoboxView.Style, model: InfoboxViewModel) -> InfoboxView {
+        let view = InfoboxView(style: style, withAutoLayout: true)
+        view.model = model
+        return view
     }
 }
