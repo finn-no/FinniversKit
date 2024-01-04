1ee374238ad4945964d852bfbee9753f3b60a966
Nina Røsdal Graneggen <ninagraneggen@gmail.com>
2024-01-04 16:38:01 +0100

    Switch is btnAction

--- FinniversKit/Sources/Components/Switch/SwitchView.swift
+++ FinniversKit/Sources/Components/Switch/SwitchView.swift
@@ -39,7 +39,7 @@ public class SwitchView: UIView {
 
     private lazy var uiSwitch: UISwitch = {
         let uiSwitch = UISwitch(withAutoLayout: true)
-        uiSwitch.onTintColor = .btnPrimary //DARK
+        uiSwitch.onTintColor = .btnAction //DARK
         uiSwitch.addTarget(self, action: #selector(swichValueChanged), for: .valueChanged)
         return uiSwitch
     }()
