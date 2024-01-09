24c54fddffef0114eee09ba5cc619bddd258afaa
Peter Bødskov <peter.bodskov@schibsted.com>
2024-01-09 11:43:41 +0100

    fix: falling back to multiline functions for colors

--- FinniversKit/Sources/DNA/Color/ColorProvider.swift
+++ FinniversKit/Sources/DNA/Color/ColorProvider.swift
@@ -66,115 +66,216 @@ public struct DefaultColorProvider: ColorProvider {
 
     public static let shared = DefaultColorProvider()
 
-    public var bgPrimary: UIColor { .dynamicColor(defaultColor: .white, darkModeColor: .darkMilk) }
-
-    public var bgSecondary: UIColor { .dynamicColor(defaultColor: .aqua50, darkModeColor: .darkIce) }
-
-    public var bgTertiary: UIColor { .dynamicColor(defaultColor: .blueGray50, darkModeColor: .darkMarble) }
-
-    public var bgQuaternary: UIColor { .dynamicColor(defaultColor: .blueGray50, darkModeColor: .darkMilk) }
-
-    public var bgBottomSheet: UIColor { .dynamicColor(defaultColor: .white, darkModeColor: .darkIce) }
-
-    public var bgAlert: UIColor { .yellow100 }
-
-    public var bgInfo: UIColor { .dynamicColor(defaultColor: .aqua50, darkModeColor: .darkIce) }
-
-    public var bgInfoHeader: UIColor { .dynamicColor(defaultColor: .aqua200, darkModeColor: .blue600) }
-
-    public var bgSuccess: UIColor { .green100 }
-
-    public var bgCritical: UIColor { .red100 }
-
-    public var bgInformationAlert: UIColor { .yellow50 }
-
-    public var bgInformationCritical: UIColor { .red50 }
-
-    public var bgInformationInfo: UIColor { .aqua50 }
-
-    public var bgInformationSuccess: UIColor { .green50 }
-
-    public var borderWarningSubtle: UIColor { .yellow300 }
-
-    public var borderNegativeSubtle: UIColor { .red300 }
-
-    public var borderInfoSubtle: UIColor { .aqua300 }
-
-    public var borderPositiveSubtle: UIColor { .green300 }
-
-    public var borderWarning: UIColor { .yellow600 }
-
-    public var borderNegative: UIColor { .red600 }
-
-    public var borderInfo: UIColor { .aqua600 }
-
-    public var borderPositive: UIColor { .green600 }
-
-    public var btnPrimary: UIColor { .dynamicColor(defaultColor: .blue600, darkModeColor: .darkCallToAction) }
-
-    public var btnDisabled: UIColor { .dynamicColor(defaultColor: .blueGray300, darkModeColor: .darkSardine) }
-
-    public var btnCritical: UIColor { .red600 }
-
-    public var btnAction: UIColor { .dynamicColor(defaultColor: .blue600, darkModeColor: .darkPrimaryBlue) }
-
-    public var textPrimary: UIColor { .dynamicColor(defaultColor: .gray700, darkModeColor: .white) }
-
-    public var textSecondary: UIColor { .dynamicColor(defaultColor: .gray500, darkModeColor: .darkStone) }
-
-    public var textTertiary: UIColor { .white }
-
-    public var textAction: UIColor { .dynamicColor(defaultColor: .blue600, darkModeColor: .darkPrimaryBlue) }
-
-    public var textDisabled: UIColor { .dynamicColor(defaultColor: .blueGray300, darkModeColor: .darkSardine) }
-
-    public var textAlert: UIColor { .lightNuttyBrown }
-
-    public var textCritical: UIColor {
-        .dynamicColor(
-            defaultColor: .red600,
-            darkModeColor: .red400
-        )
+    public var bgPrimary: UIColor {
+        .dynamicColor(defaultColor: .white, darkModeColor: .darkMilk)
     }
 
-    public var accentPrimaryBlue: UIColor { .blue600 }
+    public var bgSecondary: UIColor {
+        .dynamicColor(defaultColor: .aqua50, darkModeColor: .darkIce)
+    }
 
-    public var accentSecondaryBlue: UIColor { .aqua400 }
+    public var bgTertiary: UIColor {
+        .dynamicColor(defaultColor: .blueGray50, darkModeColor: .darkMarble)
+    }
 
-    public var accentPea: UIColor { .green400 }
+    public var bgQuaternary: UIColor {
+        .dynamicColor(defaultColor: .blueGray50, darkModeColor: .darkMilk)
+    }
 
-    public var accentToothpaste: UIColor { .aqua200 }
+    public var bgBottomSheet: UIColor {
+        .dynamicColor(defaultColor: .white, darkModeColor: .darkIce)
+    }
 
-    public var textCTADisabled: UIColor { .dynamicColor(defaultColor: .gray700, darkModeColor: .darkLicorice) }
+    public var bgAlert: UIColor {
+        .yellow100
+    }
 
-    public var textToast: UIColor { .gray700 }
+    public var bgInfo: UIColor {
+        .dynamicColor(defaultColor: .aqua50, darkModeColor: .darkIce)
+    }
 
-    public var tableViewSeparator: UIColor { .borderDefault }
+    public var bgInfoHeader: UIColor {
+        .dynamicColor(defaultColor: .aqua200, darkModeColor: .blue600)
+    }
 
-    public var imageBorder: UIColor { .borderDefault }
+    public var bgSuccess: UIColor {
+        .green100
+    }
 
-    public var decorationSubtle: UIColor { .btnDisabled }
+    public var bgCritical: UIColor {
+        .red100
+    }
 
-    public var iconPrimary: UIColor { .textPrimary }
+    public var bgInformationAlert: UIColor {
+        .yellow50
+    }
 
-    public var iconSecondary: UIColor { .textSecondary }
+    public var bgInformationCritical: UIColor {
+        .red50
+    }
 
-    public var iconTertiary: UIColor { .textTertiary }
+    public var bgInformationInfo: UIColor {
+        .aqua50
+    }
 
-    public var borderDefault: UIColor { .dynamicColor(defaultColor: .blueGray300, darkModeColor: .darkSardine) }
+    public var bgInformationSuccess: UIColor {
+        .green50
+    }
 
-    public var marketplaceNavigationBarIcon: UIColor { .aqua400 }
+    public var borderWarningSubtle: UIColor {
+        .yellow300
+    }
+
+    public var borderNegativeSubtle: UIColor {
+        .red300
+    }
+
+    public var borderInfoSubtle: UIColor {
+        .aqua300
+    }
+
+    public var borderPositiveSubtle: UIColor {
+        .green300
+    }
+
+    public var borderWarning: UIColor {
+        .yellow600
+    }
+
+    public var borderNegative: UIColor {
+        .red600
+    }
+
+    public var borderInfo: UIColor {
+        .aqua600
+    }
+
+    public var borderPositive: UIColor {
+        .green600
+    }
+
+    public var btnPrimary: UIColor {
+        .dynamicColor(defaultColor: .blue600, darkModeColor: .darkCallToAction)
+    }
+
+    public var btnDisabled: UIColor {
+        .dynamicColor(defaultColor: .blueGray300, darkModeColor: .darkSardine)
+    }
+
+    public var btnCritical: UIColor {
+        .red600
+    }
+
+    public var btnAction: UIColor {
+        .dynamicColor(defaultColor: .blue600, darkModeColor: .darkPrimaryBlue)
+    }
+
+    public var textPrimary: UIColor {
+        .dynamicColor(defaultColor: .gray700, darkModeColor: .white)
+    }
+
+    public var textSecondary: UIColor {
+        .dynamicColor(defaultColor: .gray500, darkModeColor: .darkStone)
+    }
+
+    public var textTertiary: UIColor {
+        .white
+    }
+
+    public var textAction: UIColor {
+        .dynamicColor(defaultColor: .blue600, darkModeColor: .darkPrimaryBlue)
+    }
+
+    public var textDisabled: UIColor {
+        .dynamicColor(defaultColor: .blueGray300, darkModeColor: .darkSardine)
+    }
+
+    public var textAlert: UIColor {
+        .lightNuttyBrown
+    }
+
+    public var textCritical: UIColor {
+        .dynamicColor( defaultColor: .red600, darkModeColor: .red400 )
+    }
+
+    public var accentPrimaryBlue: UIColor {
+        .blue600
+    }
+
+    public var accentSecondaryBlue: UIColor {
+        .aqua400
+    }
+
+    public var accentPea: UIColor {
+        .green400
+    }
+
+    public var accentToothpaste: UIColor {
+        .aqua200
+    }
+
+    public var textCTADisabled: UIColor {
+        .dynamicColor(defaultColor: .gray700, darkModeColor: .darkLicorice)
+    }
+
+    public var textToast: UIColor {
+        .gray700
+    }
+
+    public var tableViewSeparator: UIColor {
+        .borderDefault
+    }
+
+    public var imageBorder: UIColor {
+        .borderDefault
+    }
+
+    public var decorationSubtle: UIColor {
+        .btnDisabled
+    }
+
+    public var iconPrimary: UIColor {
+        .textPrimary
+    }
+
+    public var iconSecondary: UIColor {
+        .textSecondary
+    }
+
+    public var iconTertiary: UIColor {
+        .textTertiary
+    }
+
+    public var borderDefault: UIColor {
+        .dynamicColor(defaultColor: .blueGray300, darkModeColor: .darkSardine)
+    }
+
+    public var marketplaceNavigationBarIcon: UIColor {
+        .aqua400
+    }
 
     // NMP brand colors
-    public var nmpBrandTabBarIconSelected: UIColor { .dynamicColor(defaultColor: .blue600, darkModeColor: .darkCallToAction) }
+    public var nmpBrandTabBarIconSelected: UIColor {
+        .dynamicColor(defaultColor: .blue600, darkModeColor: .darkCallToAction)
+    }
 
-    public var nmpBrandControlSelected: UIColor { .blue600 }
+    public var nmpBrandControlSelected: UIColor {
+        .blue600
+    }
 
-    public var nmpBrandDecoration: UIColor { .blue600 }
+    public var nmpBrandDecoration: UIColor {
+        .blue600
+    }
 
-    public var nmpBrandColorPrimary: UIColor { .blue600 }
+    public var nmpBrandColorPrimary: UIColor {
+        .blue600
+    }
 
-    public var nmpBrandColorSecondary: UIColor { .aqua400 }
+    public var nmpBrandColorSecondary: UIColor {
+        .aqua400
+    }
 
-    public var loadingIndicator: UIColor { .aqua400 }
+    public var loadingIndicator: UIColor {
+        .aqua400
+    }
 }
