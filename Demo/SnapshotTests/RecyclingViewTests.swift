59e18997cfb9d499cc81f900b74a3cfe858ab992
Adrian Sergheev <adrian.sergheev@schibsted.com>
2024-01-10 12:14:15 +0100

    upgrade dependency

--- Demo/SnapshotTests/RecyclingViewTests.swift
+++ Demo/SnapshotTests/RecyclingViewTests.swift
@@ -3,6 +3,7 @@ import XCTest
 import FinniversKit
 import DemoKitSnapshot
 
+@MainActor
 class RecyclingViewTests: XCTestCase {
     private func snapshot(_ component: RecyclingDemoViews, record: Bool = false, line: UInt = #line) {
         snapshotTest(demoable: component.demoable, record: record, line: line)
