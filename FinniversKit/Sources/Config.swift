510e190e5eea75c7d37e3479f8d1555b4ff6bd2e
Mohsen <mohsen.hosseinpour@schibsted.com>
2024-03-05 12:52:38 +0100

    Update Warp

--- FinniversKit/Sources/Config.swift
+++ FinniversKit/Sources/Config.swift
@@ -14,8 +14,8 @@ public struct Config {
     public static var isDynamicTypeEnabled: Bool = true
 }
 
-public var warpToken = Warp.Config.tokenProvider
-public var warpUIToken = Warp.Config.uiTokenProvider
+public let warpToken = Warp.Config.tokenProvider
+public let warpUIToken = Warp.Config.uiTokenProvider
 
 @objc public extension Bundle {
     static var finniversKit: Bundle {
