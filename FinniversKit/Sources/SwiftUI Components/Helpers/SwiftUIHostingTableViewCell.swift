77f44200779b7564ce55ffd2485ee749dcd4f833
Andrii Momot <andrii.momot@schibsted.com>
2024-03-22 20:09:41 +0100

    Add HostingCells for TableView & CollectionView

---
+++ FinniversKit/Sources/SwiftUI Components/Helpers/SwiftUIHostingTableViewCell.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+public class SwiftUIHostingTableViewCell<Content: View>: UITableViewCell {
+
+    private var controller: UIHostingController<Content>?
+
+    public func host(_ view: Content, parent: UIViewController? = nil) {
+
+        if let controller = controller {
+            controller.rootView = view
+            controller.view.layoutIfNeeded()
+        } else {
+            let controller = UIHostingController(rootView: view)
+            controller.view.translatesAutoresizingMaskIntoConstraints = false
+            controller.view.backgroundColor = .clear
+            self.controller = controller
+            
+            parent?.addChild(controller)
+            
+            addSubview(controller.view)
+            
+            NSLayoutConstraint.activate([
+                controller.view.leadingAnchor.constraint(equalTo: leadingAnchor),
+                controller.view.trailingAnchor.constraint(equalTo: trailingAnchor),
+                controller.view.topAnchor.constraint(equalTo: topAnchor),
+                controller.view.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+            
+            if let parent = parent {
+                controller.didMove(toParent: parent)
+            }
+            
+            controller.view.layoutIfNeeded()
+        }
+    }
+}
