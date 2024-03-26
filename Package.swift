4a35c1672c2330d9e25664a09214bbbaee7b160d
Andrii <Omg0Mom@gmail.com>
2024-03-26 13:48:45 +0100

    Add HostingCells for TableView & CollectionView (#1337)
    * Add HostingCells for TableView & CollectionView
    ---------
    
    Co-authored-by: Andrii Momot <andrii.momot@schibsted.com>
    Co-authored-by: Agustí Pellicer <agusti.pellicer@schibsted.com>

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/warp-ds/warp-ios.git", "0.0.1"..."999.0.0")
+        .package(url: "https://github.com/warp-ds/warp-ios.git", "0.0.7"..."999.0.0")
     ],
     targets: [
         .target(
