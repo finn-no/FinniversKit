87210f87ec223c1457bbaaeb33736052afad534f
Charlie Eddus <charlieeddus@gmail.com>
2024-04-02 12:39:05 +0200

    Remove all code regarding Message templates (#1338)
    * Remove all code regarding Message templates
    
    * Remove references to deleted files
    
    * Rerecord snapshots for MessageForm

--- FinniversKit/Sources/Fullscreen/MessageForm/MessageFormViewModel.swift
+++ FinniversKit/Sources/Fullscreen/MessageForm/MessageFormViewModel.swift
@@ -4,19 +4,7 @@
 
 import Foundation
 
-public struct MessageFormTemplate {
-    public let text: String
-    public let id: String?
-
-    public init(text: String, id: String? = nil) {
-        self.text = text
-        self.id = id
-    }
-}
-
 public protocol MessageFormViewModel: AnyObject {
-    var showTemplateToolbar: Bool { get }
-
     var titleText: String { get }
     var sendButtonText: String { get }
     var editButtonText: String { get }
@@ -26,17 +14,11 @@ public protocol MessageFormViewModel: AnyObject {
     var deleteActionText: String { get }
     var transparencyText: String { get }
     var disclaimerText: String { get }
-    var messageTemplates: [MessageFormTemplate] { get }
 
     var messageText: String { get }
     var messageHint: String { get }
     var telephoneText: String { get }
     var telephoneHint: String { get }
-
-    var replaceAlertTitle: String { get }
-    var replaceAlertMessage: String { get }
-    var replaceAlertActionText: String { get }
-    var replaceAlertCancelText: String { get }
 
     var cancelFormAlertTitle: String { get }
     var cancelFormAlertMessage: String { get }
