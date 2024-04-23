9612f1c1437c282e8209585c479ec3185883be67
teddy-falsen-hiis <teddy.falsen.hiis@schibsted.com>
2024-04-23 15:19:59 +0200

    Add divider to SettingsViewIconCell

--- FinniversKit/Sources/Recycling/ListViews/Settings/Cell/SettingsViewIconCell.swift
+++ FinniversKit/Sources/Recycling/ListViews/Settings/Cell/SettingsViewIconCell.swift
@@ -4,6 +4,7 @@ struct SettingsViewIconCell: View {
     let title: String
     let icon: UIImage
     let tintColor: Color?
+    let isLastItem: Bool
 
     var body: some View {
         HStack(alignment: .center) {
@@ -20,12 +21,38 @@ struct SettingsViewIconCell: View {
                 .frame(width: .spacingM, height: .spacingM)
                 .foregroundColor(tintColor)
                 .padding([.trailing], .spacingXL)
-        }
+        }.hairlineDivider(!isLastItem)
     }
 }
 
 struct SettingsViewIconCell_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsViewIconCell(title: "Personvernerklæring", icon: .init(systemName: "square.and.arrow.up")!, tintColor: .bgSuccess) // swiftlint:disable:this force_unwrapping
+        SettingsViewIconCell(title: "Personvernerklæring", icon: .init(systemName: "square.and.arrow.up")!, tintColor: .bgSuccess, isLastItem: false) // swiftlint:disable:this force_unwrapping
+    }
+}
+
+struct HairlineDividerModifier: ViewModifier {
+    let isLastItem: Bool
+    let inset: EdgeInsets
+
+    public func body(content: Content) -> some View {
+        ZStack {
+            content
+            if isLastItem {
+                VStack {
+                    Spacer()
+                    Divider().padding(inset)
+                }
+            }
+        }.listRowInsets(EdgeInsets())
+    }
+}
+
+extension View {
+    public func hairlineDivider(
+        _ isLastItem: Bool,
+        inset: EdgeInsets = EdgeInsets(top: 0, leading: .spacingM, bottom: 0, trailing: 0)
+    ) -> some View {
+        modifier(HairlineDividerModifier(isLastItem: isLastItem, inset: inset))
     }
 }
