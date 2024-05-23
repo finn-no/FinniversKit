83f29af51d3cf61c834e8654e75cf3ec3d893677
Attila Szilagyi <attila.szilagyi@schibsted.com>
2024-05-23 18:35:13 +0200

    Set corner radius for button default style

--- FinniversKit/Sources/Base Components/ButtonStyle.swift
+++ FinniversKit/Sources/Base Components/ButtonStyle.swift
@@ -93,6 +93,7 @@ public struct DefaultStyle: ButtonStyle {
         .background(
             configuration.isPressed ? Color(UIColor.defaultButtonHighlightedBodyColor) : Color.bgPrimary
         )
+        .cornerRadius(.spacingS)
         .roundedBorder(
             radius: .spacingS,
             color: configuration.isPressed ? .nmpBrandControlSelected : .btnDisabled
