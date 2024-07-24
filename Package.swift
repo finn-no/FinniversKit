e8957e278c100028714b044215d385a06be7a35d
Mohsen <mohsen.hosseinpour@schibsted.com>
2024-07-24 14:49:03 +0200

    Update warp

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/warp-ds/warp-ios.git", branch: "nmp-app-design-review")
+        .package(url: "https://github.com/warp-ds/warp-ios.git", "0.0.16"..."999.0.0")
     ],
     targets: [
         .target(
