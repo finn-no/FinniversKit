5df8a58209f30955e8c4e73b4e46aa57c295bfc3
Attila Szilagyi <attila.szilagyi@schibsted.com>
2024-08-22 15:19:55 +0200

    Fix tests.

--- Demo/Sources/SwiftUI/HTMLTextDemoViewController.swift
+++ Demo/Sources/SwiftUI/HTMLTextDemoViewController.swift
@@ -3,6 +3,7 @@ import Foundation
 import SwiftUI
 import UIKit
 import DemoKit
+import Warp
 
 final class HTMLTextDemoViewController: UIViewController {
     let hostingController: UIHostingController<HTMLText>
@@ -50,17 +51,34 @@ extension HTMLTextDemoViewController: TweakableDemo {
     }
 
     func configure(forTweakAt index: Int) {
+        let spanMapper: HTMLStringSwiftUIStyleTranslator.SpanMapper = { attributes, currentStyle in
+            for attribute in attributes {
+                guard attribute.name == "style",
+                      attribute.value == "color:tjt-price-highlight" else {
+                    return
+                }
+
+                currentStyle.foregroundColor = Warp.Token.textNegative
+            }
+        }
+
         switch Tweaks.allCases[index] {
         case .default:
             htmlView = HTMLText("This is <b>HTML</b>")
         case .linebreak:
             htmlView = HTMLText("This is <b>HTML</b><br>over two lines")
         case .priceHighlight:
-            htmlView = HTMLText("Shipping costs <span style=\"color:tjt-price-highlight\">60 NOK</span>")
+            htmlView = HTMLText(
+                "Shipping costs <span style=\"color:tjt-price-highlight\">60 NOK</span>",
+                spanMapper: spanMapper
+            )
         case .strikethrough:
             htmlView = HTMLText("Old price is <del>80 NOK</del>")
         case .mix:
-            htmlView = HTMLText("New price is <b>60 kr</b>, old price is <del>80 kr</del><br>Shipping is <span style=\"color:tjt-price-highlight\">60 kr</span>")
+            htmlView = HTMLText(
+                "New price is <b>60 kr</b>, old price is <del>80 kr</del><br>Shipping is <span style=\"color:tjt-price-highlight\">60 kr</span>",
+                spanMapper: spanMapper
+            )
         }
     }
 }
