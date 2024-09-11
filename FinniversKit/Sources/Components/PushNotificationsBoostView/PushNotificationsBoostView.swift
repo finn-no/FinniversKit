6dcb84fec5b4ef2f24c737efe226dac20df924c4
Andrii Momot <andrii.momot@schibsted.com>
2024-09-11 15:18:49 +0200

    view implementation

--- FinniversKit/Sources/Components/PushNotificationsBoostView/PushNotificationsBoostView.swift
+++ FinniversKit/Sources/Components/PushNotificationsBoostView/PushNotificationsBoostView.swift
@@ -1,16 +1,41 @@
 import SwiftUI
 import Warp
 
-struct PushNotificationsBoostView: View {
+public struct PushNotificationsBoostViewModel {
+    let title: String
+    let description: String
 
-    var body: some View {
+    public init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+}
+
+public struct PushNotificationsBoostView: View {
+    public static let pushNotificationBoostViewCellIdentifier = "pushNotificationBoostViewCellIdentifier"
+    let model: PushNotificationsBoostViewModel
+
+    public init(model: PushNotificationsBoostViewModel) {
+        self.model = model
+    }
+
+    public var body: some View {
         VStack(alignment: .leading, spacing: Warp.Spacing.spacing200) {
             Divider()
                 .foregroundColor(.border)
                 .padding([.leading, .trailing], Warp.Spacing.spacing200)
 
             HStack {
-//                Image(.)
+                Image(named: .alarmOff)
+
+                VStack(alignment: .leading, spacing: Warp.Spacing.spacing50) {
+                    Text(model.title)
+                        .font(.finnFont(.captionStrong))
+                    Text(model.description)
+                        .font(.finnFont(.body))
+                }
+
+                Image(named: .arrowRight)
             }
 
             Divider()
@@ -21,5 +46,8 @@ struct PushNotificationsBoostView: View {
 }
 
 #Preview {
-    PushNotificationsBoostView()
+    PushNotificationsBoostView(model: PushNotificationsBoostViewModel(
+        title: "Boost your chances of a quick sale!",
+        description: "Get real-time alerts when buyers message you or express interest in your items."
+    ))
 }
