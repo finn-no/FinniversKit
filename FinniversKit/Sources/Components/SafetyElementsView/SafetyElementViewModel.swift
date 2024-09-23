e819cffff86090b0153fc9127c1bf47aed9c753f
Bastian Stien <bastian.stien@gmail.com>
2024-09-23 09:29:34 +0200

    SafetyElementViewModel: Add property emphasizedBody

--- FinniversKit/Sources/Components/SafetyElementsView/SafetyElementViewModel.swift
+++ FinniversKit/Sources/Components/SafetyElementsView/SafetyElementViewModel.swift
@@ -1,20 +1,25 @@
-//
-//  Copyright © 2020 FINN AS. All rights reserved.
-//
-
 import Foundation
 
 public struct SafetyElementViewModel {
     public let title: String
     public let icon: UIImage
     public let body: String
+    public let emphasizedBody: String?
     public let topLink: LinkButtonViewModel?
     public let bottomLink: LinkButtonViewModel?
 
-    public init(title: String, icon: UIImage, body: String, topLink: LinkButtonViewModel? = nil, bottomLink: LinkButtonViewModel? = nil) {
+    public init(
+        title: String,
+        icon: UIImage,
+        body: String,
+        emphasizedBody: String? = nil,
+        topLink: LinkButtonViewModel? = nil,
+        bottomLink: LinkButtonViewModel? = nil
+    ) {
         self.title = title
         self.icon = icon
         self.body = body
+        self.emphasizedBody = emphasizedBody
         self.topLink = topLink
         self.bottomLink = bottomLink
     }
