830fe21b6bcd4c71fdb164da886ce58cba32bad2
Bastian Stien <bastian.stien@gmail.com>
2024-10-16 13:20:14 +0200

    Bump platform to iOS 16

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "FinniversKit",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "FinniversKit",
