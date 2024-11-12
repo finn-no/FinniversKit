1d1edc03ab054332b8f532220adb2a5db3d42765
Andrii Momot <andrii.momot@schibsted.com>
2024-11-12 14:42:49 +0100

    better solution

--- FinniversKit/Sources/Extensions/Foundation/StringExtensions.swift
+++ FinniversKit/Sources/Extensions/Foundation/StringExtensions.swift
@@ -33,9 +33,9 @@ public extension String {
         NSAttributedString(string: self, attributes: attributes)
     }
 
-    // Extension if we want to ignore emojies for VoiceOver reader
+    // Ignores emojies symbols accrodingly to https://www.ascii-code.com
     func withoutEmoji() -> String {
-        let filteredScalars = unicodeScalars.filter { $0.value <= 255 }
+        let filteredScalars = unicodeScalars.filter { !$0.properties.isEmojiPresentation }
         return String(filteredScalars)
     }
 }
