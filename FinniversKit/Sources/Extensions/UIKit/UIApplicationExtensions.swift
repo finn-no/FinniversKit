fa52aac470c927a5fc112d385e03b835666889e4
Mariana Steblii <mariana.steblii@schibsted.com>
2025-02-12 13:29:31 +0100

    Fix UIApplication

--- FinniversKit/Sources/Extensions/UIKit/UIApplicationExtensions.swift
+++ FinniversKit/Sources/Extensions/UIKit/UIApplicationExtensions.swift
@@ -3,7 +3,7 @@ import UIKit
 extension UIApplication {
     public var firstWindow: UIWindow? {
         if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            return scene.windows.filter { $0.isKeyWindow }.first
+            return scene.windows.first { $0.isKeyWindow }
         }
         return nil
     }
