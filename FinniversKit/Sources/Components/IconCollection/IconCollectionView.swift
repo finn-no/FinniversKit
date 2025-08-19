88257392a087a0a2bc50f443afcb3f05485853cb
Mariana Steblii <mariana.steblii@schibsted.com>
2025-08-19 09:17:50 +0200

    Pass padding

--- FinniversKit/Sources/Components/IconCollection/IconCollectionView.swift
+++ FinniversKit/Sources/Components/IconCollection/IconCollectionView.swift
@@ -21,6 +21,7 @@ public final class IconCollectionView: UIView {
     // MARK: - Private properties
 
     private var alignment: Alignment
+    private var horisontalPadding: CGFloat
     private var viewModels = [IconCollectionViewModel]()
 
     private lazy var collectionView: UICollectionView = {
@@ -46,7 +47,7 @@ public final class IconCollectionView: UIView {
     private lazy var margins: UIEdgeInsets = {
         switch alignment {
         case .horizontal:
-            return UIEdgeInsets(vertical: Warp.Spacing.spacing200, horizontal: Warp.Spacing.spacing50)
+            return UIEdgeInsets(vertical: Warp.Spacing.spacing200, horizontal: horisontalPadding)
         case .vertical:
             return .zero
         }
@@ -54,8 +55,9 @@ public final class IconCollectionView: UIView {
 
     // MARK: - Init
 
-    public init(alignment: Alignment = .vertical) {
+    public init(alignment: Alignment = .vertical, horisontalPadding: CGFloat) {
         self.alignment = alignment
+        self.horisontalPadding = horisontalPadding
         super.init(frame: .zero)
         setup()
     }
