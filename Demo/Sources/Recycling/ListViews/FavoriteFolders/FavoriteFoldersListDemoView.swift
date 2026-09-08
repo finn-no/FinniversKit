cac9ca3f3ac13f004642b2beacb764fa905fddbc
Andrii Momot <andrii.momot@m10s.io>
2026-09-08 10:57:59 +0200

    record snapshots

--- Demo/Sources/Recycling/ListViews/FavoriteFolders/FavoriteFoldersListDemoView.swift
+++ Demo/Sources/Recycling/ListViews/FavoriteFolders/FavoriteFoldersListDemoView.swift
@@ -134,24 +134,7 @@ extension FavoriteFoldersListDemoView: FavoriteFoldersListViewDataSource {
                                  loadImageWithPath imagePath: String,
                                  imageWidth: CGFloat,
                                  completion: @escaping @Sendable ((UIImage?) -> Void)) {
-        guard let url = URL(string: imagePath) else {
-            completion(nil)
-            return
-        }
-
-        // Demo code only.
-        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
-            usleep(50_000)
-            DispatchQueue.main.async {
-                if let data = data, let image = UIImage(data: data) {
-                    completion(image)
-                } else {
-                    completion(nil)
-                }
-            }
-        }
-
-        task.resume()
+        completion(nil)
     }
 
     func favoriteFoldersListView(_ view: FavoriteFoldersListView, cancelLoadingImageWithPath imagePath: String, imageWidth: CGFloat) {}
